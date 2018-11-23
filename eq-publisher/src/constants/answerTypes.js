74132a649b34b8f1df4d43d70aaeea468d6f7f46
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2018-11-23 14:59:44 +0000

    Implemented automatic piping of checkbox answers

---
+++ eq-publisher/src/constants/answerTypes.js
@@ -0,0 +1,19 @@
+const CHECKBOX = "Checkbox";
+const RADIO = "Radio";
+const TEXTFIELD = "TextField";
+const TEXTAREA = "TextArea";
+const CURRENCY = "Currency";
+const NUMBER = "Number";
+const DATE = "Date";
+const DATE_RANGE = "DateRange";
+
+module.exports = {
+  CHECKBOX,
+  RADIO,
+  TEXTFIELD,
+  TEXTAREA,
+  CURRENCY,
+  NUMBER,
+  DATE,
+  DATE_RANGE
+};
