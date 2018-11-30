450d8119e1ca7766777440aea4c34680fb3c550f
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2018-11-30 12:53:40 +0000

    fixed knexfile

--- eq-author-api/config/knexfile.js
+++ eq-author-api/config/knexfile.js
@@ -1,7 +1,5 @@
 module.exports = {
   client: "pg",
-  connection: {
-    connection: process.env.DB_CONNECTION_URI
-  },
+  connection: process.env.DB_CONNECTION_URI,
   acquireConnectionTimeout: 5000
 };
