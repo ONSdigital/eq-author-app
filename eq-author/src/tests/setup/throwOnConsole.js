356e1a5795eaf77f32fda8e6997d59c77537594d
Adam Mahmood <adammahmood2@gmail.com>
2019-01-11 17:24:18 +0000

    Fix partial error messages being thrown during unit tests

--- eq-author/src/tests/setup/throwOnConsole.js
+++ eq-author/src/tests/setup/throwOnConsole.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console, import/unambiguous */
 const actualConsoleError = console.error.bind(console);
 
-console.error = jest.fn(function(msg) {
-  actualConsoleError(msg);
+console.error = jest.fn(function(msg, ...rest) {
+  actualConsoleError(msg, ...rest);
   throw msg instanceof Error ? msg : new Error(msg);
 });
