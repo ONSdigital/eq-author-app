066552247573b4ccb3d241341d9b7f47813581d8
Tom Andrews <git@tomandrews.co.uk>
2019-01-22 13:40:29 +0000

    Don't rebuild docker containers
    1. Move run scripts to reference the images previously built in the job
    2. Only push docker images on successful builds
    3. Only login to docker once
    4. Stop tagging author twice

--- eq-author-api/scripts/ci.sh
+++ eq-author-api/scripts/ci.sh
@@ -7,9 +7,4 @@ yarn test --coverage
 bash <(curl -s https://codecov.io/bash) -e TRAVIS_NODE_VERSION
 yarn test:breakingChanges
 
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-export TAG=`if [ "$TRAVIS_PULL_REQUEST_BRANCH" == "" ]; then echo "latest"; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi`
-export TAG=${TAG//\//-}
 docker build -t onsdigital/eq-author-api:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
-echo "Pushing with tag [$TAG]"
-docker push onsdigital/eq-author-api:$TAG
