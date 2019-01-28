a2a2f835632bd7cc414fec8b85e4965849aef973
Samiwel Thomas <samiwel.thomas@gmail.com>
2019-01-28 15:00:41 +0000

    Fix delete routing rule functionality.

--- eq-author-api/schema/resolvers/routing2/routingRule2/index.js
+++ eq-author-api/schema/resolvers/routing2/routingRule2/index.js
@@ -1,6 +1,6 @@
 const isMutuallyExclusive = require("../../../../utils/isMutuallyExclusive");
 
-const { flatMap, find, some } = require("lodash/fp");
+const { flatMap, find, some, first, remove, reject } = require("lodash/fp");
 const save = require("../../../../utils/saveQuestionnaire");
 const {
   createDestination,
@@ -66,10 +66,56 @@ Resolvers.Mutation = {
     if (!isMutuallyExclusiveDestination(destination)) {
       throw new Error("Can only provide one destination.");
     }
+
+    /**
+     *
+     * const validateDestination = async (rule, destinationField, destination) => {
+    const routing = await repositories.Routing2.getById(rule.routingId);
+    const availableDestinations = await repositories.Page.getRoutingDestinations(
+      routing.pageId
+    );
+    let list;
+    if (destinationField === "pageId") {
+      list = availableDestinations.questionPages;
+    } else if (destinationField === "sectionId") {
+      list = availableDestinations.sections;
+    }
+    const id = parseInt(destination[destinationField], 10);
+
+    const result = find({ id })(list);
+    if (isNil(result)) {
+      throw new Error(`The provided desination is invalid`);
+    }
+  };
+
+     return async ({ id, destination }) => {
+    const routingRule = await repositories.RoutingRule2.getById(id);
+    const destinationField = Object.keys(destination)[0];
+    if (destinationField !== "logical") {
+      await validateDestination(routingRule, destinationField, destination);
+    }
+
+    await repositories.Destination.update({
+      id: routingRule.destinationId,
+      ...destination,
+    });
+    return routingRule;
+  };
+     */
     return ctx.modifiers.RoutingRule.update({ id, destination });
   },
-  deleteRoutingRule2: (root, { input }, ctx) =>
-    ctx.modifiers.RoutingRule.delete(input.id),
+  deleteRoutingRule2: (root, { input }, ctx) => {
+    const routing = find(routing => {
+      if (some({ id: input.id }, routing.rules)) {
+        return routing;
+      }
+    }, flatMap(page => page.routing, flatMap(section => section.pages, ctx.questionnaire.sections)));
+
+    routing.rules = reject({ id: input.id }, routing.rules);
+
+    save(ctx.questionnaire);
+    return routing;
+  },
 };
 
 module.exports = Resolvers;
