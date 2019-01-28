f045abfb09197637c6a7c0ef721be5b3c3d965a0
Samiwel Thomas <samiwel.thomas@gmail.com>
2019-01-28 13:54:02 +0000

    Fix create routing rule.

--- eq-author-api/schema/resolvers/routing2/routingRule2/index.js
+++ eq-author-api/schema/resolvers/routing2/routingRule2/index.js
@@ -1,6 +1,14 @@
 const isMutuallyExclusive = require("../../../../utils/isMutuallyExclusive");
 
 const { flatMap, find, some } = require("lodash/fp");
+const save = require("../../../../utils/saveQuestionnaire");
+const {
+  createDestination,
+  createRoutingRule,
+  createExpressionGroup,
+  createExpression,
+  createLeftSide,
+} = require("../../../../src/businessLogic");
 
 const isMutuallyExclusiveDestination = isMutuallyExclusive([
   "sectionId",
@@ -15,17 +23,45 @@ Resolvers.RoutingRule2 = {
   expressionGroup: routingRule => routingRule.expressionGroup,
   routing: ({ id }, args, ctx) => {
     const pages = flatMap(section => section.pages, ctx.questionnaire.sections);
-    return find(page => {
-      if (page.routing && some({ id }, page.routing.rules)) {
-        return page.routing;
+    const allRouting = flatMap(page => page.routing, pages);
+    const routing = find(routing => {
+      if (some({ id }, routing.rules)) {
+        return routing;
       }
-    }, pages);
+    }, allRouting);
+    return routing;
   },
 };
 
 Resolvers.Mutation = {
-  createRoutingRule2: async (root, { input }, ctx) =>
-    ctx.modifiers.RoutingRule.create(input.routingId),
+  createRoutingRule2: async (root, { input }, ctx) => {
+    const routing = find(
+      { id: input.routingId },
+      flatMap(
+        page => page.routing,
+        flatMap(section => section.pages, ctx.questionnaire.sections)
+      )
+    );
+
+    const routingRule = createRoutingRule({
+      expressionGroup: createExpressionGroup({
+        expressions: [
+          createExpression({
+            left: createLeftSide({
+              type: "Null",
+              nullReason: "NoRoutableAnswerOnPage",
+            }),
+          }),
+        ],
+      }),
+      destination: createDestination({ logical: "NextPage" }),
+    });
+
+    routing.rules.push(routingRule);
+
+    save(ctx.questionnaire);
+    return routingRule;
+  },
   updateRoutingRule2: async (root, { input: { id, destination } }, ctx) => {
     if (!isMutuallyExclusiveDestination(destination)) {
       throw new Error("Can only provide one destination.");
