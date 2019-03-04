f6e2655529bbe829dc7dd9d4a6ce066a54d648f8
Tom Andrews <git@tomandrews.co.uk>
2019-03-04 14:02:30 +0000

    Throw a browser exception if a graphql error happens. This will not affect the running UI

--- eq-author/src/apollo/createApolloErrorLink.js
+++ eq-author/src/apollo/createApolloErrorLink.js
@@ -2,8 +2,11 @@ import { onError } from "apollo-link-error";
 import { apiDownError } from "redux/saving/actions";
 
 export default getStore =>
-  onError(({ networkError }) => {
-    if (networkError) {
+  onError(({ networkError, graphQLErrors }) => {
+    if (networkError || graphQLErrors) {
       getStore().dispatch(apiDownError());
+      const error =
+        networkError || graphQLErrors.map(e => e.message).join("\n");
+      throw new Error(`Graphql Error detected: ${error}`);
     }
   });
