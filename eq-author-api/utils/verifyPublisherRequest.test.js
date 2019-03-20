d81fe9d7ed9145d02998ee4582478aa5cca8b7ed
SamGodwin2 <sam.godwin@ext.ons.gov.uk>
2019-03-20 14:38:22 +0000

    New stuff works

--- eq-author-api/utils/verifyPublisherRequest.test.js
+++ eq-author-api/utils/verifyPublisherRequest.test.js
@@ -1,14 +1,37 @@
 const verifyPublisherRequest = require("../utils/verifyPublisherRequest");
+const jwt = require("jsonwebtoken");
+const yaml = require("js-yaml");
+const fs = require("fs");
+const SIGNING_ALGORITHM = "RS256";
 
 describe("verifyPublisherRequest", () => {
-  it("should throw if the key is not verified", () => {
-    const invalidToken =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiUHVibGlzaGVyIiwibmFtZSI6IlB1Ymxpc2hlciIsImVtYWlsIjoiZXEudGVhbUBvbnMuZ292LnVrIiwicGljdHVyZSI6IiIsImlhdCI6MTU1MzA4OTU5NH0.qNdMN4JsTOGCNp6uniAOYH-cqPp0lGevJC_12ZFNUbA";
-    verifyPublisherRequest(invalidToken);
+  let validKey, invalidKey, auth;
+  beforeAll(() => {
+    const keysYaml = yaml.safeLoad(fs.readFileSync("./keys.test.yml", "utf8"));
+    const keysJson = JSON.parse(JSON.stringify(keysYaml));
+    validKey = keysJson.keys.publisherAuthSigningKey.value;
+    invalidKey = keysJson.keys.invalidPublisherAuthSigningKey.value;
+    auth = {
+      userId: "test_user",
+      name: "test_user",
+      email: "eq.team@ons.gov.uk",
+      picture: "",
+    };
   });
-  it("return on a successful verification", () => {
-    const validToken =
-      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiUHVibGlzaGVyIiwiaWF0IjoxNTUzMDg5NTA5fQ.T9hBwMmF9o-6Gvf4dEgm4ybM6gNf1Z56858ru7PY_MxkUqVhkQJ34u4OJitBEyZjI6e_78cvnJ0Jmtu5IZxyFnwOMMHFTomiaKEeJuhc51-PyqiZFkC5hxGlmYrfP1y8W1kKqvKO_sLnAT5wx_wOGcWDj5QNz0RjJ8dmFdWB0FrGXSZeXeBn6ySduuw6lvBzGs4-VcO8lB9SZHHEUAd3bYtqbyFWLHXBl9wSZnNp-Mix7EWC6D8VVYIeg3PyW-SDgwpLt6fBvhSIJdJpx-8ln3Xq6Nwdohz35SyYMQ6A7TTBfbYT-R7IgPcKm8nRG7ErSczXKoagWkcE_B_j4J3hsw";
-    verifyPublisherRequest(validToken);
+  it("should throw if the key is not verified", () => {
+    const token = jwt.sign(auth, invalidKey, { algorithm: SIGNING_ALGORITHM });
+    try {
+      verifyPublisherRequest(token);
+    } catch (e) {
+      expect(e.message).toEqual("invalid signature");
+    }
+  });
+  it("should successfully return on a successful verification", () => {
+    const token = jwt.sign(auth, validKey, { algorithm: SIGNING_ALGORITHM });
+    try {
+      verifyPublisherRequest(token);
+    } catch (e) {
+      expect(true).toEqual(false);
+    }
   });
 });
