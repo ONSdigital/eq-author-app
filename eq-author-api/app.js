5a264b578cde49a768d02239533d4b25f83ac4c6
Samiwel Thomas <samiwel.thomas@gmail.com>
2019-05-20 16:44:50 +0100

    Initial work to integrate apollo opentracing.
    Added docker compose and application configuration necessary to instrument the graphql API using apollo opentracing library.
    
    Prometheus metrics are collected for display from Jaeger.
    
    Added separate command for running the API with tracing enabled and added some documentation to the readme.
    
    Add some unit tests around tracer creation.
    
    Add some tests to the server.

--- eq-author-api/app.js
+++ eq-author-api/app.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 
 const logger = require("pino")();
 
-const server = require("./server");
+const { createApp } = require("./server");
 
 const { PORT = 4000 } = process.env;
+const server = createApp();
 
 server.listen(PORT, "0.0.0.0", () => {
   logger.child({ port: PORT }).info("Listening on port");
---
+++ eq-author-api/tracer.js
@@ -0,0 +1,35 @@
+const {
+  initTracerFromEnv,
+  PrometheusMetricsFactory,
+} = require("jaeger-client");
+const promClient = require("prom-client");
+
+let tracer;
+
+const createTracer = logger => {
+  if (!tracer) {
+    const config = {
+      serviceName: process.env.JAEGER_SERVICE_NAME,
+    };
+
+    const namespace = config.serviceName;
+    const metrics = new PrometheusMetricsFactory(promClient, namespace);
+
+    const options = {
+      tags: {
+        "eq_author_api.version": process.env.EQ_AUTHOR_API_VERSION,
+      },
+      metrics,
+      logger,
+    };
+
+    tracer = initTracerFromEnv(config, options);
+  }
+
+  return tracer;
+};
+
+module.exports = logger => ({
+  localTracer: createTracer(logger),
+  serverTracer: createTracer(logger),
+});
