e3830db5cce5b693608c147ca5329ac74aac27c1
Tom Andrews <git@tomandrews.co.uk>
2019-05-22 15:16:06 +0100

    Work from the cache where we can

--- eq-author-api/scripts/ci.sh
+++ eq-author-api/scripts/ci.sh
@@ -7,4 +7,4 @@ yarn test --coverage
 bash <(curl -s https://codecov.io/bash) -e TRAVIS_NODE_VERSION -c -F api
 yarn test:breakingChanges
 
-docker build -t onsdigital/eq-author-api:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
+docker build --cache-from onsdigital/eq-author-api:$TAG -t onsdigital/eq-author-api:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
--- eq-publisher/scripts/ci.sh
+++ eq-publisher/scripts/ci.sh
@@ -5,4 +5,4 @@ yarn install --frozen-lockfile
 yarn lint
 yarn test --coverage
 bash <(curl -s https://codecov.io/bash) -e TRAVIS_NODE_VERSION -c -F publisher
-docker build -t onsdigital/eq-publisher:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
+docker build --cache-from onsdigital/eq-publisher:$TAG  -t onsdigital/eq-publisher:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
