2636a22bee80f1d8289817f073eab13bdc017e75
Samiwel Thomas <samiwel.thomas@gmail.com>
2019-06-05 09:49:08 +0100

    Add a script to grant firebase users admin roles.

---
+++ eq-author-api/scripts/make-admin.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const admin = require("firebase-admin");
+const program = require("commander");
+const fs = require("fs").promises;
+
+program
+  .description("A utility that applies the admin role to a firebase user.")
+  .usage("-p <projectName> -u <uid> -k <serviceAccountKey> [-r]")
+  .option(
+    "-k, --service-account-key <serviceAccountKey>",
+    "path to the service account key file"
+  )
+  .option(
+    "-p, --projectName <projectName>",
+    "firebase project name used for DatabaseURL"
+  )
+  .option("-u, --uid <uid>", "firebase user Id to make admin")
+  .option("-r, --remove", "remove the admin role")
+  .parse(process.argv);
+
+const formatError = arg => `Missing ${arg}. See -h for usage info.`;
+
+const makeAdmin = async (
+  projectName,
+  uid,
+  serviceAccountKey,
+  remove = false
+) => {
+  if (!projectName) {
+    throw new Error(formatError("project name"));
+  }
+  if (!uid) {
+    throw new Error(formatError("uid"));
+  }
+
+  if (!serviceAccountKey) {
+    throw new Error(formatError("service account key"));
+  }
+
+  const serviceAccount = await fs.readFile(serviceAccountKey, "utf-8");
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(serviceAccount)),
+    databaseURL: `https://${projectName}.firebaseio.com`,
+  });
+
+  await admin.auth().setCustomUserClaims(program.uid, {
+    admin: !remove,
+  });
+
+  const userRecord = await admin.auth().getUser(uid);
+  return userRecord.customClaims.admin;
+};
+
+makeAdmin(
+  program.projectName,
+  program.uid,
+  program.serviceAccountKey,
+  program.remove
+)
+  .then(isAdmin => {
+    console.log(
+      `User ${program.uid} in ${program.projectName} is ${
+        isAdmin === true ? "now" : "no longer"
+      } an admin`
+    );
+    process.exit(0);
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
