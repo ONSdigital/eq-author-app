90fea96a103b7ec32dd2595306ce918c4c5a8060
Matthew Elcock <mattiose@outlook.com>
2020-03-03 10:13:42 +0000

    Modified datastore logging

--- eq-author-api/db/datastore/index.js
+++ eq-author-api/db/datastore/index.js
@@ -3,12 +3,13 @@ const logger = require("pino")();
 const DEFAULT_DATABASE = "dynamodb";
 
 const databaseName = process.env.DATABASE || DEFAULT_DATABASE;
-const datastore = require(`./datastore-${databaseName}`);
 
 if (!process.env.DATABASE) {
-  logger.info(
-    `Env var DATABASE not set; using default value: ${DEFAULT_DATABASE}`
-  );
+  logger.info(`Env var DATABASE not set; using default: ${DEFAULT_DATABASE}`);
+} else {
+  logger.info(`Using database: ${databaseName}`);
 }
+
+const datastore = require(`./datastore-${databaseName}`);
 
 module.exports = datastore;
