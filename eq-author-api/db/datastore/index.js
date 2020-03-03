ddc0364eb18e82d4ac892bb4b220c3d14a664a79
Matthew Elcock <mattiose@outlook.com>
2020-03-03 09:47:36 +0000

    Fixed linting issues

--- eq-author-api/db/datastore/index.js
+++ eq-author-api/db/datastore/index.js
@@ -2,11 +2,11 @@ const logger = require("pino")();
 
 const DYNAMODB = "dynamodb";
 
-const databaseName = process.env.DATABASE || 'dynamodb';
-const datastore = require(`./datastore-${databaseName}`)
+const databaseName = process.env.DATABASE || DYNAMODB;
+const datastore = require(`./datastore-${databaseName}`);
 
 if (!process.env.DATABASE) {
-  logger.info("Env var DATABASE not set; using default value: dynamodb")
+  logger.info(`Env var DATABASE not set; using default value: ${DYNAMODB}`);
 }
 
 module.exports = datastore;
