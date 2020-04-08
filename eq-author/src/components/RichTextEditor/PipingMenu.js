d81b91cd12b411edf3bdaeceb0755baaa235bece
thomas.mcauliffe <tjamesmac@gmail.com>
2020-04-08 15:43:50 -0700

    Add area to uncomment

--- eq-author/src/components/RichTextEditor/PipingMenu.js
+++ eq-author/src/components/RichTextEditor/PipingMenu.js
@@ -191,6 +191,35 @@ export const UnwrappedPipingMenu = ({
   match,
   ...otherProps
 }) => {
+  // if (!canFocus) {
+  //   return (
+  //     <>
+  //       {allowableTypes.includes(ANSWER) && (
+  //         <MenuButton title="Pipe value" disabled data-test="piping-button">
+  //           <IconPiping />
+  //         </MenuButton>
+  //       )}
+  //       {allowableTypes.includes(METADATA) && (
+  //         <MenuButton
+  //           title="Pipe metadata"
+  //           disabled
+  //           data-test="piping-button-metadata"
+  //         >
+  //           <IconPipingMetadata />
+  //         </MenuButton>
+  //       )}
+  //       {allowableTypes.includes(VARIABLES) && (
+  //         <MenuButton
+  //           title="Pipe variable"
+  //           disabled
+  //           data-test="piping-button-variable"
+  //         >
+  //           <IconPipingVariable />
+  //         </MenuButton>
+  //       )}
+  //     </>
+  //   );
+  // }
   return (
     <AvailablePipingContentQuery
       questionnaireId={match.params.questionnaireId}
