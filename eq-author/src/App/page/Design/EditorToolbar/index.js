910ba276d90baeac6f05b2b91211bb191066bc22
Matthew Elcock <mattiose@outlook.com>
2020-05-12 13:17:53 +0100

    Added grid to control toolbar layout

--- eq-author/src/App/page/Design/EditorToolbar/index.js
+++ eq-author/src/App/page/Design/EditorToolbar/index.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
 
 export const Toolbar = styled.div`
-  display: flex;
-  align-items: flex-start;
+  display: grid;
+  grid-template-columns: 50% 50%;
   padding: 1.5em 2em;
 `;
 
 export const Buttons = styled.div`
   margin: 0 0 0 auto;
+  grid-column-start: 2;
+  grid-row-start: 1;
   display: grid;
   grid-gap: 0.5em;
   grid-auto-flow: column;
-  flex: 0 0 auto;
   padding-left: 1em;
 `;
