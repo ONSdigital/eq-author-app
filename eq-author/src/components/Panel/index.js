1be81b30f65955dc3e8e843dcac6bdc7416313e3
Matthew Elcock <mattiose@outlook.com>
2020-05-28 15:17:15 +0100

    Merge

--- eq-author/src/components/Panel/index.js
+++ eq-author/src/components/Panel/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 import { radius, colors } from "constants/theme";
 
 const Panel = styled.div`
