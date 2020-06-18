9c86c68b7103dced3c4fdf7cdf85cb870d022301
zimboflyman <nemazine@hotmail.com>
2020-06-18 18:02:35 +0100

    sideBar and content

--- eq-author/src/App/page/Logic/index.js
+++ eq-author/src/App/page/Logic/index.js
@@ -2,19 +2,82 @@ import React from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import { Redirect, Link, Switch, Route } from "react-router-dom";
-
+import { buildPagePath } from "utils/UrlUtils";
+import Loading from "components/Loading";
+import Error from "components/Error";
 import { get } from "lodash";
 import PropTypes from "prop-types";
 import { propType } from "graphql-anywhere";
 
-import Routing from "./Routing";
-import EditorLayout from "components/EditorLayout";
-import Loading from "components/Loading";
-import Error from "components/Error";
-import { buildPagePath } from "utils/UrlUtils";
+import styled from "styled-components";
+import { colors } from "constants/theme";
+import { rgba } from "polished";
+import { Grid, Column } from "components/Grid";
 
+import EditorLayout from "components/EditorLayout";
 import QuestionPageEditor from "App/page/Design/QuestionPageEditor";
+import Routing from "./Routing";
+
 const ROUTING_PAGE_TYPES = ["QuestionPage"];
+const activeClassName = "active";
+
+const LogicMainCanvas = styled.div`
+  display: flex;
+  border: 1px solid ${colors.lightGrey};
+  border-radius: 4px;
+  background: ${colors.white};
+`;
+
+// const TabsContainer = styled.div`
+//   border-right
+// `;
+
+// const StyledLi = styled.li`
+// `;
+
+const LogicContainer = styled.div`
+  padding: 0.8em;
+  border-left: 1px solid ${colors.lightGrey};
+`;
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const LogicLink = styled(Link)`
+  --color-text: ${colors.black};
+  text-decoration: none;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 1em;
+  color: var(--color-text);
+  font-size: 1em;
+  border-left: 4px solid ${colors.lightGrey};
+  border-bottom: 1px solid ${colors.lightGrey};
+
+  &:hover {
+    background: ${rgba(0, 0, 0, 0.2)};
+  }
+
+  &:focus {
+    outline: 3px solid ${colors.orange};
+    outline-offset: -3px;
+  }
+
+  &:active {
+    outline: none;
+  }
+
+  &.${activeClassName} {
+    --color-text: ${colors.white};
+
+    background: ${colors.blue};
+    pointer-events: none;
+  }
+`;
 
 export class UnwrappedQuestionRoutingRoute extends React.Component {
   static propTypes = {
@@ -73,54 +136,71 @@ export class UnwrappedQuestionRoutingRoute extends React.Component {
     ];
 
     return (
-      <div style={{ display: "flex" }}>
-        <div
-          style={{
-            padding: "10px",
-            width: "20%",
-          }}
-        >
-          <ul style={{ listStyleType: "none", padding: 0 }}>
-            <li>
-              <Link to={`${match.url}/routing`}>Routing Logic</Link>
-            </li>
-            <li>
-              <Link to={`${match.url}/skip`}>Skip Logic</Link>
-            </li>
-          </ul>
-        </div>
-        <div style={{ flex: 1, padding: "10px" }}>
-          {routes.map(route => (
-            <Route
-              key={route.path}
-              path={route.path}
-              exact={route.exact}
-              component={route.main}
-            />
-          ))}
-        </div>
-        {/* <div style={{ flex: 1, padding: "10px" }}>
-          <Switch>
-            <Route
-              key="page-routing"
-              path={`${match.path}`}
-              // path="/q/:questionnaireId/page/:pageId/logic/routing"
-              component={Routing}
-              // page={page}
-            />
-          </Switch>
-        </div> */}
-      </div>
+      <LogicMainCanvas>
+        <Grid>
+          <Column gutters={false} cols={2.5}>
+            <StyledUl>
+              <li>
+                <LogicLink>Select your logic</LogicLink>
+              </li>
+              <li>
+                <LogicLink
+                  exact
+                  to={`${match.url}/routing`}
+                  activeClassName="active"
+                  onlyActiveOnIndex
+                >
+                  Routing Logic
+                </LogicLink>
+              </li>
+              <li>
+                <LogicLink
+                  exact
+                  to={`${match.url}/skip`}
+                  activeClassName="active"
+                  onlyActiveOnIndex
+                >
+                  Skip Logic
+                </LogicLink>
+              </li>
+            </StyledUl>
+          </Column>
+
+          <Column gutters={false} cols={9.5}>
+            <LogicContainer>
+              {routes.map(route => (
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))}
+            </LogicContainer>
+          </Column>
+        </Grid>
+      </LogicMainCanvas>
+      //  {/* <div style={{ flex: 1, padding: "10px" }}>
+      //           <Switch>
+      //             <Route
+      //               key="page-routing"
+      //               path={`${match.path}`}
+      //               // path="/q/:questionnaireId/page/:pageId/logic/routing"
+      //               component={Routing}
+      //               // page={page}
+      //             />
+      //           </Switch>
+      //         </div> */}
 
       // <div>
-      //   <ul>
+      //   <StyledUl>
       //     <li>
       //       <Link to={`${match.url}/routing`}>Routing Logic</Link>
       //     </li>
       //     <li>
       //       <Link to="/dashboard">Skip Logic</Link>
       //     </li>
-      //   </ul>
+      //   </StyledUl>
 
       //   <hr />
 
