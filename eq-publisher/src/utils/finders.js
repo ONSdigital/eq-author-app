d1109e7150d38a3e85eb28664a19d3c1283ed532
Matthew Elcock <mattiose@outlook.com>
2020-07-03 13:44:29 +0100

    Tests

--- eq-publisher/src/utils/finders.js
+++ eq-publisher/src/utils/finders.js
@@ -1,23 +1,11 @@
-const { filter, flatMap, compact } = require("lodash");
+const { flatMap, compact } = require("lodash");
 
 const getAllAnswers = questionnaire =>
   flatMap(questionnaire.sections, section =>
     compact(flatMap(section.pages, page => page.answers))
   );
 
-const getAnswerByOptionId = (questionnaire, optionId) => {
-  const answers = getAllAnswers(questionnaire);
-  for (const answer of answers) {
-    if (filter(answer.options, option => option.id === optionId).length) {
-      return answer;
-    }
-    if (answer.mutuallyExclusiveOption.id === optionId) {
-      return answer;
-    }
-  }
-};
-
 const getAnswerById = (questionnaire, answerId) =>
   getAllAnswers(questionnaire).find(answer => answer.id === answerId);
 
-module.exports = { getAnswerByOptionId, getAnswerById, getAllAnswers };
+module.exports = { getAnswerById, getAllAnswers };
