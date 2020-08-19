9f799a71cd7d4c610ec5e45d28d4b558101d4cfc
martyncolmer <martyc1974@hotmail.com>
2020-08-19 16:13:37 +0100

    added scroll pane

--- eq-author/.storybook/preview.js
+++ eq-author/.storybook/preview.js
@@ -1,4 +1,17 @@
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+export const decorators = [
+  Story => (
+    <Router history={createMemoryHistory({ initialEntries: ["/"] })}>
+      <Route path="/">
+        <Story />
+      </Route>
+    </Router>
+  ),
+];
