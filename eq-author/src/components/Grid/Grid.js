4212a186299585b571821fb4bc68ca32579f1c9c
thomas.mcauliffe <tjamesmac@gmail.com>
2020-09-01 08:48:41 -0700

    Lower the max-height

--- eq-author/src/components/Grid/Grid.js
+++ eq-author/src/components/Grid/Grid.js
@@ -6,6 +6,10 @@ const Grid = styled.div.attrs(() => ({ "data-test": "grid" }))`
   width: 100%;
   height: 100%;
 
+  @media (max-height: 580px) {
+    height: auto;
+  }
+
   flex: ${({ fillHeight }) => (fillHeight ? 1 : 0)} 1 auto;
   flex-direction: row;
   align-items: ${({ align }) => alignOptions[align]};
