39a5223f80ef21bddb847f56959e9ff0b9b4c9b9
Matthew Elcock <mattiose@outlook.com>
2020-09-08 09:52:45 +0100

    Updated script to run on merge to master

---
+++ eq-author/.storybook/theme.js
@@ -0,0 +1,12 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+
+  fontBase: '"Open Sans", sans-serif',
+
+  brandTitle: "Author",
+  brandUrl: "https://github.com/ONSdigital/eq-author-app",
+  brandImage:
+    "https://raw.githubusercontent.com/ONSdigital/eq-author-app/master/eq-author/public/android-chrome-192x192.png",
+});
