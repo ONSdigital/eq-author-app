52cea6e7c63c720dc0a603146441eebb56ed540c
Thomas Baggaley <tbaggaley@protonmail.com>
2020-09-21 11:17:20 +0100

    Merge refactoring changes

---
+++ eq-author/src/App/page/Design/Validation/DateValidation/PositionPicker.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { byTestAttr } from "tests/utils/selectors";
+
+import PositionPicker from "./PositionPicker";
+
+describe("Date Position Picker", () => {
+    let props, wrapper;
+    beforeEach(() => {
+        props = {
+            value: "before",
+            onChange: jest.fn(),
+            onUpdate: jest.fn()
+        };
+
+        wrapper = shallow(<PositionPicker {...props} />);
+    });
+
+    it("Should render", () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("should trigger update answer validation when the relative position changes", () => {
+        const relativePositionField = wrapper.find(
+            byTestAttr("relative-position-select")
+        );
+        relativePositionField.simulate("change", "event");
+        expect(props.onChange).toHaveBeenCalledWith("event");
+        relativePositionField.simulate("blur", "event");
+        expect(props.onUpdate).toHaveBeenCalledWith("event");
+    });
+});
