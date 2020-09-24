61fa52841bdfb774b7e8ad4f85dd14e3926fbdac
zimboflyman <nemazine@hotmail.com>
2020-09-24 17:17:30 +0100

    extra tests

--- eq-author/src/App/page/Design/Validation/DurationValidation.test.js
+++ eq-author/src/App/page/Design/Validation/DurationValidation.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import Duration from "./Duration";
 import { DATE_RANGE } from "constants/answer-types";
 
 import DurationValidation from "./DurationValidation";
+import { ERR_NO_VALUE } from "constants/validationMessages";
 
 const createWrapper = (props, render = shallow) =>
   render(<DurationValidation {...props} />);
@@ -24,17 +25,25 @@ describe("Duration Validation", () => {
         id: "123",
         enabled: true,
         duration: {
-          value: 5,
+          value: null,
           unit: "Months",
         },
-        validationErrorInfo: { errors: [] },
+        validationErrorInfo: {
+          errors: [
+            {
+              errorCode: "ERR_NO_VALUE",
+              field: "minDuration",
+              id: "0efd3ed1-8e0d-4b0c-9e39-59010751dbdf",
+              type: "validation",
+            }
+          ]
+        },
       },
       onToggleValidationRule: jest.fn(),
       onChange: jest.fn(),
       onUpdate: jest.fn(),
       displayName: "Some date",
       testId: "duration-test-id",
-      hasError: false,
     };
 
     wrapper = createWrapper(props);
@@ -51,5 +60,20 @@ describe("Duration Validation", () => {
 
     duration.simulate("update", "event");
     expect(props.onUpdate).toHaveBeenCalledWith("event");
+  });
+
+  it("should display validation message when error present", () => {
+    const wrapper = shallow(<DurationValidation {...props} />).find(
+      "DurationValidation__StyledError"
+    );
+    expect(wrapper.text()).toEqual(ERR_NO_VALUE);
+  });
+
+  it("should display error styling when error present", () => {
+    const wrapper = mount(<DurationValidation {...props} />).find(
+      "Duration__DurationNumber"
+    );
+
+    expect(wrapper).toHaveStyleRule("border-radius: 4px;");
   });
 });
