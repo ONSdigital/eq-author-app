081b0e9ad4d830e0a4624041295f92a16b928de0
Thomas Baggaley <tbaggaley@pm.me>
2020-10-23 10:19:07 +0100

    EAR 922 changes - disable qCode entry for checkbox questions (only options should get qCodes)

--- eq-author/src/App/qcodes/QCodesTable/index.js
+++ eq-author/src/App/qcodes/QCodesTable/index.js
@@ -284,28 +284,32 @@ const Row = memo(
           <>
             <SpacedTableColumn>{questionMatrix[type]}</SpacedTableColumn>
             <SpacedTableColumn>{label}</SpacedTableColumn>
-            <SpacedTableColumn>
-              <ErrorWrappedInput
-                value={qCode}
-                onChange={e => setQcode(e.value)}
-                onBlur={() => handleBlur(id, type, qCode)}
-                name={`${id}-qcode-entry`}
-                data-test={`${id}-test-input`}
-                error={error}
-              />
+            {type === "Checkbox" ? (
+              <EmptyTableColumn />
+            ) : (
+              <SpacedTableColumn>
+                <ErrorWrappedInput
+                  value={qCode}
+                  onChange={e => setQcode(e.value)}
+                  onBlur={() => handleBlur(id, type, qCode)}
+                  name={`${id}-qcode-entry`}
+                  data-test={`${id}-test-input`}
+                  error={error}
+                />
 
-              {error && (
-                <QcodeValidationError right>
-                  {QCODE_IS_NOT_UNIQUE}
-                </QcodeValidationError>
-              )}
+                {error && (
+                  <QcodeValidationError right>
+                    {QCODE_IS_NOT_UNIQUE}
+                  </QcodeValidationError>
+                )}
 
-              {noValQCodeError && (
-                <QcodeValidationError right>
-                  {QCODE_REQUIRED}
-                </QcodeValidationError>
-              )}
-            </SpacedTableColumn>
+                {noValQCodeError && (
+                  <QcodeValidationError right>
+                    {QCODE_REQUIRED}
+                  </QcodeValidationError>
+                )}
+              </SpacedTableColumn>
+            )}
           </>
         );
       },
