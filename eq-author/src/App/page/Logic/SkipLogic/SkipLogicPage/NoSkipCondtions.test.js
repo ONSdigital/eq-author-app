191a3943416d9f725268a8391cb7a0d415c0985f
Joshua Chapman <joshua.chapman@ons.gov.uk>
2020-11-02 12:27:23 +0000

    Updated test with first question parameter

--- eq-author/src/App/page/Logic/SkipLogic/SkipLogicPage/NoSkipCondtions.test.js
+++ eq-author/src/App/page/Logic/SkipLogic/SkipLogicPage/NoSkipCondtions.test.js
@@ -2,11 +2,18 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import NoSkipConditions from "./NoSkipConditions";
+import NavigationSidebar from "../../../../QuestionnaireDesignPage/NavigationSidebar";
+// src/app/page/logic/skiplogic/skiplogicpage/
+// src/app/QuestionnaireDesignPage/NavigationSidebar
+
 
 describe("components/NoSkipConditions", () => {
   it("should render", () => {
     const wrapper = shallow(
-      <NoSkipConditions onAddSkipCondtions={jest.fn()} title="Test">
+      <NoSkipConditions
+        onAddSkipCondtions={jest.fn()}
+        title="Test"
+        isFirstQuestion={true}>
         Ullamcorper Venenatis Fringilla
       </NoSkipConditions>
     );
@@ -16,11 +23,31 @@ describe("components/NoSkipConditions", () => {
   it("should call onAddSkipConditions when button clicked", () => {
     const onAddSkipCondtions = jest.fn();
     const wrapper = shallow(
-      <NoSkipConditions onAddSkipCondtions={onAddSkipCondtions} title="Test">
+      <NoSkipConditions
+        onAddSkipCondtions={onAddSkipCondtions}
+        title="Test"
+        isFirstQuestion={true}>
         Ullamcorper Venenatis Fringilla
       </NoSkipConditions>
     );
     wrapper.find("[data-test='btn-add-skip-condition']").simulate("click");
     expect(onAddSkipCondtions).toHaveBeenCalled();
   });
+
+
+  
+  // NEW TESTS
+
+
+  // it("should add new page below current page", () => {
+  //   wrapper.find(NavigationSidebar).simulate("addQuestionPage");
+
+  //   expect(mockHandlers.onAddQuestionPage).toHaveBeenCalledWith(
+  //     section.id,
+  //     page.position + 1
+  //   );
+  // });  
+
+
+
 });
