a0e61e68a9c11fdd8f4dcd4365a0afefde74c277
Joshua Chapman <joshua.chapman@ons.gov.uk>
2020-11-13 12:25:04 +0000

    Disabled add routing button

--- eq-author/src/App/page/Logic/Routing/RoutingPage/NoRouting.test.js
+++ eq-author/src/App/page/Logic/Routing/RoutingPage/NoRouting.test.js
@@ -2,25 +2,61 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import NoRouting from "./NoRouting";
+import { render, fireEvent, screen } from "tests/utils/rtl";
 
 describe("components/NoRouting", () => {
-  it("should render", () => {
-    const wrapper = shallow(
+  const disabledTitle = "Routing is not available for this quesiton";
+  const disabledParagraph =
+    "You can't route on the last question in a questionnaire.";
+  const enabledTitle = "No routing rules exist for this question";
+  const enabledParagraph =
+    "Users completing this question will be taken to the next page.";
+
+  it("should render with button enabled", () => {
+    const { queryByText } = render(
       <NoRouting onAddRouting={jest.fn()} title="Test">
         Ullamcorper Venenatis Fringilla
       </NoRouting>
     );
-    expect(wrapper).toMatchSnapshot();
+
+    expect(queryByText(enabledTitle)).toBeTruthy();
+    expect(queryByText(enabledParagraph)).toBeTruthy();
+    expect(queryByText(disabledTitle)).toBeFalsy();
+    expect(queryByText(disabledParagraph)).toBeFalsy();
+
+    expect(screen.getByTestId("btn-add-routing")).not.toHaveStyle(
+      `pointer-events: none; 
+       opacity: 0.6;`
+    );
+  });
+
+  it("should render with button disabled", () => {
+    const { queryByText } = render(
+      <NoRouting onAddRouting={jest.fn()} title="Test" isLastPage>
+        Ullamcorper Venenatis Fringilla
+      </NoRouting>
+    );
+
+    expect(queryByText(disabledTitle)).toBeTruthy();
+    expect(queryByText(disabledParagraph)).toBeTruthy();
+    expect(queryByText(enabledTitle)).toBeFalsy();
+    expect(queryByText(enabledParagraph)).toBeFalsy();
+
+    expect(screen.getByTestId("btn-add-routing")).toHaveStyle(
+      `pointer-events: none; 
+       opacity: 0.6;`
+    );
   });
 
   it("should call onAddRouting when button clicked", () => {
     const onAddRouting = jest.fn();
-    const wrapper = shallow(
+    const { getByTestId } = render(
       <NoRouting onAddRouting={onAddRouting} title="Test">
         Ullamcorper Venenatis Fringilla
       </NoRouting>
     );
-    wrapper.find("[data-test='btn-add-routing']").simulate("click");
-    expect(onAddRouting).toHaveBeenCalled();
+    const button = getByTestId("btn-add-routing");
+    fireEvent.click(button);
+    expect(onAddRouting).toHaveBeenCalledTimes(1);
   });
 });
