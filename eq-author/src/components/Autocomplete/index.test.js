4d9e16eb119061e6fa9ef435cc47d17d6c504289
thomas.mcauliffe <tjamesmac@gmail.com>
2020-11-16 09:26:22 +0000

    Add component template

---
+++ eq-author/src/components/Autocomplete/index.test.js
@@ -0,0 +1,9 @@
+describe("components/Autocomplete", () => {
+  it("should receive focus", () => {});
+  it("should have correct styling on selected options", () => {});
+  it("should be accessible with a keyboard", () => {});
+  it("should select an option when pressing <button of your choice here>", () => {});
+  it("should open dropdown when not empty", () => {});
+  it("should return no results", () => {});
+  it("should save option onBlur", () => {});
+});
