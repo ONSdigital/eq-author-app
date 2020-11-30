49160c4b9b4bd7b1783f83236edaf68154b5ef4b
Thomas Baggaley <tbaggaley@pm.me>
2020-11-30 13:06:48 +0000

    🛠 Fix deleting pages on front-end

--- eq-author/src/App/page/Design/withDeletePage.js
+++ eq-author/src/App/page/Design/withDeletePage.js
@@ -71,22 +71,26 @@ const handleDeletion = (
       params: { questionnaireId },
     },
   },
-  section,
+  { folders },
   nextPage
-) =>
+) => {
+  const newPageCreated = folders.length === 1 && folders[0].pages.length === 1;
+
   history.push(
     buildPagePath({
       questionnaireId,
-      pageId: section.pages.length === 1 ? section.pages[0].id : nextPage.id,
+      pageId: newPageCreated ? folders[0].pages[0].id : nextPage.id,
     })
   );
+};
 
 export const mapMutateToProps = props => ({
   onDeletePage(page) {
     const { ownProps, mutate } = props;
     const { client } = ownProps;
     const cachedSection = getCachedSection(client, page.section.id);
-    const nextPage = getNextPage(cachedSection.pages, page.id);
+    const cachedPages = cachedSection.folders.flatMap(({ pages }) => pages);
+    const nextPage = getNextPage(cachedPages, page.id);
     const mutation = mutate({
       variables: { input: { id: page.id } },
       refetchQueries: [
