e3bea00cf3f510e61f3fab2d0e24452a8e6220e3
Thomas Baggaley <tbaggaley@pm.me>
2020-12-03 11:31:48 +0000

    🧹 Lint

--- eq-author-api/src/businessLogic/onFolderDeleted.js
+++ eq-author-api/src/businessLogic/onFolderDeleted.js
@@ -1,7 +1,6 @@
 const onPageDeleted = require("../../src/businessLogic/onPageDeleted");
 
 const onFolderDeleted = (ctx, removedFolder) => {
-  console.log(removedFolder);
   if (removedFolder.pages) {
     removedFolder.pages.forEach(page => {
       onPageDeleted(ctx, removedFolder, page);
