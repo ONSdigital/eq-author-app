1218c3c481ff3814ec5cea3a023d23cd61217c8b
Thomas Baggaley <tbaggaley@pm.me>
2020-12-03 16:27:45 +0000

    ✅️ Refactor createMockQuestionnaire for tests
    Add folder functionality & add ability to specify quantity options for
    each element.
    
    To be used to refactor some of the existing tests.

--- eq-author/src/tests/utils/createMockQuestionnaire.js
+++ eq-author/src/tests/utils/createMockQuestionnaire.js
@@ -1,23 +1,65 @@
 import { times } from "lodash";
 
-export const buildPages = (sectionNumber, count) =>
-  times(count, i => ({
-    id: `${sectionNumber}.${i + 1}`,
-    title: `Page ${sectionNumber}.${i + 1}`,
-    displayName: `Page ${sectionNumber}.${i + 1}`,
-    position: i,
-  }));
+const DEFAULT_PAGE_COUNT = 1;
+const DEFAULT_FOLDER_COUNT = 2;
+const DEFAULT_SECTION_COUNT = 2;
 
-export const buildSections = count =>
-  times(count, i => ({
+export const buildPages = ({
+  sectionNumber = 1,
+  folderNumber = 1,
+  pageCount = DEFAULT_PAGE_COUNT,
+} = {}) =>
+  times(pageCount, i => {
+    const id = `${sectionNumber}.${folderNumber}.${i + 1}`;
+
+    return {
+      id,
+      title: `Page ${id}`,
+      displayName: `Page ${id}`,
+      alias: id,
+      position: i,
+    };
+  });
+
+export const buildFolders = ({
+  sectionNumber = 1,
+  folderCount = DEFAULT_FOLDER_COUNT,
+  pageCount = DEFAULT_PAGE_COUNT,
+} = {}) =>
+  times(folderCount, i => {
+    const id = `${sectionNumber}.${i + 1}`;
+
+    return {
+      id,
+      alias: `Folder ${id}`,
+      enabled: false,
+      position: i,
+      pages: buildPages({
+        sectionNumber,
+        folderNumber: i + 1,
+        pageCount,
+      }),
+    };
+  });
+
+export const buildSections = ({
+  sectionCount = DEFAULT_SECTION_COUNT,
+  folderCount = DEFAULT_FOLDER_COUNT,
+  pageCount = DEFAULT_PAGE_COUNT,
+} = {}) =>
+  times(sectionCount, i => ({
     id: `${i + 1}`,
     title: `Section ${i + 1}`,
     displayName: `Section ${i + 1}`,
-    pages: buildPages(i + 1, 2),
+    folders: buildFolders({
+      sectionNumber: i + i,
+      folderCount,
+      pageCount,
+    }),
     position: i,
   }));
 
-export const buildQuestionnaire = (numOfSections = 2) => ({
+export const buildQuestionnaire = options => ({
   id: "1",
-  sections: buildSections(numOfSections),
+  sections: buildSections(options),
 });
