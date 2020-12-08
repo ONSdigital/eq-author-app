e9db3874caf0527cc869b19f19a5d864613dc78d
martyncolmer <martyc1974@hotmail.com>
2020-12-08 09:57:49 +0000

    First commit

---
+++ eq-author-api/db/datastore/datastore-mongodb.js
@@ -0,0 +1,490 @@
+const MongoClient = require("mongodb").MongoClient;
+const { v4: uuidv4 } = require("uuid");
+const { removeEmpty } = require("../../utils/removeEmpty");
+const { pick } = require("lodash/fp");
+const { baseQuestionnaireFields, id } = require("../baseQuestionnaireSchema");
+const { logger } = require("../../utils/logger");
+const {
+  questionnaireCreationEvent,
+} = require("../../utils/questionnaireEvents");
+
+const url = process.env.MONGODB_URL;
+const dbname = process.env.MONGODB_NAME;
+let dbo;
+
+MongoClient.connect(url, function(err, db) {
+  if (err) {
+    logger.error(err);
+    throw err;
+  }
+  dbo = db.db(dbname);
+  logger.info("Database connected");
+});
+
+const BASE_FIELDS = [
+  ...Object.keys(baseQuestionnaireFields),
+  "updatedAt",
+  "history",
+];
+
+const justListFields = pick(BASE_FIELDS);
+
+const createQuestionnaire = async (questionnaire, ctx) => {
+  const updatedAt = new Date();
+
+  if (!questionnaire.id) {
+    questionnaire.id = uuidv4();
+  }
+
+  let { id } = questionnaire;
+
+  delete questionnaire._id;
+
+  const baseQuestionnaire = removeEmpty({
+    ...justListFields(questionnaire),
+    history: [questionnaireCreationEvent(questionnaire, ctx)],
+    updatedAt,
+  });
+
+  const versionQuestionnaire = removeEmpty({
+    ...questionnaire,
+    updatedAt,
+  });
+
+  try {
+    let collection = dbo.collection("questionnaires");
+    await collection.insertOne(baseQuestionnaire);
+    collection = dbo.collection("versions");
+    await collection.insertOne(versionQuestionnaire);
+  } catch (error) {
+    logger.error(`Unable to create questionnaire with ID: ${id}`);
+    logger.error(error);
+  }
+
+  logger.info(`Created questionnaire with ID: ${id}`);
+
+  return {
+    ...questionnaire,
+    updatedAt,
+  };
+};
+
+const getQuestionnaire = async id => {
+  try {
+    const collection = dbo.collection("versions");
+    const questionnaire = await collection.findOne(
+      { id: id },
+      { sort: { createdAt: -1 } }
+    );
+
+    if (!questionnaire) {
+      logger.info("No questionnaires found");
+      return null;
+    }
+    if (!questionnaire.sections) {
+      questionnaire.sections = [];
+    }
+
+    if (!questionnaire.metadata) {
+      questionnaire.metadata = [];
+    }
+
+    if (!questionnaire.editors) {
+      questionnaire.editors = [];
+    }
+    return questionnaire;
+  } catch (err) {
+    logger.error(
+      `Unable to get latest version of questionnaire with ID: ${id}`
+    );
+    logger.error(err);
+    return err;
+  }
+};
+
+const getQuestionnaireMetaById = async id => {
+  try {
+    const collection = dbo.collection("questionnaires");
+    const questionnaire = await collection.findOne({ id: id });
+
+    if (!questionnaire) {
+      logger.info("No base questionnaire found");
+      return null;
+    }
+
+    const updatedQuestionnaire = {
+      ...questionnaire,
+      history: questionnaire.history.map(historyItem => ({
+        ...historyItem,
+        time: historyItem.time.toDate(),
+      })),
+      updatedAt: questionnaire.updatedAt.toDate(),
+      createdAt: questionnaire.createdAt.toDate(),
+    };
+    return updatedQuestionnaire;
+  } catch (error) {
+    logger.error(`Error getting base questionnaire with ID ${id}`);
+    logger.error(error);
+    return;
+  }
+};
+
+const saveQuestionnaire = async changedQuestionnaire => {
+  const { id } = changedQuestionnaire;
+
+  try {
+    if (!id) {
+      throw new Error(
+        "Unable to save questionnaire; cannot find required field: ID"
+      );
+    }
+
+    const createdAt = new Date();
+    const updatedAt = createdAt;
+
+    const originalQuestionnaire = await getQuestionnaire(id);
+
+    const updatedQuestionnaire = removeEmpty({
+      ...originalQuestionnaire,
+      ...changedQuestionnaire,
+    });
+
+    let collection = dbo.collection("questionnaires");
+    await collection.updateOne(
+      { id: id },
+      { $set: { ...justListFields(updatedQuestionnaire), updatedAt } },
+      { upsert: true }
+    );
+
+    delete updatedQuestionnaire._id;
+    collection = dbo.collection("versions");
+    await collection.insertOne({
+      ...updatedQuestionnaire,
+      updatedAt,
+      createdAt,
+    });
+
+    return { ...updatedQuestionnaire, updatedAt };
+  } catch (error) {
+    logger.error(`Error updating questionnaire with ID ${id}`);
+    logger.error(error);
+    return;
+  }
+};
+
+const deleteQuestionnaire = async id => {
+  try {
+    const collection = dbo.collection("questionnaires");
+    await collection.deleteOne({ id: id });
+    return;
+  } catch (error) {
+    logger.error(`Unable to delete questionnaire with ID: ${id}`);
+    logger.error(error);
+    return;
+  }
+};
+
+const createUser = async user => {
+  let { id, name, email } = user;
+
+  try {
+    if (!email) {
+      throw new Error("Cannot create new user without required field: email");
+    }
+
+    if (!id) {
+      id = uuidv4();
+    }
+
+    if (!name) {
+      name = email;
+    }
+
+    const collection = dbo.collection("users");
+    await collection.insertOne({ ...user, id, name, email });
+  } catch (error) {
+    logger.error(`Error creating user with ID: ${id}: `);
+    logger.error(error);
+    return;
+  }
+
+  return { ...user, id, name };
+};
+
+const getUserByExternalId = async externalId => {
+  try {
+    if (!externalId) {
+      throw new Error("Cannot find user without required field 'externalId'");
+    }
+
+    const collection = dbo.collection("users");
+    const user = await collection.findOne({ externalId: externalId });
+
+    if (!user) {
+      logger.info(`User with external ID ${externalId} not found`);
+      return;
+    }
+
+    return { ...user, id: user.id };
+  } catch (error) {
+    logger.error(`Unable to find user with external ID ${externalId}`);
+    logger.error(error);
+    return;
+  }
+};
+
+const getUserById = async id => {
+  try {
+    const collection = dbo.collection("users");
+    const user = await collection.findOne({ id: id });
+
+    if (!user) {
+      logger.info("No users found");
+      return;
+    }
+
+    return { ...user, id: user.id };
+  } catch (error) {
+    logger.error(`Error getting user with ID: ${id}`);
+    logger.error(error);
+    return error;
+  }
+};
+
+const listQuestionnaires = async () => {
+  try {
+    const collection = dbo.collection("questionnaires");
+    const questionnaires = await collection.find().toArray();
+
+    if (!questionnaires) {
+      logger.info("No questionnaires found");
+      return [];
+    }
+
+    const transformedQuestionnaires = questionnaires
+      .map(q => ({ ...q, editors: q.editors || [] }))
+      .sort((a, b) => (a.createdAt > b.createdAt ? -1 : 1));
+
+    return transformedQuestionnaires;
+  } catch (error) {
+    logger.error("Unable to retrieve questionnaires");
+    logger.error(error);
+    return;
+  }
+};
+
+const createComments = async questionnaireId => {
+  try {
+    if (!questionnaireId) {
+      throw new Error(
+        "Cannot create default comments without required paramater: questionnaireId"
+      );
+    }
+    const defaultComments = { questionnaireId, comments: {} };
+    const collection = dbo.collection("comments");
+    await collection.insertOne(defaultComments);
+    return defaultComments;
+  } catch (error) {
+    logger.error(
+      `Unable to create comment structure for questionnaire with ID ${questionnaireId}`
+    );
+    logger.error(error);
+    return error;
+  }
+};
+
+const listUsers = async () => {
+  try {
+    const collection = dbo.collection("users");
+    const users = collection.find().toArray();
+    if (!collection) {
+      logger.info("No users found");
+      return [];
+    }
+
+    return users;
+  } catch (error) {
+    logger.error(`Unable to get a list of all users`);
+    logger.error(error);
+    return error;
+  }
+};
+
+const saveMetadata = async metadata => {
+  const { id } = metadata;
+
+  if (!id) {
+    throw new Error(
+      "Cannot find required field ID within the given base questionnaire"
+    );
+  }
+
+  try {
+    const updatedMetadata = { ...metadata, updatedAt: new Date() };
+    const collection = dbo.collection("questionnaires");
+    await collection.updateOne(
+      { id: id },
+      { $set: updatedMetadata },
+      { upsert: true }
+    );
+    return updatedMetadata;
+  } catch (error) {
+    logger.error(`Cannot update base questionnaire with ID ${id}`);
+    logger.error(error);
+    return error;
+  }
+};
+
+const createHistoryEvent = async (qid, event) => {
+  try {
+    if (!qid) {
+      throw new Error(
+        "Unable to create a history event without first parameter: qid"
+      );
+    }
+
+    if (!event) {
+      throw new Error(
+        "Unable to create history event without second paramater: event"
+      );
+    }
+    const questionnaire = await getQuestionnaireMetaById(qid);
+    questionnaire.history.unshift(event);
+
+    const collection = dbo.collection("questionnaires");
+    await collection.updateOne(
+      { id: id },
+      { $set: { history: questionnaire.history } },
+      { upsert: true }
+    );
+
+    return questionnaire.history;
+  } catch (error) {
+    logger.error(
+      `Error creating history event in questionnaire with ID ${qid}`
+    );
+    logger.error(error);
+    return error;
+  }
+};
+
+const updateUser = async changedUser => {
+  const { id } = changedUser;
+  try {
+    if (!id) {
+      throw new Error("Cannot update user without required field: id");
+    }
+    const collection = dbo.collection("users");
+    const existingUser = await collection.findOne({ id: id });
+
+    const user = Object.assign(changedUser, existingUser);
+    await collection.updateOne(
+      { id: id },
+      { $set: { ...user } },
+      { upsert: true }
+    );
+    return user;
+  } catch (error) {
+    logger.error(`Unable update user with ID ${id}`);
+    logger.error(error);
+    return error;
+  }
+};
+
+const saveComments = async updatedCommentsObject => {
+  const { questionnaireId, comments: updatedComments } = updatedCommentsObject;
+  try {
+    if (!questionnaireId) {
+      throw new Error(
+        "Unable to save comments without required field: questionnaireId"
+      );
+    }
+
+    const collection = dbo.collection("comments");
+    await collection.updateOne(
+      { questionnaireId: questionnaireId },
+      { $set: { comments: { ...updatedComments } } },
+      { upsert: true }
+    );
+
+    return updatedComments;
+  } catch (error) {
+    logger.error(
+      `Unable to save comments for questionnaire with ID ${questionnaireId}`
+    );
+    logger.error(error);
+    return error;
+  }
+};
+
+const getCommentsForQuestionnaire = async questionnaireId => {
+  try {
+    const collection = dbo.collection("comments");
+    const comments = await collection.findOne({
+      questionnaireId: questionnaireId,
+    });
+
+    const data = comments;
+
+    const listOfComponents = Object.keys(data.comments);
+    listOfComponents.forEach(component => {
+      const componentComments = data.comments[component];
+      data.comments[component] = componentComments.map(comment => {
+        let editedTime;
+        if (comment.editedTime) {
+          editedTime = comment.editedTime;
+        } else {
+          editedTime = null;
+        }
+
+        const replies = comment.replies.map(reply => {
+          let editedTime;
+          if (reply.editedTime) {
+            editedTime = reply.editedTime;
+          } else {
+            editedTime = null;
+          }
+
+          return {
+            ...reply,
+            editedTime,
+            createdTime: reply.createdTime,
+          };
+        });
+
+        return {
+          ...comment,
+          replies,
+          editedTime,
+          createdTime: comment.createdTime,
+        };
+      });
+    });
+    return data;
+  } catch (error) {
+    logger.error(
+      `Unable to get comments for questionnaire with ID ${questionnaireId}`
+    );
+    logger.error(error);
+    return error;
+  }
+};
+
+module.exports = {
+  createQuestionnaire,
+  saveQuestionnaire,
+  deleteQuestionnaire,
+  listQuestionnaires,
+  getQuestionnaire,
+  getQuestionnaireMetaById,
+  createComments,
+  createUser,
+  getUserByExternalId,
+  getUserById,
+  listUsers,
+  createHistoryEvent,
+  saveMetadata,
+  getCommentsForQuestionnaire,
+  saveComments,
+  updateUser,
+};
