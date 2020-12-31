a9f90d764812ee6aee61dcf5d0d411785e133f69
Thomas Baggaley <tbaggaley@pm.me>
2020-12-31 17:19:07 +0000

    ♻️ Refactor SkipLogicRoute
    In the modern style

--- eq-author/src/App/page/Logic/SkipLogic/index.js
+++ eq-author/src/App/page/Logic/SkipLogic/index.js
@@ -1,10 +1,6 @@
-/* eslint-disable import/no-unresolved */
 import React from "react";
 import gql from "graphql-tag";
-import { Query } from "react-apollo";
-import { Redirect } from "react-router-dom";
-
-import { get } from "lodash";
+import { useQuery } from "@apollo/react-hooks";
 import PropTypes from "prop-types";
 
 import Loading from "components/Loading";
@@ -12,85 +8,42 @@ import Error from "components/Error";
 
 import SkipLogicPage from "./SkipLogicPage";
 import transformNestedFragments from "utils/transformNestedFragments";
-import { buildPagePath } from "utils/UrlUtils";
 import Logic from "App/shared/Logic";
-const ROUTING_PAGE_TYPES = ["QuestionPage"];
 
-export class UnwrappedQuestionSkipLogicRoute extends React.Component {
-  static propTypes = {
-    data: PropTypes.object, // eslint-disable-line
-    loading: PropTypes.bool.isRequired,
-    error: PropTypes.object, // eslint-disable-line
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        questionnaireId: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  };
+export const SkipLogicRoute = ({ match }) => {
+  const { loading, data } = useQuery(SKIPLOGIC_QUERY, {
+    variables: {
+      input: match.params,
+    },
+  });
 
-  renderContent() {
-    const { data, loading, error, match } = this.props;
+  const page = data?.page;
 
-    if (loading) {
-      return <Loading height="20em">Loading skip logic</Loading>;
-    }
+  return (
+    <Logic>
+      {page ? (
+        <SkipLogicPage page={page} />
+      ) : loading ? (
+        <Loading height="20em"> Loading skip logic </Loading>
+      ) : (
+        <Error> Something went wrong </Error>
+      )}
+    </Logic>
+  );
+};
 
-    const page = get(data, "page");
-
-    if (error || !page) {
-      return <Error>Something went wrong</Error>;
-    }
-
-    if (!ROUTING_PAGE_TYPES.includes(page.pageType)) {
-      return (
-        <Redirect
-          to={buildPagePath({
-            questionnaireId: match.params.questionnaireId,
-            pageId: page.id,
-          })}
-        />
-      );
-    }
-
-    return <SkipLogicPage page={page} />;
-  }
-
-  render() {
-    return <Logic {...this.props}>{this.renderContent()}</Logic>;
-  }
-}
-
-const query = gql`
-  query GetRouting($input: QueryInput!) {
-    page(input: $input) {
-      id
-      displayName
-      pageType
-      ...SkipLogicPage
-      section {
-        id
-        position
+const SKIPLOGIC_QUERY = transformNestedFragments(
+  gql`
+    query GetSkipLogic($input: QueryInput!) {
+      page(input: $input) {
+        ...SkipLogicPage
       }
     }
-  }
-`;
-const SKIPLOGIC_QUERY = transformNestedFragments(
-  query,
+  `,
   SkipLogicPage.fragments
 );
 
-const QueryingRoute = props => (
-  <Query
-    query={SKIPLOGIC_QUERY}
-    variables={{ input: props.match.params }}
-    fetchPolicy="cache-and-network"
-  >
-    {innerProps => (
-      <UnwrappedQuestionSkipLogicRoute {...innerProps} {...props} />
-    )}
-  </Query>
-);
-QueryingRoute.propTypes = {
+SkipLogicRoute.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
       pageId: PropTypes.string.isRequired,
@@ -98,4 +51,4 @@ QueryingRoute.propTypes = {
   }).isRequired,
 };
 
-export default QueryingRoute;
+export default SkipLogicRoute;
