f2139af2fb477976b8a119b2f9d5d4e9b76240c8
Thomas Baggaley <tbaggaley@pm.me>
2021-01-07 17:09:57 +0000

    🛠 Fix Skip/Routing pages not showing titles

--- eq-author/src/App/questionConfirmation/Logic/Routing/index.js
+++ eq-author/src/App/questionConfirmation/Logic/Routing/index.js
@@ -4,14 +4,27 @@ import NoRouting, {
   Title,
   Paragraph,
 } from "App/shared/Logic/Routing/NoRouting";
+import GET_PAGE_QUERY from "./fragment.graphql";
 
-export default () => (
-  <Logic>
-    <NoRouting disabled>
-      <Title> Routing logic not available for confirmation questions </Title>
-      <Paragraph>
-        The route will be based on the answer to the previous question.
-      </Paragraph>
-    </NoRouting>
-  </Logic>
-);
+import { useQuery } from "@apollo/react-hooks";
+
+export default ({ match }) => {
+  const { data } = useQuery(GET_PAGE_QUERY, {
+    variables: {
+      id: match.params.confirmationId,
+    },
+  });
+
+  const page = data?.questionConfirmation;
+
+  return (
+    <Logic page={page}>
+      <NoRouting disabled>
+        <Title> Routing logic not available for confirmation questions </Title>
+        <Paragraph>
+          The route will be based on the answer to the previous question.
+        </Paragraph>
+      </NoRouting>
+    </Logic>
+  );
+};
