bff8bb5148fb94ba459ebb842a39c92868960fd3
Tom Baggaley <tbaggaley@pm.me>
2021-02-12 16:37:56 +0000

    ♻ displayName resolver: just use cheerio once
    Calls to cheerio are expensive with many titles to parse - code was
    unnecessarily calling it twice via isHtml and then stripHTML
    
    Once is plenty

--- eq-author-api/utils/html.js
+++ eq-author-api/utils/html.js
@@ -1,12 +1,8 @@
 const cheerio = require("cheerio");
-const { isNull, toString, trim } = require("lodash");
 
-const isHtml = value => !isNull(cheerio(trim(value)).html());
-
-const stripTags = value =>
-  isHtml(toString(value)) ? cheerio(value).text() : value;
+const stripTags = (value) =>
+  typeof value === "string" ? cheerio(value).text() : value;
 
 module.exports = {
-  isHtml,
   stripTags,
 };
