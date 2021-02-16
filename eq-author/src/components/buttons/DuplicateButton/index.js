c7b9178a4c518426c7915bbde51b116d5224ebb8
Matthew Elcock <mattiose@outlook.com>
2021-02-16 14:07:35 +0000

    Added tooltip to duplicate questionnaire button

--- eq-author/src/components/buttons/DuplicateButton/index.js
+++ eq-author/src/components/buttons/DuplicateButton/index.js
@@ -4,15 +4,48 @@ import PropTypes from "prop-types";
 import IconCopy from "assets/icon-copy.svg?inline";
 import IconText from "components/IconText";
 import Button from "components/buttons/Button";
+import Tooltip from "components/Forms/Tooltip";
 
-const DuplicateButton = ({ onClick, hideText, children, ...props }) => {
+const ToolTip = ({ children }) => (
+  <Tooltip content="Duplicate" place="top" offset={{ top: 0, bottom: 10 }}>
+    {children}
+  </Tooltip>
+);
+
+const DuplicateButton = ({ onClick, hideText, children, ...props }) => (
+  <Button onClick={onClick} variant="tertiary" small {...props}>
+    <IconText icon={IconCopy} hideText={hideText}>
+      {children || "Duplicate"}
+    </IconText>
+  </Button>
+);
+
+const Component = ({ onClick, hideText, children, ...props }) => {
+  if (hideText) {
+    return (
+      <ToolTip>
+        <DuplicateButton onClick={onClick} hideText={hideText} {...props}>
+          {children}
+        </DuplicateButton>
+      </ToolTip>
+    );
+  }
+
   return (
-    <Button onClick={onClick} variant="tertiary" small {...props}>
-      <IconText icon={IconCopy} hideText={hideText}>
-        {children || "Duplicate"}
-      </IconText>
-    </Button>
+    <DuplicateButton onClick={onClick} hideText={hideText} {...props}>
+      {children}
+    </DuplicateButton>
   );
+};
+
+ToolTip.propTypes = {
+  children: PropTypes.node,
+};
+
+Component.propTypes = {
+  children: PropTypes.node,
+  onClick: PropTypes.func.isRequired,
+  hideText: PropTypes.bool,
 };
 
 DuplicateButton.propTypes = {
@@ -21,4 +54,4 @@ DuplicateButton.propTypes = {
   hideText: PropTypes.bool,
 };
 
-export default DuplicateButton;
+export default Component;
