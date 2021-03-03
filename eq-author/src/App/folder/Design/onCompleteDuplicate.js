f9b4976ab5a3a785e009489bffd95cfd7b5342b7
farres1 <sam.farrell@ons.gov.uk>
2021-03-03 13:07:18 +0000

    Display new folder when folder is duplicated

--- eq-author/src/App/folder/Design/onCompleteDuplicate.js
+++ eq-author/src/App/folder/Design/onCompleteDuplicate.js
@@ -1,28 +1,13 @@
-import { buildPagePath, buildSectionPath } from "utils/UrlUtils";
+import { buildFolderPath } from "utils/UrlUtils";
 
-const onCompleteDuplicate = (
-  response,
-  history,
-  questionnaireId,
-  sectionId,
-  folderPosition
-) => {
-  const section = response.duplicateFolder.section;
+const onCompleteDuplicate = (response, history, questionnaireId) => {
+  const { id } = response.duplicateFolder;
 
-  const pages = section.folders.flatMap(({ pages }) => pages);
-
-  const nextPage = pages[folderPosition + 1];
-
-  const buildPath =
-    folderPosition === 0
-      ? buildSectionPath({
-          questionnaireId: questionnaireId,
-          sectionId: sectionId,
-        })
-      : buildPagePath({
-          questionnaireId: questionnaireId,
-          pageId: nextPage.id,
-        });
+  const buildPath = buildFolderPath({
+    questionnaireId: questionnaireId,
+    folderId: id,
+    tab: "design",
+  });
 
   history.push(buildPath);
 };
