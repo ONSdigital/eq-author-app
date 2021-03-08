30c5618429a18406611a66286e12886fb5865197
thomas.mcauliffe <tjamesmac@gmail.com>
2021-03-08 14:56:17 +0000

    refactor: to rtl

--- eq-author/src/App/QuestionnaireDesignPage/index.test.js
+++ eq-author/src/App/QuestionnaireDesignPage/index.test.js
@@ -1,79 +1,63 @@
 import React from "react";
-import { Query, Subscription } from "react-apollo";
-import { shallow, mount } from "enzyme";
+import { render } from "tests/utils/rtl";
 import { buildQuestionnaire } from "tests/utils/createMockQuestionnaire";
+import { useQuery } from "@apollo/react-hooks";
 import {
   organiseAnswers,
   flattenAnswers,
   duplicatesAnswers,
 } from "utils/getAllAnswersFlatMap";
 
-import {
-  SECTION,
-  PAGE,
-  QUESTION_CONFIRMATION,
-  INTRODUCTION,
-  FOLDER,
-} from "constants/entities";
-import {
-  ERR_PAGE_NOT_FOUND,
-  ERR_UNAUTHORIZED_QUESTIONNAIRE,
-} from "constants/error-codes";
+import QuestionnaireContext from "components/QuestionnaireContext";
+import { MeContext } from "App/MeContext";
 
-import NavigationSidebar from "./NavigationSidebar";
+import { PAGE } from "constants/entities";
 
-import {
-  UnwrappedQuestionnaireDesignPage as QuestionnaireDesignPage,
-  withAuthCheck,
-  withValidations,
-  withQuestionnaire,
-} from "./";
+import { UnwrappedQuestionnaireDesignPage as QuestionnaireDesignPage } from "./";
+
+jest.mock("components/BaseLayout/PermissionsBanner", () => ({
+  __esModule: true,
+  default: () => <></>,
+}));
+
+jest.mock("components/EditorLayout/Header", () => ({
+  __esModule: true,
+  default: () => <></>,
+}));
+
+jest.mock("@apollo/react-hooks", () => ({
+  __esModule: true,
+  ...jest.requireActual("@apollo/react-hooks"),
+  useSubscription: jest.fn(),
+  useQuery: jest.fn(),
+}));
 
 describe("QuestionnaireDesignPage", () => {
-  let mockHandlers;
-  let wrapper;
-  let match;
-  let confirmation,
-    page,
-    folder,
-    section,
-    questionnaire,
-    validations,
-    sectionsForFlatAnswers,
-    answerList,
-    flatAnswersTest,
-    duplicateTest;
+  let sectionsForFlatAnswers, answerList, flatAnswersTest, duplicateTest;
 
-  beforeEach(() => {
-    questionnaire = buildQuestionnaire();
-    section = questionnaire.sections[0];
-    folder = questionnaire.sections[0].folders[0];
-    page = questionnaire.sections[0].folders[0].pages[0];
-
-    confirmation = {
-      id: "4",
-      title: "Confirmation",
-    };
-
-    validations = {
+  const defaultSetup = (changes = {}) => {
+    const questionnaire = buildQuestionnaire();
+    questionnaire.totalErrorCount = 0;
+    const page = questionnaire.sections[0].folders[0].pages[0];
+    useQuery.mockImplementation(() => ({ loading: false, data: { page } }));
+    const validations = {
       id: "3",
       errorCount: 0,
       pages: [],
     };
 
-    mockHandlers = {
-      onUpdateSection: jest.fn(),
-      onAddQuestionPage: jest.fn(),
-      onAddSection: jest.fn(),
-      onAddFolder: jest.fn(),
-      onUpdatePage: jest.fn(),
-      onDeletePage: jest.fn(),
-      onDeleteSection: jest.fn(),
-      onCreateQuestionConfirmation: jest.fn(),
-      onAddCalculatedSummaryPage: jest.fn(),
+    const user = {
+      id: "123",
+      displayName: "Batman",
+      name: "Bruce",
+      email: "IAmBatman@dccomics.com",
+      __typename: "User",
+      picture: "",
+      admin: true,
+      signOut: jest.fn(),
     };
 
-    match = {
+    const match = {
       params: {
         questionnaireId: questionnaire.id,
         entityName: PAGE,
@@ -81,414 +65,52 @@ describe("QuestionnaireDesignPage", () => {
       },
     };
 
-    wrapper = shallow(
-      <QuestionnaireDesignPage
-        {...mockHandlers}
-        match={match}
-        questionnaire={questionnaire}
-        validations={validations}
-        loading={false}
-      />
+    const props = {
+      questionnaire,
+      validations,
+      match,
+      loading: false,
+      ...changes,
+    };
+
+    const signOut = jest.fn();
+
+    const utils = render(
+      <MeContext.Provider value={{ me: user, signOut }}>
+        <QuestionnaireContext.Provider value={questionnaire}>
+          <QuestionnaireDesignPage {...props} />
+        </QuestionnaireContext.Provider>
+      </MeContext.Provider>,
+      {
+        route: `/q/${questionnaire.id}/page/${page.id}/design`,
+        urlParamMatcher: "/q/:questionnaireId/page/:pageId/:tab",
+      }
     );
-  });
+    return { ...utils };
+  };
 
   it("should render", () => {
-    expect(wrapper).toMatchSnapshot();
+    const { getByTestId } = defaultSetup();
+    expect(getByTestId("question-page-editor")).toBeVisible();
+    expect(getByTestId("main-navigation")).toBeVisible();
+    expect(getByTestId("side-nav")).toBeVisible();
   });
 
-  it("should throw an error for invalid entity types", () => {
-    wrapper.setProps({
-      match: {
-        params: {
-          questionnaireId: questionnaire.id,
-          entityName: "invalid",
-        },
-      },
-    });
+  it("should render a loading state", () => {
+    const { getByText } = defaultSetup({ loading: true });
 
-    expect(() =>
-      wrapper.find(NavigationSidebar).simulate("addQuestionPage")
-    ).toThrow();
+    expect(getByText("Loading questionnaire…")).toBeVisible();
   });
 
-  describe("onIntroductionPage", () => {
-    beforeEach(() => {
-      wrapper.setProps({
-        questionnaire: {
-          ...questionnaire,
-          introduction: {
-            id: "1",
-          },
-        },
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: INTRODUCTION,
-          },
-        },
-      });
-    });
-
-    it("should disable adding question page when on introduction page", () => {
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionPage")
-      ).toEqual(false);
-    });
-
-    it("should disable adding folder page when on introduction page", () => {
-      expect(wrapper.find(NavigationSidebar).prop("canAddFolder")).toEqual(
-        false
-      );
-    });
-
-    it("should disable adding confirmation question when on introduction page", () => {
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionConfirmation")
-      ).toEqual(false);
-    });
-
-    it("should disable adding calculated summary when on introduction page", () => {
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddCalculatedSummaryPage")
-      ).toEqual(false);
-    });
-  });
-
-  describe("onAddFolder", () => {
-    it("Should be able to add a folder at the start when on a section", () => {
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: SECTION,
-            entityId: section.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addFolder");
-
-      expect(mockHandlers.onAddFolder).toHaveBeenCalledWith(section.id, 0);
-    });
-
-    it("Should be able to add a folder below the current folder", () => {
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: FOLDER,
-            entityId: folder.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addFolder");
-
-      expect(mockHandlers.onAddFolder).toHaveBeenCalledWith(
-        section.id,
-        folder.position + 1
-      );
-    });
-
-    it("Should be able to add a folder below the current page", () => {
-      wrapper.find(NavigationSidebar).simulate("addFolder");
-
-      expect(mockHandlers.onAddFolder).toHaveBeenCalledWith(
-        section.id,
-        page.position + 1
-      );
-    });
-
-    it("Should be able to add a folder below the current confirmation page", () => {
-      page.confirmation = confirmation;
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: QUESTION_CONFIRMATION,
-            entityId: confirmation.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addFolder");
-
-      expect(mockHandlers.onAddFolder).toHaveBeenCalledWith(
-        section.id,
-        page.position + 1
-      );
-    });
-
-    it("Throws when it doesn't recognise the current entity", () => {
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: "BeBe Zahara Benet",
-            entityId: folder.id,
-          },
-        },
-      });
-
-      expect(() =>
-        wrapper.find(NavigationSidebar).simulate("addFolder")
-      ).toThrow();
-    });
-  });
-
-  describe("onAddQuestionPage", () => {
-    it("should add new page below current page", () => {
-      wrapper.find(NavigationSidebar).simulate("addQuestionPage");
-
-      expect(mockHandlers.onAddQuestionPage).toHaveBeenCalledWith(
-        section.id,
-        page.position + 1
-      );
-    });
-
-    it("should be able to add a page at the start when on a section", () => {
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: SECTION,
-            entityId: section.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addQuestionPage");
-
-      expect(mockHandlers.onAddQuestionPage).toHaveBeenCalledWith(
-        section.id,
-        0
-      );
-    });
-
-    it("should be able to add a page after the confirmation when on a confirmation page", () => {
-      page.confirmation = confirmation;
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: QUESTION_CONFIRMATION,
-            entityId: confirmation.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addQuestionPage");
-
-      expect(mockHandlers.onAddQuestionPage).toHaveBeenCalledWith(
-        section.id,
-        page.position + 1
-      );
-    });
-  });
-
-  describe("onAddCalculatedSummaryPage", () => {
-    it("should add new page below current page", () => {
-      wrapper.find(NavigationSidebar).simulate("addCalculatedSummaryPage");
-
-      expect(mockHandlers.onAddCalculatedSummaryPage).toHaveBeenCalledWith(
-        section.id,
-        page.position + 1
-      );
-    });
-
-    it("should be able to add a page at the start when on a section", () => {
-      wrapper.setProps({
-        match: {
-          params: {
-            questionnaireId: questionnaire.id,
-            entityName: SECTION,
-            entityId: section.id,
-          },
-        },
-      });
-
-      wrapper.find(NavigationSidebar).simulate("addCalculatedSummaryPage");
-
-      expect(mockHandlers.onAddCalculatedSummaryPage).toHaveBeenCalledWith(
-        section.id,
-        0
-      );
-    });
-  });
-
-  describe("getTitle", () => {
+  describe("Document title", () => {
     it("should display existing title if loading", () => {
-      wrapper.setProps({ loading: true });
-
-      const getTitle = wrapper
-        .findWhere((n) => n.name() === "GetContext")
-        .props().title;
-
-      expect(getTitle()).toEqual("");
+      defaultSetup({ loading: true });
+      expect(document.title).toEqual("- Page 1.1.1");
     });
 
     it("should display questionnaire title if no longer loading", () => {
-      const getTitle = wrapper
-        .findWhere((n) => n.name() === "GetContext")
-        .props().title;
-
-      expect(getTitle()).toEqual(questionnaire.title);
-    });
-  });
-
-  describe("Adding question confirmation", () => {
-    it("should call create a question confirmation when addQuestionPage is called", () => {
-      wrapper
-        .find(`[data-test="side-nav"]`)
-        .simulate("addQuestionConfirmation");
-      expect(mockHandlers.onCreateQuestionConfirmation).toHaveBeenCalledWith(
-        page.id
-      );
-    });
-
-    it("should disable adding confirmation page when the question page already has one", () => {
-      questionnaire.sections[0].folders[0].pages[0].confirmation = {
-        id: 1,
-      };
-      wrapper.setProps({ questionnaire });
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionConfirmation")
-      ).toEqual(false);
-    });
-
-    it("should disable adding question confirmation when not on a page", () => {
-      match.params.entityName = "foo";
-      wrapper.setProps({ match });
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionConfirmation")
-      ).toEqual(false);
-    });
-
-    it("should disable adding question confirmation when not on a question page", () => {
-      questionnaire.sections[0].folders[0].pages[0] = {
-        id: "1",
-        title: "",
-        pageType: "NotQuestionPage",
-        position: 0,
-      };
-      wrapper.setProps({ questionnaire });
-      expect(wrapper.find(NavigationSidebar).props()).toMatchObject({
-        canAddQuestionConfirmation: false,
-      });
-    });
-
-    it("should disable adding question confirmation when the page cannot be found", () => {
-      match.params.entityId = "hello";
-      wrapper.setProps({ match });
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionConfirmation")
-      ).toEqual(false);
-    });
-
-    it("should disable adding question confirmation, question page & calculated summary whilst loading", () => {
-      wrapper.setProps({
-        loading: true,
-        questionnaire: null,
-      });
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionPage")
-      ).toEqual(false);
-
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddCalculatedSummaryPage")
-      ).toEqual(false);
-
-      expect(
-        wrapper.find(NavigationSidebar).prop("canAddQuestionConfirmation")
-      ).toEqual(false);
-    });
-
-    it("should trigger PAGE_NOT_FOUND error if no question data available after loading finished", () => {
-      const throwWrapper = () => {
-        wrapper.setProps({
-          loading: false,
-          questionnaire: null,
-        });
-      };
-
-      expect(throwWrapper).toThrow(new Error(ERR_PAGE_NOT_FOUND));
-    });
-
-    describe("withAuthCheck", () => {
-      it("should throw ERR_UNAUTHORIZED_QUESTIONNAIRE if access denied", () => {
-        const props = {
-          error: {
-            networkError: {
-              bodyText: ERR_UNAUTHORIZED_QUESTIONNAIRE,
-            },
-          },
-        };
-
-        const Component = withAuthCheck(() => <h1>hello</h1>);
-
-        expect(() => shallow(<Component {...props} />)).toThrow(
-          new Error(ERR_UNAUTHORIZED_QUESTIONNAIRE)
-        );
-      });
-      it("should render questionnaire design page if access granted", () => {
-        const Component = withAuthCheck(() => <h1>hello</h1>);
-        const wrapper = mount(<Component />);
-        expect(wrapper.find("h1")).toHaveLength(1);
-      });
-    });
-
-    describe("withValidations", () => {
-      let match;
-      beforeEach(() => {
-        match = {
-          params: {
-            questionnaireId: "qId",
-          },
-        };
-      });
-      it("should render the component with the validations from the subscription", () => {
-        const Component = withValidations(() => <hr />);
-        const renderFunc = shallow(<Component match={match} />)
-          .find(Subscription)
-          .renderProp("children");
-        const wrapper = renderFunc({
-          data: { validationUpdated: "validations" },
-        });
-        expect(wrapper).toMatchSnapshot();
-      });
-      it("should call the subscription with the questionnaire id", () => {
-        const Component = withValidations(() => <hr />);
-        const wrapper = shallow(<Component match={match} />);
-        expect(wrapper).toMatchSnapshot();
-      });
-    });
-
-    describe("withQuestionnaire", () => {
-      let match;
-      beforeEach(() => {
-        match = {
-          params: {
-            questionnaireId: "qId",
-          },
-        };
-      });
-      it("should render the component with the questionnaire", () => {
-        const Component = withQuestionnaire(() => <hr />);
-        const renderFunc = shallow(<Component match={match} />)
-          .find(Query)
-          .renderProp("children");
-        const wrapper = renderFunc({
-          loading: false,
-          error: null,
-          data: { questionnaire: "validations" },
-        });
-        expect(wrapper).toMatchSnapshot();
-      });
-      it("should call the query with the questionnaire id", () => {
-        const Component = withQuestionnaire(() => <hr />);
-        const wrapper = shallow(<Component match={match} />);
-        expect(wrapper).toMatchSnapshot();
-      });
+      defaultSetup();
+      expect(document.title).toEqual("questionnaire - Page 1.1.1");
     });
   });
 
