829d9dffa1ade966fa5ffaa9080d6b998cd37215
thomas.mcauliffe <tjamesmac@gmail.com>
2021-03-09 09:49:21 +0000

    borked test

--- eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/NavigationHeader.test.js
+++ eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/NavigationHeader.test.js
@@ -1,77 +1,63 @@
 import React from "react";
-import { render, fireEvent } from "tests/utils/rtl";
-import { useParams } from "react-router-dom";
+import { render, fireEvent, waitFor, screen } from "tests/utils/rtl";
+// import userEvent from "@testing-library/user-event";
 
 import { buildQuestionnaire } from "tests/utils/createMockQuestionnaire";
 
-import { CalculatedSummaryPage } from "constants/page-types";
+import QuestionnaireContext from "components/QuestionnaireContext";
 
 import NavigationHeader from "./NavigationHeader";
+
+jest.mock("components/NavigationCallbacks", () => ({
+  useNavigationCallbacks: () => ({
+    onAddQuestionPage: null,
+    onAddFolder: jest.fn(),
+    onAddCalculatedSummaryPage: jest.fn(),
+  }),
+}));
+
+// jest.mock("react-router-dom", () => ({
+//   useParams: () => ({ entityName: "something" }),
+// }));
 
 function defaultSetup(changes) {
   const props = {
     ...changes,
   };
-  const utils = render(<NavigationHeader {...props} />);
+  const questionnaire = buildQuestionnaire();
+  const utils = render(
+    <QuestionnaireContext.Provider value={questionnaire}>
+      <NavigationHeader {...props} />
+    </QuestionnaireContext.Provider>
+  );
 
   return { ...utils };
 }
 
-// import { UnwrappedNavigationHeader as NavigationHeader } from "./NavigationHeader";
+function openSetup() {
+  const utils = defaultSetup();
+  const menuButton = "btn-add-menu";
+  fireEvent.click(utils.getByTestId(menuButton));
+  return { ...utils };
+}
+
 describe("NavigationHeader", () => {
   it("should render", () => {
     const { getByTestId } = defaultSetup();
-    expect(getByTestId("add-menu")).toBeVisible();
+    expect(getByTestId("btn-add-menu")).toBeVisible();
+  });
+
+  it("should open up menu", () => {
+    const { queryByTestId } = defaultSetup();
+    expect(queryByTestId("addmenu-window")).toBeNull();
+    fireEvent.click(queryByTestId("btn-add-menu"));
+    expect(queryByTestId("addmenu-window")).toBeVisible();
+  });
+
+  it("should close after firing", async () => {
+    const { queryByTestId, debug } = openSetup();
+    fireEvent.click(queryByTestId("btn-add-question-page"));
+    debug();
+    await waitFor(expect(screen.queryByTestId("addmenu-window")).toBeNull());
   });
 });
-// describe("NavigationHeader", () => {
-//   const mockHandlers = {
-//     onAddQuestionPage: jest.fn(),
-//     onAddSection: jest.fn(),
-//     onAddQuestionConfirmation: jest.fn(),
-//     onAddCalculatedSummaryPage: jest.fn(),
-//   };
-//   const props = {
-//     questionnaire: {},
-//     canAddQuestionPage: true,
-//     canAddCalculatedSummaryPage: true,
-//     canAddQuestionConfirmation: true,
-//     onUpdateQuestionnaire: jest.fn(),
-//     onCreateQuestionConfirmation: jest.fn(),
-//     match: { params: { questionnaireId: "1" } },
-//     me: { id: "123", email: "j@h.com", admin: true },
-//     ...mockHandlers,
-//   };
-//   const createWrapper = () => shallow(<NavigationHeader {...props} />);
-
-it("should render", () => {
-  //     expect(createWrapper()).toMatchSnapshot();
-});
-
-//   // it("should allow a page to be added", () => {
-//   //   const wrapper = createWrapper();
-//   //   wrapper.find('[data-test="add-menu"]').simulate("addQuestionPage");
-
-//   //   expect(mockHandlers.onAddQuestionPage).toHaveBeenCalled();
-//   // });
-
-//   // it("should allow a section to be added", () => {
-//   //   const wrapper = createWrapper();
-//   //   wrapper.find('[data-test="add-menu"]').simulate("addSection");
-
-//   //   expect(mockHandlers.onAddSection).toHaveBeenCalled();
-//   // });
-
-//   // it("should allow a calculated summary to be added", () => {
-//   //   const wrapper = createWrapper();
-//   //   wrapper.find('[data-test="add-menu"]').simulate("addCalculatedSummaryPage");
-//   //   expect(mockHandlers.onAddCalculatedSummaryPage).toHaveBeenCalled();
-//   // });
-
-//   // it("should allow a question confirmation to be added", () => {
-//   //   const wrapper = createWrapper();
-//   //   wrapper.find('[data-test="add-menu"]').simulate("addQuestionConfirmation");
-
-//   //   expect(mockHandlers.onAddQuestionConfirmation).toHaveBeenCalled();
-//   // });
-// });
