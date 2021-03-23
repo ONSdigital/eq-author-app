6166d8d8c8a323e85e4e18e6bca153f5537ed7fc
farres1 <71004932+farres1@users.noreply.github.com>
2021-03-23 12:21:27 +0000

    Ear 1195 vertical tab component (#1879)
    * Create initial settings tabs
    
    * Design update
    
    * Add settings container with vertical line
    
    * Change VerticalTabs components to use props
    
    * Add default props
    
    * Display tab links using props
    
    * Add initial URL
    
    * Refactor general settings code
    
    * Initial state implementation
    
    * Implement initial active tablink
    
    * Implement active tablink
    
    * Refactor settings pages
    
    * Fix duplicated settings page issue
    
    * Initial correct URL implementation
    
    * Fix vertical tabs broken URL issue
    
    * Update test
    
    * Blank commit
    
    * Export tab items from file
    
    * Initial type implementation
    
    * Disabling and redirecting themes tab
    
    * Disabling and redirecting themes tab
    
    * Disabling and redirecting themes tab
    
    * Update snapshot
    
    * Add theme page test
    
    * Fix lint error
    
    * Add vertical tabs storybook
    
    * Change story name
    
    * Redirect settings URL to general settings page
    
    * Redirect settings URL to general settings page
    
    * Text changes
    
    * Apply suggestions from code review
    
    Co-authored-by: Matt <mattiose@outlook.com>
    
    Co-authored-by: Matt <mattiose@outlook.com>

---
+++ eq-author/src/App/settings/ThemesPage.js
@@ -0,0 +1,98 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { withRouter, useRouteMatch } from "react-router-dom";
+
+import { colors } from "constants/theme";
+
+import VerticalTabs from "components/VerticalTabs";
+import tabItems from "./TabItems";
+
+import Header from "components/EditorLayout/Header";
+import ScrollPane from "components/ScrollPane";
+import { Field, Label } from "components/Forms";
+import { Grid, Column } from "components/Grid";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const SettingsContainer = styled.div`
+  padding: 0.8em;
+  border-left: 1px solid ${colors.lightGrey};
+`;
+
+const PageMainCanvas = styled.div`
+  display: flex;
+  border: 1px solid ${colors.lightGrey};
+  border-radius: 4px;
+  background: ${colors.white};
+`;
+
+const PageContainer = styled.div`
+  padding: 0.8em;
+  border-left: 1px solid ${colors.lightGrey};
+`;
+
+const StyledPanel = styled.div`
+  max-width: 97.5%;
+  padding: 1.3em;
+`;
+
+const ThemesPage = ({ questionnaire }) => {
+  const { type } = questionnaire;
+
+  const match = useRouteMatch();
+
+  return (
+    <Container>
+      <Header title="Settings" />
+      <PageContainer>
+        <PageMainCanvas>
+          <Grid>
+            <VerticalTabs
+              title="Questionnaire Settings"
+              cols={2.5}
+              tabItems={tabItems(match.params, type)}
+            />
+            <Column gutters={false} cols={9.5}>
+              <SettingsContainer>
+                <ScrollPane>
+                  <StyledPanel>
+                    <Field>
+                      <Label>Themes, IDs, form types and legal bases</Label>
+                    </Field>
+                    <Field>
+                      <p data-test="theme-description">
+                        The theme sets the design of the eQ for respondents. It
+                        changes the header across the survey, as well as the
+                        contact details and the legal basis on the introduction
+                        page. The COVID theme also changes the thank you page
+                        respondents see once they&apos;ve submitted the survey.
+                      </p>
+                    </Field>
+                    <Field>
+                      <p>
+                        The preview theme is applied when you view the survey
+                        using the View Survey button.
+                      </p>
+                    </Field>
+                  </StyledPanel>
+                </ScrollPane>
+              </SettingsContainer>
+            </Column>
+          </Grid>
+        </PageMainCanvas>
+      </PageContainer>
+    </Container>
+  );
+};
+ThemesPage.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  questionnaire: PropTypes.object.isRequired,
+};
+
+export default withRouter(ThemesPage);
