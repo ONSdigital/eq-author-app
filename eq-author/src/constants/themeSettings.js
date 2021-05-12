ad8ffc41483fa5323fa7706bac6983b2f2d4ea79
Thomas Baggaley <tbaggaley@pm.me>
2021-05-12 11:39:52 +0100

    🛠 Add constants files

---
+++ eq-author/src/constants/themeSettings.js
@@ -0,0 +1,9 @@
+export const THEME_TITLES = {
+  default: "GB theme",
+  northernireland: "NI theme",
+  covid: "COVID theme",
+  epe: "EPE theme",
+  epeni: "EPE NI theme",
+  ukis: "UKIS theme",
+  ukisni: "UKIS NI theme",
+};
