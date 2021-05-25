137df5bf1e53d3131d5703f156f55b9895f601e4
Matthew Elcock <mattiose@outlook.com>
2021-05-25 14:38:49 +0100

    Refactoring

--- eq-author/src/App/page/Design/answers/BasicAnswer/index.js
+++ eq-author/src/App/page/Design/answers/BasicAnswer/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useMutation } from "@apollo/react-hooks";
 
 import styled from "styled-components";
