c69487afb89163bf9f311d4a784b6f3e2b7dc9bf
Matthew Elcock <mattiose@outlook.com>
2021-06-09 14:31:18 +0100

    Moving pages in folders now does not flicker

--- eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/dragDropFunctions/index.js
+++ eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/dragDropFunctions/index.js
@@ -5,6 +5,18 @@ import {
   getSectionByFolderId,
 } from "utils/questionnaireUtils";
 
+// https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another
+const array_move = (arr, old_index, new_index) => {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+  return arr; // for testing
+};
+
 export const onDragEnd = (
   questionnaire,
   destination,
@@ -46,21 +58,66 @@ export const onDragEnd = (
 
   //If the user is moving a page into a folder
   if (pageBeingMoved && destinationFolder) {
-    const { id } = pageBeingMoved;
+    const { id: pageId, title } = pageBeingMoved;
     const { id: folderId } = destinationFolder;
+    const { id: sectionId, folders } = getSectionByFolderId(
+      questionnaire,
+      folderId
+    );
+    const { index: newPosition } = destination;
 
-    const { id: sectionId } = getSectionByFolderId(questionnaire, folderId);
-    const { index: position } = destination;
+    // Template an optimistic response as best we can
+    const optimisticResponse = {
+      movePage: {
+        position: newPosition,
+        id: pageId,
+        title,
+        section: {
+          id: sectionId,
+          folders: folders.map(({ pages, validationErrorInfo, ...rest }) => ({
+            ...rest,
+            pages: pages.map(({ id, position: oldPosition }) => ({
+              id,
+              position: oldPosition,
+              __typename: "QuestionPage",
+            })),
+            validationErrorInfo: {
+              errors: [],
+              ...validationErrorInfo,
+              __typename: "ValidationErrorInfo",
+            },
+            __typename: "Folder",
+          })),
+          __typename: "Section",
+        },
+        __typename: "QuestionPage",
+      },
+    };
 
+    // Fix optimistic response - move the page into the new position
+    optimisticResponse.movePage.section.folders.forEach(
+      ({ enabled, pages }) =>
+        enabled && array_move(pages, pageBeingMoved.position, newPosition)
+    );
+
+    // Fix optimistic response - Fix position props in pages
+    optimisticResponse.movePage.section.folders.forEach(
+      ({ enabled, pages }) =>
+        enabled && pages.forEach((page, index) => (page.position = index))
+    );
+
+    // Call the DB to move the page, passing in our optimistic response to avoid
+    // flickering if it takes time to respond
     movePage({
       variables: {
         input: {
-          id,
+          id: pageId,
           sectionId,
           folderId,
-          position,
+          position: newPosition,
         },
       },
+      optimisticResponse,
     });
   }
 
