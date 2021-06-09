808e00d0b4aaef5de2a38b78258972b2abfc2cee
Joshua Chapman <joshua.chapman@ons.gov.uk>
2021-06-09 14:08:27 +0100

    Fixed tests for Cheerio

--- eq-publisher/src/utils/convertPipes.js
+++ eq-publisher/src/utils/convertPipes.js
@@ -1,4 +1,4 @@
-const cheerio = require("cheerio");
+const cheerio = require("cheerio").default;
 const { flatMap, includes, compact } = require("lodash");
 const { unescapePiping } = require("./HTMLUtils");
 
