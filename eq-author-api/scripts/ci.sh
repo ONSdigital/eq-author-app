a692dd99fe328646f7f248299bb3cad21cf2fa53
Joshua Chapman <joshua.chapman@ons.gov.uk>
2021-06-11 15:45:54 +0100

    Remove references to codecov

--- eq-author-api/scripts/ci.sh
+++ eq-author-api/scripts/ci.sh
@@ -4,7 +4,6 @@ set -e
 yarn install --frozen-lockfile
 yarn lint --max-warnings=0
 yarn test --coverage
-bash <(curl -s https://codecov.io/bash) -e TRAVIS_NODE_VERSION -c -F api
 yarn test:breakingChanges
 
 docker build -t onsdigital/eq-author-api:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
--- eq-publisher/scripts/ci.sh
+++ eq-publisher/scripts/ci.sh
@@ -4,5 +4,4 @@ set -e
 yarn install --frozen-lockfile
 yarn lint
 yarn test --coverage
-bash <(curl -s https://codecov.io/bash) -e TRAVIS_NODE_VERSION -c -F publisher
 docker build -t onsdigital/eq-publisher:$TAG --build-arg APPLICATION_VERSION=$(git rev-parse HEAD) -f Dockerfile .
