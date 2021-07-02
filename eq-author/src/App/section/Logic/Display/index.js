1c4a17bfe5642d4d05e187b7c8cb5196c3f7d409
farres1 <sam.farrell@ons.gov.uk>
2021-07-02 09:15:13 +0100

    Convert display logic page to functional component

--- eq-author/src/App/section/Logic/Display/index.js
+++ eq-author/src/App/section/Logic/Display/index.js
@@ -1,60 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
+import CustomPropTypes from "custom-prop-types";
 import { propType } from "graphql-anywhere";
 import { get } from "lodash";
 import { Redirect } from "react-router-dom";
 import { Query } from "react-apollo";
 
 import DisplayPage from "./DisplayPage";
-import transformNestedFragments from "utils/transformNestedFragments";
 
 import Logic from "..";
 
 import Loading from "components/Loading";
 import Error from "components/Error";
 
-export class UnwrappedSectionDisplay extends React.Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      section: propType(),
-      // transformNestedFragments(
-      //   DisplayPage.fragments[0],
-      //   DisplayPage.fragments.slice(1)
-      // )
-    }),
-    loading: PropTypes.bool.isRequired,
-    error: PropTypes.object, // eslint-disable-line
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        questionnaireId: PropTypes.string.isRequired,
-      }).isRequired,
-    }).isRequired,
-  };
+const DisplayLogicPage = ({ data, loading, error, match }) => {
+  const section = get(data, "section");
 
-  renderContent() {
-    const { data, loading, error, match } = this.props;
-
-    if (loading) {
-      return <Loading height="20em">Loading display</Loading>;
-    }
-
-    const section = get(data, "section");
-
-    if (error || !section) {
-      return <Error>Something went wrong</Error>;
-    }
-
-    return <DisplayPage section={section} />;
+  if (loading) {
+    return <Loading height="20em">Loading display</Loading>;
   }
 
-  render() {
-    const section = this.props.data?.section;
+  if (error || !section) {
     return (
-      //   <PageContextProvider value={section}>
-      <Logic section={section}>{this.renderContent()}</Logic>
-      //   </PageContextProvider>
+      <Logic>
+        <Error>Something went wrong</Error>
+      </Logic>
     );
   }
-}
 
-export default UnwrappedSectionDisplay;
+  return (
+    <Logic section={section}>
+      <DisplayPage section={section} />
+    </Logic>
+  );
+};
+
+DisplayLogicPage.propTypes = {
+  data: PropTypes.shape({
+    section: CustomPropTypes.section,
+    // propType(
+    // transformNestedFragments(
+    //   DisplayPage.fragments[0],
+    //   DisplayPage.fragments.slice(1)
+    // )
+    // )
+  }),
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.object, // eslint-disable-line
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      questionnaireId: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default DisplayLogicPage;
