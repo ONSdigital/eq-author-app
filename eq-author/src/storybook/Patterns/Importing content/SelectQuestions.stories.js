e121b0dd1302d04392b0436f0ae5371df9226807
Matthew Elcock <mattiose@outlook.com>
2021-07-05 12:48:14 +0100

    Setup

---
+++ eq-author/src/storybook/Patterns/Importing content/SelectQuestions.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default {
+  title: "Patterns/Importing content/Select questions",
+  component: <p>Hi</p>,
+};
+
+const Template = (args) => <p>Hi</p>;
+
+export const Default = Template.bind({});
