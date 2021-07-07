44815ee6b8c80dd549db5779ce40cfe77cee2818
zimboflyman <nemazine@hotmail.com>
2021-07-07 10:15:30 +0100

    remove impish consol.logs

--- eq-author-api/migrations/addSectionHubSettings.js
+++ eq-author-api/migrations/addSectionHubSettings.js
@@ -1,13 +1,11 @@
 module.exports = function addSectionHubSettings(questionnaire) {
   questionnaire.sections.forEach((section) => {
-    console.log('section :>> ', section);
     if (section.requiredCompleted === undefined || section.requiredCompleted === null) {
       section.requiredCompleted = false;
     }
     if (section.showOnHub === undefined || section.showOnHub === null) {
       section.showOnHub = true;
     }
-    console.log('section : after >> ', section);
 
   });
 
