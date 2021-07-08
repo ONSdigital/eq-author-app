27218125925201ea3cefd904cfbc30228047f82c
martyncolmer <martyc1974@hotmail.com>
2021-07-08 15:47:47 +0100

    fix for expression migration

--- eq-author-api/migrations/updateExpressionSchema.js
+++ eq-author-api/migrations/updateExpressionSchema.js
@@ -2,10 +2,18 @@ const { getExpressions } = require("../schema/resolvers/utils");
 
 module.exports = (questionnaire) => {
   getExpressions({ questionnaire }).forEach((expression) => {
-    delete expression.left.id;
-    delete expression.left.__typename;
-    delete expression.left.options;
-    delete expression.right.options;
+    if (expression.left?.id) {
+      delete expression.left.id;
+    }
+    if (expression.left?.__typename) {
+      delete expression.left.__typename;
+    }
+    if (expression.left?.options) {
+      delete expression.left.options;
+    }
+    if (expression.right?.options) {
+      delete expression.right.options;
+    }
   });
 
   return questionnaire;
