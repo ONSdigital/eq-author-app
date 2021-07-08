d5d61b49cf03303d6a4fcc834fff79bde72cabdf
Ashley Tucker <ashley.tucker@ons.gov.uk>
2021-07-08 16:52:27 +0100

    Styled Component Bug
    Getting a weird error where the styled component was trying to effect things in the DOM. Looking into it, the solutions are things that have already been done in the file. One workaround, was to use a template literal on the prop.

--- eq-author/src/components/QuestionPicker/Item/index.js
+++ eq-author/src/components/QuestionPicker/Item/index.js
@@ -131,7 +131,7 @@ const WrappedItem = ({
         variant={variant}
         className={`${variant}`}
         aria-selected={selected}
-        unselectable={unselectable}
+        unselectable={`${unselectable}`}
         tabIndex={unselectable ? -1 : 0}
         onClick={onClick}
         onKeyUp={({ keyCode }) => onEnterUp(keyCode, onClick)}
