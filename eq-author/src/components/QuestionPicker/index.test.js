c4555f4329c1f6cb9001d3433e9c97f2132f6560
Ashley Tucker <ashley.tucker@ons.gov.uk>
2021-07-08 16:50:51 +0100

    Testing Files
    First round of testing for the modal questionnaire picker.

--- eq-author/src/components/QuestionPicker/index.test.js
+++ eq-author/src/components/QuestionPicker/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render } from "tests/utils/rtl";
+import { render, fireEvent } from "tests/utils/rtl";
 
 import QuestionPicker from ".";
 import mockSections from "../../tests/mocks/mockSections.json";
@@ -12,7 +12,7 @@ describe("QuestionPicker", () => {
     showSearch: true,
     warningPanel:
       "You cannot import folders but you can import any questions they contain.",
-    onSubmit: (selectedAnswers) => jest.fn(selectedAnswers),
+    onSubmit: jest.fn(),
     onClose: jest.fn(),
     startingSelectedQuestions: [],
     sections: mockSections,
@@ -21,9 +21,78 @@ describe("QuestionPicker", () => {
   const renderQuestionPicker = (args) =>
     render(<QuestionPicker isOpen {...defaultProps} {...args} />);
 
-  it("Can render", () => {
-    const { getByText } = renderQuestionPicker({});
+    describe("Testing the render", () => {
+      it("Can render", () => {
+        const { getByText } = renderQuestionPicker({});
 
-    expect(getByText(defaultProps.title)).toBeInTheDocument();
+        expect(getByText(defaultProps.title)).toBeInTheDocument();
+      });
+
+    });
+
+    describe("Function Firing ", () => {
+      it("onSubmit Fires", () => {
+        const { getByText } = renderQuestionPicker({});
+
+        const selectButton = getByText("Select");
+
+        fireEvent.click(selectButton);
+
+        expect(defaultProps.onSubmit).toHaveBeenCalled();
+      });
+
+    });
+
+  describe("Testing the search UI", () => {
+    it("Types pet into the search bar and returns the question Why do you not have pets?", () => {
+      const { getByText, getByTestId } = renderQuestionPicker({});
+
+      const searchBar =  getByTestId("search-bar");
+
+      fireEvent.change(searchBar, {
+        target: { value: "pet" },
+      });
+  
+      expect(getByText("Why do you not have any pets?")).toBeInTheDocument();
+    });
+
+    it("Types car into the search bar and returns the question Do you have a car && what colour car do you have?", () => {
+      const { getByText, getByTestId } = renderQuestionPicker({});
+
+      const searchBar =  getByTestId("search-bar");
+
+      fireEvent.change(searchBar, {
+        target: { value: "car" },
+      });
+  
+      expect(getByText("Do you have a car?")).toBeInTheDocument();
+      expect(getByText("What colour car do you have?")).toBeInTheDocument();
+    });
+
+    it("Doesnt show the search bar if showSearch is false", () => {
+      const defaultPropsWithSearchFalse = {
+        title: "Select the question(s) to import",
+        isOpen: true,
+        showSearch: false,
+        warningPanel:
+          "You cannot import folders but you can import any questions they contain.",
+        onSubmit: (selectedAnswers) => jest.fn(selectedAnswers),
+        onClose: jest.fn(),
+        startingSelectedQuestions: [],
+        sections: mockSections,
+      };
+
+      const renderQuestionPicker = (args) =>
+      render(<QuestionPicker isOpen {...defaultPropsWithSearchFalse} {...args} />);
+
+      const { queryByTestId } = renderQuestionPicker({});
+      const searchBar =  queryByTestId("search-bar");
+
+      expect(searchBar).toBeNull();
+
+
+    });
   });
+
+
 });
