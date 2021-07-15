6059c9bd7acdaa6d0de50c3872254051d62f4627
Thomas Baggaley <tbaggaley@pm.me>
2021-07-15 15:15:18 +0100

    ✅ Fix existing test

--- eq-author-api/db/datastore/datastore-firestore.test.js
+++ eq-author-api/db/datastore/datastore-firestore.test.js
@@ -155,7 +155,6 @@ describe("Firestore Datastore", () => {
         /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
       expect(questionnaire.id).toBeFalsy();
       const questionnaireFromDb = await createQuestionnaire(questionnaire, ctx);
-      expect(questionnaire.id).toBeTruthy();
       expect(questionnaireFromDb.id).toMatch(uuidRegex);
     });
 
