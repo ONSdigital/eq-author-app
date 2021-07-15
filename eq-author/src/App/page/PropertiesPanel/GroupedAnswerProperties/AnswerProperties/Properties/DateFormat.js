914442d936b3e6a4e0270f0d09bfca0cd96ae8bb
Ashley Tucker <ashley.rhys.tucker@gmail.com>
2021-07-15 12:21:05 +0100

    Conditonal render based on hub for date format

--- eq-author/src/App/page/PropertiesPanel/GroupedAnswerProperties/AnswerProperties/Properties/DateFormat.js
+++ eq-author/src/App/page/PropertiesPanel/GroupedAnswerProperties/AnswerProperties/Properties/DateFormat.js
@@ -1,15 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { enableOn } from "utils/featureFlags";
 
 import { Field, Select } from "components/Forms";
 
 const DateFormat = ({ id, value, onChange }) => (
   <Field>
-    <Select value={value} onChange={onChange} id={id} name={id}>
-      <option value="dd/mm/yyyy">dd / Month / yyyy</option>
-      <option value="mm/yyyy">Month / yyyy</option>
-      <option value="yyyy">yyyy</option>
-    </Select>
+      {enableOn(["hub"]) ? 
+
+        <Select value={value} onChange={onChange} id={id} name={id}>
+            <option value="dd/mm/yyyy">dd / mm / yyyy</option>
+            <option value="mm/yyyy">mm / yyyy</option>
+            <option value="yyyy">yyyy</option> 
+        </Select>
+
+      : 
+        <Select value={value} onChange={onChange} id={id} name={id}>
+          <option value="dd/mm/yyyy">dd / Month / yyyy</option>
+          <option value="mm/yyyy">Month / yyyy</option>
+          <option value="yyyy">yyyy</option>
+        </Select>
+      }
+
   </Field>
 );
 
