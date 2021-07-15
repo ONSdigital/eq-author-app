7fa80cc3e3197f9428156bcb59420cd896b95c1b
Ashley Tucker <ashley.rhys.tucker@gmail.com>
2021-07-15 16:05:50 +0100

    Refinement of the dateformat component

--- eq-author/src/App/page/PropertiesPanel/GroupedAnswerProperties/AnswerProperties/Properties/DateFormat.js
+++ eq-author/src/App/page/PropertiesPanel/GroupedAnswerProperties/AnswerProperties/Properties/DateFormat.js
@@ -4,24 +4,15 @@ import { enableOn } from "utils/featureFlags";
 
 import { Field, Select } from "components/Forms";
 
+const monthText = enableOn(["hub"]) ? "mm" : "Month";
+
 const DateFormat = ({ id, value, onChange }) => (
   <Field>
-      {enableOn(["hub"]) ? 
-
-        <Select data-test="select" value={value} onChange={onChange} id={id} name={id}>
-            <option data-test="day-month-year" value="dd/mm/yyyy">dd / mm / yyyy</option>
-            <option data-test="month-year" value="mm/yyyy">mm / yyyy</option>
-            <option data-test="year" value="yyyy">yyyy</option> 
-        </Select>
-
-      : 
-        <Select data-test="select" value={value} onChange={onChange} id={id} name={id}>
-          <option data-test="day-month-year" value="dd/mm/yyyy">dd / Month / yyyy</option>
-          <option data-test="month-year" value="mm/yyyy">Month / yyyy</option>
-          <option data-test="year" value="yyyy">yyyy</option>
-        </Select>
-      }
-
+    <Select data-test="select" value={value} onChange={onChange} id={id} name={id}>
+        <option data-test="day-month-year" value="dd/mm/yyyy">{`dd / ${monthText} / yyyy`}</option>
+        <option data-test="month-year" value="mm/yyyy">{`${monthText} / yyyy`}</option>
+        <option data-test="year" value="yyyy">yyyy</option> 
+    </Select>
   </Field>
 );
 
