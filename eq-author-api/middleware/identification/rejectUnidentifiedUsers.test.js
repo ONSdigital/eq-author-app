7e46915970ab542bfc72bcf353f9253dfcf16bab
martyncolmer <martyc1974@hotmail.com>
2021-07-21 09:49:21 +0100

    fix api tests

--- eq-author-api/middleware/identification/rejectUnidentifiedUsers.test.js
+++ eq-author-api/middleware/identification/rejectUnidentifiedUsers.test.js
@@ -9,12 +9,12 @@ describe("rejectUnidentifiedUsers", () => {
   });
 
   it("should respond with 401 if the user is unverified", () => {
-    req.user = { isVerified: false };
+    req.user = { isVerified: false, emailVerified: true };
     rejectUnidentifiedUsers(req, res, next);
     expect(res.status).toHaveBeenCalledWith(401);
   });
   it("should call next if user verified", () => {
-    req.user = { isVerified: true };
+    req.user = { isVerified: true, emailVerified: true };
     rejectUnidentifiedUsers(req, res, next);
     expect(next).toHaveBeenCalled();
   });
