082164153267005883541a8a48fd4b55f2e0fd59
Matthew Elcock <mattiose@outlook.com>
2021-08-06 13:09:23 +0100

    Added variable name to colours

--- eq-author/src/storybook/Theme/ColourComponents.js
+++ eq-author/src/storybook/Theme/ColourComponents.js
@@ -41,7 +41,7 @@ const Circle = styled.div`
 
 const camelize = (str) =>
   str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-    if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+    if (+match === 0) return ""; // eslint-disable-line
     return index === 0 ? match.toLowerCase() : match.toUpperCase();
   });
 
