6ec9eada2415215cb38e17e9b324ee6db3a99266
zimboflyman <nemazine@hotmail.com>
2021-09-10 14:54:09 +0100

    fix NavItem Story

---
+++ eq-author/src/storybook/Components/ExampleContainer/index.js
@@ -0,0 +1,15 @@
+import react from "react";
+import styled from "styled-components";
+
+const ExampleContainer = styled.div`
+  box-sizing: border-box;
+  width: 25%;
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`;
+
+export default ExampleContainer;
