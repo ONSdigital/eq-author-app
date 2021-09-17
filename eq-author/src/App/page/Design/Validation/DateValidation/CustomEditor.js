5d8588759880b3811d9ed047081d651c136b84fa
farres1 <sam.farrell@ons.gov.uk>
2021-09-17 13:08:46 +0100

    Add error styling to inputs

--- eq-author/src/App/page/Design/Validation/DateValidation/CustomEditor.js
+++ eq-author/src/App/page/Design/Validation/DateValidation/CustomEditor.js
@@ -6,23 +6,28 @@ import { ERR_NO_VALUE } from "constants/validationMessages";
 import ValidationError from "components/ValidationError";
 import { DateInput } from "./components.js";
 
-const CustomEditor = ({ validation, onChange, onUpdate }) => (
-  <>
-    <DateInput
-      name="customDate"
-      type="date"
-      value={validation.customDate}
-      onChange={onChange}
-      onBlur={onUpdate}
-      max="9999-12-30"
-      min="1000-01-01"
-      data-test="custom-date-input"
-    />
-    {validation.validationErrorInfo.errors.some(
-      (error) => error.errorCode === "ERR_NO_VALUE"
-    ) && <ValidationError>{ERR_NO_VALUE}</ValidationError>}
-  </>
-);
+const CustomEditor = ({ validation, onChange, onUpdate }) => {
+  const hasError = validation.validationErrorInfo.errors.some(
+    (error) => error.errorCode === "ERR_NO_VALUE"
+  );
+
+  return (
+    <>
+      <DateInput
+        name="customDate"
+        type="date"
+        value={validation.customDate}
+        onChange={onChange}
+        onBlur={onUpdate}
+        max="9999-12-30"
+        min="1000-01-01"
+        invalid={hasError}
+        data-test="custom-date-input"
+      />
+      {hasError && <ValidationError>{ERR_NO_VALUE}</ValidationError>}
+    </>
+  );
+};
 
 CustomEditor.propTypes = {
   validation: PropTypes.shape({
--- eq-author/src/App/page/Design/Validation/DateValidation/MetadataEditor.js
+++ eq-author/src/App/page/Design/Validation/DateValidation/MetadataEditor.js
@@ -7,21 +7,29 @@ import { ERR_NO_VALUE } from "constants/validationMessages";
 import ValidationError from "components/ValidationError";
 import MetadataContentPicker from "../MetadataContentPicker.js";
 
-const MetadataEditor = ({ onChangeUpdate, answer, validation }) => (
-  <>
-    <MetadataContentPicker
-      answerId={answer.id}
-      onSubmit={onChangeUpdate}
-      selectedContentDisplayName={get(validation.previousAnswer, "displayName")}
-      selectedMetadataDisplayName={get(validation.metadata, "displayName")}
-      selectedId={get(validation.previousAnswer, "id")}
-      data-test="metadata-date-editor"
-    />
-    {validation.validationErrorInfo.errors.some(
-      (error) => error.errorCode === "ERR_NO_VALUE"
-    ) && <ValidationError>{ERR_NO_VALUE}</ValidationError>}
-  </>
-);
+const MetadataEditor = ({ onChangeUpdate, answer, validation }) => {
+  const hasError = validation.validationErrorInfo.errors.some(
+    (error) => error.errorCode === "ERR_NO_VALUE"
+  );
+
+  return (
+    <>
+      <MetadataContentPicker
+        answerId={answer.id}
+        onSubmit={onChangeUpdate}
+        selectedContentDisplayName={get(
+          validation.previousAnswer,
+          "displayName"
+        )}
+        selectedMetadataDisplayName={get(validation.metadata, "displayName")}
+        selectedId={get(validation.previousAnswer, "id")}
+        data-test="metadata-date-editor"
+        hasError={hasError}
+      />
+      {hasError && <ValidationError>{ERR_NO_VALUE}</ValidationError>}
+    </>
+  );
+};
 
 MetadataEditor.propTypes = {
   validation: PropTypes.shape({
