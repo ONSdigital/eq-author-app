158d3392cb0d5ad0eaafec29aa46e5711c75d5c3
ashtuck <ashley.rhys.tucker@gmail.com>
2021-09-24 11:14:29 +0100

    Added a refetch request to get the questionnaire

--- eq-author-api/schema/resolvers/createMutation.js
+++ eq-author-api/schema/resolvers/createMutation.js
@@ -15,48 +15,46 @@ const {
   UNPUBLISHED,
 } = require("../../constants/publishStatus");
 
-const createMutation = (mutation, { ignoreLockStatus = false } = {}) => async (
-  root,
-  args,
-  ctx
-) => {
-  let hasBeenUnpublished;
-  enforceHasWritePermission(ctx.questionnaire, ctx.user); // throws ForbiddenError
-  if (!ignoreLockStatus) {
-    enforceQuestionnaireLocking(ctx.questionnaire); // throws ForbiddenError
-  }
-  if (ctx.questionnaire.publishStatus === AWAITING_APPROVAL) {
-    throw new Error(
-      "User can not edit questionnaire while waiting for approval"
-    );
-  }
+const createMutation =
+  (mutation, { ignoreLockStatus = false } = {}) =>
+  async (root, args, ctx) => {
+    let hasBeenUnpublished;
+    enforceHasWritePermission(ctx.questionnaire, ctx.user); // throws ForbiddenError
+    if (!ignoreLockStatus) {
+      enforceQuestionnaireLocking(ctx.questionnaire); // throws ForbiddenError
+    }
+    if (ctx.questionnaire.publishStatus === AWAITING_APPROVAL) {
+      throw new Error(
+        "User can not edit questionnaire while waiting for approval"
+      );
+    }
 
-  const result = await mutation(root, args, ctx);
-  if (ctx.questionnaire.publishStatus === PUBLISHED) {
-    ctx.questionnaire.publishStatus = UNPUBLISHED;
-    ctx.questionnaire.surveyVersion++;
-    hasBeenUnpublished = true;
-    await createHistoryEvent(ctx.questionnaire.id, publishStatusEvent(ctx));
-  }
+    const result = await mutation(root, args, ctx);
+    if (ctx.questionnaire.publishStatus === PUBLISHED) {
+      ctx.questionnaire.publishStatus = UNPUBLISHED;
+      ctx.questionnaire.surveyVersion++;
+      hasBeenUnpublished = true;
+      await createHistoryEvent(ctx.questionnaire.id, publishStatusEvent(ctx));
+    }
 
-  await saveQuestionnaire(ctx.questionnaire);
-  ctx.validationErrorInfo = validateQuestionnaire({
-    ...ctx.questionnaire,
-    updatedAt: new Date(),
-  });
-
-  if (hasBeenUnpublished) {
-    pubsub.publish("publishStatusUpdated", {
-      questionnaire: ctx.questionnaire,
+    await saveQuestionnaire(ctx.questionnaire);
+    ctx.validationErrorInfo = validateQuestionnaire({
+      ...ctx.questionnaire,
+      updatedAt: new Date(),
     });
-  }
-  pubsub.publish("validationUpdated", {
-    questionnaire: ctx.questionnaire,
-    validationErrorInfo: ctx.validationErrorInfo,
-    user: ctx.user,
-  });
-  return result;
-};
+
+    if (hasBeenUnpublished) {
+      pubsub.publish("publishStatusUpdated", {
+        questionnaire: ctx.questionnaire,
+      });
+    }
+    // pubsub.publish("validationUpdated", {
+    //   questionnaire: ctx.questionnaire,
+    //   validationErrorInfo: ctx.validationErrorInfo,
+    //   user: ctx.user,
+    // });
+    return result;
+  };
 
 module.exports = {
   createMutation,
