7b3b7d47c484177f466ea4f7774bbe6a4a6bf91e
martyncolmer <martyc1974@hotmail.com>
2021-09-29 13:07:53 +0100

    added ci script for api

---
+++ eq-author-api/scripts/test-ci.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+AWS_REGION=eu-west-1
+AWS_ACCESS_KEY_ID=dummy
+AWS_SECRET_ACCESS_KEY=dummy
+FIREBASE_PROJECT_ID=test-firebase-id
+
+echo "running tests..."
+
+AWS_REGION=${AWS_REGION} \
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+ENABLE_IMPORT=true \
+ENABLE_OPENTRACING=false \
+FIREBASE_PROJECT_ID=${FIREBASE_PROJECT_ID} \
+REDIS_DOMAIN_NAME=0.0.0.0 \
+REDIS_PORT=6379 \
+SURVEY_REGISTER_URL=http://host.docker.internal:8080/submit/ \
+DATABASE=mongodb \
+NODE_ENV=test \
+yarn jest --runInBand --detectOpenHandles --forceExit "$@"
