a46b9bae7bea09e02a6329ed74dbd897013099d6
jasehumphr <jason.humphries@ext.ons.gov.uk>
2021-10-04 15:44:16 +0100

    more props and a snap

--- eq-author/src/App/section/Design/SectionEditor/SectionSummary/index.test.js
+++ eq-author/src/App/section/Design/SectionEditor/SectionSummary/index.test.js
@@ -58,6 +58,7 @@ describe("Section Summary", () => {
           input: {
             id: props.id,
             sectionSummary: true,
+            collapsibleSummary: false,
           },
         },
       });
