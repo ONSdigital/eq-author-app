e560a80b7838730d07ba4686c8834a4da74d094e
farres1 <sam.farrell@ons.gov.uk>
2021-10-15 13:49:21 +0100

    Fix date validaion values reset to null

--- eq-author/src/App/page/Design/Validation/readToWriteMapper.js
+++ eq-author/src/App/page/Design/Validation/readToWriteMapper.js
@@ -7,59 +7,53 @@ import {
 } from "constants/validation-entity-types";
 
 const getCustom = (entityType, customDate) => {
-  if (entityType !== CUSTOM || !customDate) {
+  if (/*entityType !== CUSTOM ||*/ !customDate) {
     return null;
   }
   return customDate;
 };
 
 const getPreviousAnswer = (entityType, previousAnswer) => {
-  if (entityType !== PREVIOUS_ANSWER || !previousAnswer) {
+  if (/*entityType !== PREVIOUS_ANSWER ||*/ !previousAnswer) {
     return null;
   }
   return previousAnswer.id;
 };
 
 const getMetadata = (entityType, metadata) => {
-  if (entityType !== METADATA || !metadata) {
+  if (/*entityType !== METADATA ||*/ !metadata) {
     return null;
   }
   return metadata.id;
 };
 
-export const dateReadToWriteMapper = (outputKey) => ({
-  id,
-  customDate,
-  previousAnswer,
-  metadata,
-  entityType,
-  ...rest
-}) => ({
-  id,
-  [outputKey]: {
-    ...omit("enabled", rest),
-    entityType,
-    custom: getCustom(entityType, customDate),
-    previousAnswer: getPreviousAnswer(entityType, previousAnswer),
-    metadata: getMetadata(entityType, metadata),
-  },
-});
+export const dateReadToWriteMapper =
+  (outputKey) =>
+  ({ id, customDate, previousAnswer, metadata, entityType, ...rest }) => ({
+    id,
+    [outputKey]: {
+      ...omit("enabled", rest),
+      entityType,
+      custom: getCustom(entityType, customDate),
+      previousAnswer: getPreviousAnswer(entityType, previousAnswer),
+      metadata: getMetadata(entityType, metadata),
+    },
+  });
 
-export const durationReadToWriteMapper = (outputKey) => ({ id, ...rest }) => ({
-  id,
-  [outputKey]: { ...omit("enabled", rest) },
-});
+export const durationReadToWriteMapper =
+  (outputKey) =>
+  ({ id, ...rest }) => ({
+    id,
+    [outputKey]: { ...omit("enabled", rest) },
+  });
 
-export const numericReadToWriteMapper = (outputKey) => ({
-  id,
-  previousAnswer,
-  entityType,
-  ...rest
-}) => ({
-  id,
-  [outputKey]: {
-    ...omit("enabled", rest),
-    entityType,
-    previousAnswer: getPreviousAnswer(entityType, previousAnswer),
-  },
-});
+export const numericReadToWriteMapper =
+  (outputKey) =>
+  ({ id, previousAnswer, entityType, ...rest }) => ({
+    id,
+    [outputKey]: {
+      ...omit("enabled", rest),
+      entityType,
+      previousAnswer: getPreviousAnswer(entityType, previousAnswer),
+    },
+  });
