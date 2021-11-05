ee27703cdd63afd10b25af43356f41eaa22098e4
martyncolmer <martyc1974@hotmail.com>
2021-11-05 01:16:31 +0000

    bit more refactoring

--- eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/dragDropFunctions/onDragEnd.js
+++ eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/dragDropFunctions/onDragEnd.js
@@ -29,12 +29,10 @@ export default (
 
   const pageBeingMoved = getPageById(questionnaire, draggableId);
   const folderBeingMoved = getFolderById(questionnaire, draggableId);
-
   const destinationSection = getSectionById(
     questionnaire,
     destination.droppableId
   );
-
   const destinationFolder = getFolderById(
     questionnaire,
     destination.droppableId
@@ -47,22 +45,22 @@ export default (
 
   //If the user is moving a page into a folder
   if (pageBeingMoved && destinationFolder) {
-    const { id: pageId } = pageBeingMoved;
-    const { id: folderId } = destinationFolder;
-    const { id: sectionId } = getSectionByFolderId(questionnaire, folderId);
-    const { index: newPosition } = destination;
+    const { id: sectionId } = getSectionByFolderId(
+      questionnaire,
+      destinationFolder.id
+    );
+    const newPosition = destination.index;
 
-    const oldFolder = getFolderByPageId(questionnaire, pageId);
-    const oldPages = oldFolder.pages;
-    oldPages.splice(oldPages.indexOf(pageBeingMoved), 1);
+    const sourceFolder = getFolderByPageId(questionnaire, pageBeingMoved.id);
+    sourceFolder.pages.splice(sourceFolder.pages.indexOf(pageBeingMoved), 1);
     destinationFolder.pages.splice(newPosition, 0, pageBeingMoved);
 
     movePage({
       variables: {
         input: {
-          id: pageId,
+          id: pageBeingMoved.id,
           sectionId,
-          folderId,
+          folderId: destinationFolder.id,
           position: newPosition,
         },
       },
@@ -78,15 +76,10 @@ export default (
 
   // If the user is moving a page into a section
   if (pageBeingMoved && destinationSection) {
-    const { id: pageId } = pageBeingMoved;
-    const { id: sectionId } = destinationSection;
-    const { index: newPosition } = destination;
+    const newPosition = destination.index;
+    const sourceFolder = getFolderByPageId(questionnaire, pageBeingMoved.id);
+    sourceFolder.pages.splice(sourceFolder.pages.indexOf(pageBeingMoved), 1);
 
-    const oldFolder = getFolderByPageId(questionnaire, pageId);
-    const oldPages = oldFolder.pages;
-    oldPages.splice(oldPages.indexOf(pageBeingMoved), 1);
-
-    const { folders } = destinationSection;
     const newFolder = {
       id: 123,
       pages: [pageBeingMoved],
@@ -101,13 +94,14 @@ export default (
       },
       __typename: "Folder",
     };
-    folders.splice(newPosition, 0, newFolder);
+
+    destinationSection.folders.splice(newPosition, 0, newFolder);
 
     movePage({
       variables: {
         input: {
-          id: pageId,
-          sectionId,
+          id: pageBeingMoved.id,
+          sectionId: destinationSection.id,
           position: newPosition,
         },
       },
@@ -123,19 +117,22 @@ export default (
 
   // If the user is moving a folder into a section
   if (folderBeingMoved && destinationSection) {
-    const { id: folderId } = folderBeingMoved;
-    const { id: sectionId } = destinationSection;
-    const { index: newPosition } = destination;
-
-    const oldSection = getSectionByFolderId(questionnaire, folderId);
-    oldSection.folders.splice(oldSection.folders.indexOf(folderBeingMoved), 1);
+    const newPosition = destination.index;
+    const sourceSection = getSectionByFolderId(
+      questionnaire,
+      folderBeingMoved.id
+    );
+    sourceSection.folders.splice(
+      sourceSection.folders.indexOf(folderBeingMoved),
+      1
+    );
     destinationSection.folders.splice(newPosition, 0, folderBeingMoved);
 
     moveFolder({
       variables: {
         input: {
-          id: folderId,
-          sectionId,
+          id: folderBeingMoved.id,
+          sectionId: destinationSection.id,
           position: newPosition,
         },
       },
