bf17059e874364b8f5eb65a0ba225f494f8c5815
martyncolmer <martyc1974@hotmail.com>
2021-11-19 16:21:47 +0000

    fix box sizing

--- eq-author/src/contexts/themeContext.js
+++ eq-author/src/contexts/themeContext.js
@@ -5,8 +5,8 @@ import { themes } from "constants/theme";
 
 const GlobalStyle = createGlobalStyle`
   html {
-    box-sizing: border-box;
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
+    box-sizing: border-box;
   }
 
   *,
