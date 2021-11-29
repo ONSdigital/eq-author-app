6f3935c7f32204b1213a5422816fe6b3aa1877df
shelleytruong <shelleytruong@outlook.com>
2021-11-29 17:01:21 +0000

    header component finished

--- eq-author/src/components-themed/Header/index.js
+++ eq-author/src/components-themed/Header/index.js
@@ -1,45 +1,60 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import PropType from "prop-types";
-import logoHeader from "assets/ons-logo-header.svg";
 
-const Logo = styled.img`
-  svg {
-    path {
-      fill: "#fff";
-    }
+const getSvgColour = (variant) => {
+  switch (variant) {
+    case "Internal":
+      return ({ theme }) => theme.colors.internalOnsLogo;
+    case "External":
+      return ({ theme }) => theme.colors.externalOnsLogo;
   }
-`;
+};
 
-const InternalHeaderTop = styled.div`
-  background-color: ${({ theme }) => theme.colors.headerTop};
+const getHeaderTopColour = (variant) => {
+  switch (variant) {
+    case "Internal":
+      return ({ theme }) => theme.colors.internalHeaderTop;
+    case "External":
+      return ({ theme }) => theme.colors.externalHeaderTop;
+  }
+};
+
+const HeaderTop = styled.div`
+  background-color: ${({ variant }) => getHeaderTopColour(variant)};
   padding: 0 1rem;
   height: 46px;
   align-items: center;
   display: flex;
+  svg {
+    path:nth-of-type(1) {
+      fill: ${({ theme }) => theme.colors.onsLogoAccent};
+    }
+    fill: ${({ variant }) => getSvgColour(variant)};
+  }
 `;
 
-const WithDescription = css`
+const TitleDescription = styled.p`
   color: ${({ theme }) => theme.colors.headerTitle};
   font-size: ${({ theme }) => theme.fontSize};
-  /* font-weight: 400; */
   line-height: 1.4;
-  /* margin: 0 0 1rem; */
+  margin: 0 0 1rem;
 `;
 
-const HeaderTitle = styled.div`
-  font-family: ${({ theme }) => theme.colors.fonts};
-  font-size: 1.444rem;
+const HeaderTitle = styled.h1`
+  font-size: 1.6666666667rem;
   color: ${({ theme }) => theme.colors.headerTitle};
-  font-weight: 600;
+  font-weight: 900;
   line-height: 1.4;
   cursor: pointer;
-  display: block;
-  ${({ variant }) =>
-    variant === "WithDescription" &&
+  text-decoration: none;
+  letter-spacing: 0.1rem;
+  margin: 0;
+  ${({ withDescription }) =>
+    withDescription &&
     `
-    font-size: 1.111rem;
-  `}
+font-size: 2rem;
+`}
 `;
 
 const HeaderMain = styled.div`
@@ -51,29 +66,32 @@ const HeaderMain = styled.div`
   ${({ variant }) => variant === "WithDescription" && WithDescription}
 `;
 
-const StyledHeader = styled.div`
-  display: block;
-  position: relative;
-  margin: 0;
-`;
-
-const Header = ({ variant, children, HeaderDescription }) => {
+const Header = ({
+  variant,
+  children,
+  withDescription,
+  headerDescription,
+  logo,
+}) => {
   return (
-    <StyledHeader>
-      <InternalHeaderTop>
-        <img src={logoHeader} alt="Office for National Statistics logo" />
-      </InternalHeaderTop>
-      <HeaderMain variant={variant}>
-        <HeaderTitle variant={variant}>{children}</HeaderTitle>
-        {variant === "WithDescription" && <div>{HeaderDescription}</div>}
+    <>
+      <HeaderTop variant={variant}>{logo}</HeaderTop>
+      <HeaderMain withDescription={withDescription}>
+        <HeaderTitle withDescription={withDescription}>{children}</HeaderTitle>
+        {withDescription && (
+          <TitleDescription>{headerDescription}</TitleDescription>
+        )}
       </HeaderMain>
-    </StyledHeader>
+    </>
   );
 };
 
 Header.propTypes = {
   variant: PropType.string,
   children: PropType.node,
+  headerDescription: PropType.string,
+  withDescription: PropType.bool,
+  logo: PropType.node,
 };
 
 Header.defaultProps = {
