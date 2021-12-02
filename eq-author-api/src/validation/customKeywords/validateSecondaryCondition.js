d7d94f7811202c4c2c1844b7b2ea4264324dac67
zimboflyman <nemazine@hotmail.com>
2021-12-02 20:00:52 +0000

    remove stray log

--- eq-author-api/src/validation/customKeywords/validateSecondaryCondition.js
+++ eq-author-api/src/validation/customKeywords/validateSecondaryCondition.js
@@ -12,8 +12,6 @@ module.exports = (ajv) =>
       currentExpression,
       { rootData: questionnaire, instancePath }
     ) {
-      console.log(`currentExpression`, currentExpression);
-
       if (currentExpression.condition === "CountOf") {
         if (
           currentExpression.secondaryCondition === null ||
