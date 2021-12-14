38fd7a128c1b80e8d90d85a8984dd7ed8d4b1007
farres1 <sam.farrell@ons.gov.uk>
2021-12-14 13:13:36 +0000

    Add visuallyhidden import

--- eq-author/src/App/page/Logic/BinaryExpressionEditor/SecondaryConditionSelector.js
+++ eq-author/src/App/page/Logic/BinaryExpressionEditor/SecondaryConditionSelector.js
@@ -5,6 +5,7 @@ import { get } from "lodash";
 
 import { colors } from "constants/theme";
 import { Number, Select, Label } from "components/Forms";
+import VisuallyHidden from "components/VisuallyHidden";
 
 const conditions = {
   SELECT: null,
