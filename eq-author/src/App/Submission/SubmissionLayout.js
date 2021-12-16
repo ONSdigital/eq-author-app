b33ca8ca055552a43f8766ee308c6ac7c3a63883
farres1 <sam.farrell@ons.gov.uk>
2021-12-16 15:44:01 +0000

    Add content to page

--- eq-author/src/App/Submission/SubmissionLayout.js
+++ eq-author/src/App/Submission/SubmissionLayout.js
@@ -3,14 +3,16 @@ import PropTypes from "prop-types";
 
 import EditorLayout from "components/EditorLayout";
 
-// import Panel from "components/Panel";
+import Panel from "components/Panel";
 
-const SubmissionLayout = ({ renderPanel }) => (
-  <EditorLayout preview title="Submission page" renderPanel={renderPanel} />
+const SubmissionLayout = ({ renderPanel, children }) => (
+  <EditorLayout preview title="Submission page" renderPanel={renderPanel}>
+    <Panel>{children}</Panel>
+  </EditorLayout>
 );
 
 SubmissionLayout.propTypes = {
-  // children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
   renderPanel: PropTypes.func,
 };
 
