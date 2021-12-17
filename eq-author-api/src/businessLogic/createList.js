e4ca52a76b76db08428848e780d4910bdd6789d6
jasehumphr <jason.humphries@ext.ons.gov.uk>
2021-12-17 12:26:17 +0000

    fixed BL

--- eq-author-api/src/businessLogic/createList.js
+++ eq-author-api/src/businessLogic/createList.js
@@ -1,9 +1,10 @@
 const { v4: uuidv4 } = require("uuid");
-
+const { omit } = require("lodash");
 const createList = (input = {}) => ({
   id: uuidv4(),
   listName: "",
   displayName: "",
+  ...omit(input),
 });
 
 module.exports = createList;
