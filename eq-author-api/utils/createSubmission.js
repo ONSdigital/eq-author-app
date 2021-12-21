90a70b4677fe74f6e333e794aabb1c8265fcbc9a
farres1 <sam.farrell@ons.gov.uk>
2021-12-21 14:54:33 +0000

    Fix default further content text

--- eq-author-api/utils/createSubmission.js
+++ eq-author-api/utils/createSubmission.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require("uuid");
 module.exports = () => {
   return {
     id: uuidv4(),
-    furtherContent: `<p>Your response will help inform decision-makers how best to support the UK population and economy at this challenging time</p><p><a href="https://www.ons.gov.uk/surveys" target="_blank" rel="noopener noreferrer">Learn more about how we use this data</a></p>`,
+    furtherContent: `<p>Your response will help inform decision-makers how best to support the UK population and economy at this challenging time.</p><p><a href="https://www.ons.gov.uk/surveys" target="_blank" rel="noopener noreferrer">Learn more about how we use this data</a></p>`,
     viewPrintAnswers: true,
     emailConfirmation: true,
     feedback: true,
