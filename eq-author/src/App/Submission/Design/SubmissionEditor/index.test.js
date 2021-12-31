07c3d9e6c1f5397c7725fb8d7ef1393e8b330753
farres1 <sam.farrell@ons.gov.uk>
2021-12-31 14:19:17 +0000

    Submission editor update mutation tests

--- eq-author/src/App/Submission/Design/SubmissionEditor/index.test.js
+++ eq-author/src/App/Submission/Design/SubmissionEditor/index.test.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { render } from "tests/utils/rtl";
+import { render, fireEvent } from "tests/utils/rtl";
+import { useMutation } from "@apollo/react-hooks";
 import Theme from "contexts/themeContext";
 
 import SubmissionEditor from ".";
+
+jest.mock("@apollo/react-hooks", () => ({
+  useMutation: jest.fn(() => [() => null]),
+}));
 
 const questionnaire = {
   id: "questionnaire-1",
@@ -29,5 +34,44 @@ describe("Submission Editor", () => {
   it("should render", () => {
     const { getByTestId } = renderSubmissionEditor();
     expect(getByTestId("submission-editor")).toBeVisible();
+  });
+
+  it("should update viewPrintAnswers when toggle switch is clicked", () => {
+    const updateSubmission = jest.fn();
+    useMutation.mockImplementation(jest.fn(() => [updateSubmission]));
+
+    const { getByTestId } = renderSubmissionEditor();
+
+    fireEvent.click(getByTestId("viewPrintAnswers-input"));
+
+    expect(updateSubmission).toHaveBeenCalledWith({
+      variables: { input: { viewPrintAnswers: false } },
+    });
+  });
+
+  it("should update emailConfirmation when toggle switch is clicked", () => {
+    const updateSubmission = jest.fn();
+    useMutation.mockImplementation(jest.fn(() => [updateSubmission]));
+
+    const { getByTestId } = renderSubmissionEditor();
+
+    fireEvent.click(getByTestId("emailConfirmation-input"));
+
+    expect(updateSubmission).toHaveBeenCalledWith({
+      variables: { input: { emailConfirmation: false } },
+    });
+  });
+
+  it("should update feedback when toggle switch is clicked", () => {
+    const updateSubmission = jest.fn();
+    useMutation.mockImplementation(jest.fn(() => [updateSubmission]));
+
+    const { getByTestId } = renderSubmissionEditor();
+
+    fireEvent.click(getByTestId("feedback-input"));
+
+    expect(updateSubmission).toHaveBeenCalledWith({
+      variables: { input: { feedback: false } },
+    });
   });
 });
