8899d5898e31ebb6a4894d3dfc98b7992bb38bf8
farres1 <sam.farrell@ons.gov.uk>
2022-01-17 10:03:23 +0000

    Refactor styling

--- eq-author/src/App/page/Design/answers/dummy/Button/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import Button from "components-themed/buttons";
-
-const DummyButton = styled(Button)`
-  border-radius: 0;
-  pointer-events: none;
-`;
-
-export default DummyButton;
--- eq-author/src/App/page/Design/answers/dummy/Input/index.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import Input from "components-themed/Input";
-
-const DummyInput = styled(Input)`
-  pointer-events: none;
-  border-radius: 0;
-`;
-
-export default DummyInput;
