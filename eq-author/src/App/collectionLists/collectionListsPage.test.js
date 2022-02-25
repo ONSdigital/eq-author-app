94531b44208cff3cb54e6c5247f5ed2f43e29b24
martyncolmer <martyc1974@hotmail.com>
2022-02-25 17:19:28 +0000

    added tests

--- eq-author/src/App/collectionLists/collectionListsPage.test.js
+++ eq-author/src/App/collectionLists/collectionListsPage.test.js
@@ -1,29 +1,28 @@
 import React from "react";
-import { render, flushPromises, fireEvent, act } from "tests/utils/rtl";
+import {
+  render,
+  flushPromises,
+  fireEvent,
+  act,
+  screen,
+  waitFor,
+} from "tests/utils/rtl";
 import { MeContext } from "App/MeContext";
-import QuestionnaireContext from "components/QuestionnaireContext";
+import { publishStatusSubscription } from "components/EditorLayout/Header";
 
 import ListCollectorContent from "./collectionListsPage";
 
 import COLLECTION_LISTS from "graphql/lists/collectionLists.graphql";
-import createCollectionListMutation from "graphql/lists/createCollectionListMutation.graphql";
-import deleteCollectionListMutation from "graphql/lists/deleteCollectionListMutation.graphql";
-import updateCollectionListMutation from "graphql/lists/updateCollectionListMutation.graphql";
-import createCollectionListAnswerMutation from "graphql/lists/createCollectionListAnswerMutation.graphql";
-import deleteCollectionListAnswerMutation from "graphql/lists/deleteCollectionListAnswerMutation.graphql";
-import withUpdateAnswer from "App/page/Design/answers/withUpdateAnswer";
-import withCreateExclusive from "App/page/Design/answers/withCreateExclusive";
-import withCreateOption from "App/page/Design/answers/withCreateOption";
-import withUpdateOption from "App/page/Design/answers/withUpdateOption";
-import withDeleteOption from "App/page/Design/answers/withDeleteOption";
+import CREATE_COLLECTION_LIST from "graphql/lists/createCollectionListMutation.graphql";
+import DELETE_COLLECTION_LIST from "graphql/lists/deleteCollectionListMutation.graphql";
 
-const renderListCollectorContent = (questionnaire, user, mocks) => {
+const renderListCollectorContent = (user, mocks) => {
   return render(
     <MeContext.Provider value={{ me: user, signOut: jest.fn() }}>
-      <ListCollectorContent questionnaire={questionnaire} />
+      <ListCollectorContent />
     </MeContext.Provider>,
     {
-      route: `/q/${questionnaire.id}/collectionLists`,
+      route: `/q/e3c3ecc4-87fb-4819-826b-ac696a4bc569/collectionLists`,
       urlParamMatcher: "/q/:questionnaireId/collectionLists",
       mocks,
     }
@@ -31,48 +30,9 @@ const renderListCollectorContent = (questionnaire, user, mocks) => {
 };
 
 describe("List Collector page", () => {
-  let mockQuestionnaire, user, mocks, queryWasCalled;
+  let user, mocks;
 
   beforeEach(() => {
-    queryWasCalled = false;
-
-    mockQuestionnaire = {
-      title: "Spyro the Dragon",
-      shortTitle: "Spyro",
-      type: "Business",
-      id: "e3c3ecc4-87fb-4819-826b-ac696a4bc569",
-      qcodes: true,
-      navigation: true,
-      hub: false,
-      summary: true,
-      collapsibleSummary: false,
-      collectionLists: {
-        id: "3e6583e3-6ab6-455a-9f38-12ddaa9963cb",
-        lists: [
-          {
-            id: "291fd0ef-2f1e-4a2f-aced-96048cc0be48",
-            listName: null,
-            displayName: "Untitled list",
-            answers: [],
-
-            __typename: "List",
-          },
-        ],
-        __typename: "CollectionLists",
-      },
-      description: "A questionnaire about a lovable, purple dragon",
-      surveyId: "123",
-      theme: "default",
-      displayName: "Roar",
-      createdBy: {
-        ...user,
-      },
-      editors: [],
-      isPublic: true,
-      permission: true,
-      sections: [],
-    };
-
     user = {
       id: "123",
       displayName: "TerradorTheDragon",
@@ -86,8 +46,23 @@ describe("List Collector page", () => {
     mocks = [
       {
         request: {
-          query: createCollectionListMutation,
-          variables: { id: mockQuestionnaire.id },
+          query: COLLECTION_LISTS,
+          variables: {},
+        },
+        result: () => ({
+          data: {
+            collectionLists: {
+              id: "3e6583e3-6ab6-455a-9f38-12ddaa9963cb",
+              lists: [],
+              __typename: "CollectionLists",
+            },
+          },
+        }),
+      },
+      {
+        request: {
+          query: CREATE_COLLECTION_LIST,
+          variables: {},
         },
         result: () => ({
           data: {
@@ -99,11 +74,54 @@ describe("List Collector page", () => {
                   listName: null,
                   displayName: "Untitled list",
                   answers: [],
-
+                  validationErrorInfo: {
+                    errors: [
+                      {
+                        errorCode: "LISTNAME_MISSING",
+                        field: "listName",
+                        id: "4defb7eb-9d0b-46f1-9ae5-a3b9135a687c",
+                        type: "list",
+                        __typename: "ValidationError",
+                      },
+                    ],
+                    id: "2bf14226-6c24-4f2e-b15e-b485181212bf",
+                    totalCount: "1",
+                    __typename: "ValidationErrorInfo",
+                  },
                   __typename: "List",
                 },
               ],
               __typename: "CollectionLists",
+            },
+          },
+        }),
+      },
+      {
+        request: {
+          query: DELETE_COLLECTION_LIST,
+          variables: { input: { id: "291fd0ef-2f1e-4a2f-aced-96048cc0be48" } },
+        },
+        result: () => ({
+          data: {
+            deleteList: {
+              id: "3e6583e3-6ab6-455a-9f38-12ddaa9963cb",
+              lists: [],
+              __typename: "CollectionLists",
+            },
+          },
+        }),
+      },
+      {
+        request: {
+          query: publishStatusSubscription,
+          variables: { id: "e3c3ecc4-87fb-4819-826b-ac696a4bc569" },
+        },
+        result: () => ({
+          data: {
+            publishStatusUpdated: {
+              id: "e3c3ecc4-87fb-4819-826b-ac696a4bc569",
+              publishStatus: "Unpublished",
+              __typename: "Questionnaire",
             },
           },
         }),
@@ -116,11 +134,31 @@ describe("List Collector page", () => {
     });
   });
 
-  it("Should render list collector page", () => {
-    const { getByTestId } = renderListCollectorContent(
-      mockQuestionnaire,
-      user,
-      mocks
-    );
+  it("Should render empty list collector page", async () => {
+    renderListCollectorContent(user, mocks);
+
+    await waitFor(() => screen.getByTestId("btn-add-list"));
+    expect(screen.getByText("Currently no lists")).toBeTruthy();
+  });
+
+  it("Should add one untitled list", async () => {
+    renderListCollectorContent(user, mocks);
+
+    await waitFor(() => screen.getByTestId("btn-add-list"));
+    fireEvent.click(screen.getByTestId("btn-add-list"));
+    await waitFor(() => screen.getByTestId("btn-add-list"));
+    expect(screen.getByText("Untitled list")).toBeTruthy();
+  });
+
+  it("Should delete a list", async () => {
+    renderListCollectorContent(user, mocks);
+
+    await waitFor(() => screen.getByTestId("btn-add-list"));
+    fireEvent.click(screen.getByTestId("btn-add-list"));
+    await waitFor(() => screen.getByTestId("btn-delete-item"));
+    expect(screen.getByText("Untitled list")).toBeTruthy();
+    fireEvent.click(screen.getByTestId("btn-delete-item"));
+    await waitFor(() => screen.getByTestId("btn-add-list"));
+    expect(screen.getByText("Currently no lists")).toBeTruthy();
   });
 });
