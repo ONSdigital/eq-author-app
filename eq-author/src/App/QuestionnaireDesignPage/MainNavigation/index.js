21f5722b14ad3ef1c7dd0241ddb0f9184be8a8e2
jasehumphr <jason.humphries@ext.ons.gov.uk>
2022-03-03 16:18:23 +0000

    Added basic platform indicator

--- eq-author/src/App/QuestionnaireDesignPage/MainNavigation/index.js
+++ eq-author/src/App/QuestionnaireDesignPage/MainNavigation/index.js
@@ -41,6 +41,14 @@ import {
   buildShortcutsPath,
 } from "utils/UrlUtils";
 
+const Platform = styled.div`
+  color: ${colors.white};
+  background: ${colors.mediumBlue};
+  position: relative;
+  text-align: center;
+  padding: 1em;
+`;
+
 const StyledMainNavigation = styled.div`
   color: ${colors.grey};
   background: ${colors.black};
@@ -99,6 +107,7 @@ export const UnwrappedMainNavigation = ({
           <UtilityBtns tabIndex="-1" data-test="keyNav" className="keyNav">
             {hasQuestionnaire && (
               <ButtonGroup vertical align="centre" margin="0.em" gutter="0.em">
+                <Platform>AWS</Platform>
                 <RouteButton variant="navigation" small to="/">
                   <IconText nav icon={homeIcon}>
                     Home
