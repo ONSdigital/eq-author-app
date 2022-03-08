d97da34cfaa4e363fdd2f5eda08bb079ba2f9950
farres1 <sam.farrell@ons.gov.uk>
2022-03-08 14:06:41 +0000

    Replace duplicated code

--- eq-author/src/components/Comments/CommentNotification/index.js
+++ eq-author/src/components/Comments/CommentNotification/index.js
@@ -5,45 +5,39 @@ const CommentNotificationNavItem = css`
   border-bottom: 3px solid ${({ theme }) => theme.colors.neonYellow};
   border-right: 3px solid ${({ theme }) => theme.colors.neonYellow};
   border-left: 5px solid ${({ theme }) => theme.colors.neonYellow};
-  padding: 0.4rem;
-  position: relative;
-  background-color: ${({ theme }) => theme.colors.neonYellow};
-  border-radius: 10%;
-  margin-right: 0.5em;
   &::before {
     border-bottom: 7px solid transparent;
     border-left: 7px solid ${({ theme }) => theme.colors.neonYellow};
     border-right: 7px solid transparent;
     border-top: 7px solid ${({ theme }) => theme.colors.neonYellow};
-    content: "";
-    height: 0;
     left: -5px;
-    position: absolute;
-    width: 0;
   }
 `;
 
 const CommentNotificationTabs = css`
-  padding: 0.4rem;
-  position: relative;
-  background-color: ${({ theme }) => theme.colors.neonYellow};
-  border-radius: 10%;
-  margin-right: 0.5em;
   display: inline-flex;
   &::before {
     border-bottom: 4px solid transparent;
     border-left: 4px solid ${({ theme }) => theme.colors.neonYellow};
     border-right: 4px solid transparent;
     border-top: 4px solid ${({ theme }) => theme.colors.neonYellow};
-    content: "";
-    height: 0;
     left: 0;
-    position: absolute;
-    width: 0;
   }
 `;
 
 const CommentNotification = styled.div`
+  background-color: ${({ theme }) => theme.colors.neonYellow};
+  position: relative;
+  padding: 0.4rem;
+  border-radius: 10%;
+  margin-right: 0.5em;
+  &::before {
+    content: "";
+    height: 0;
+    width: 0;
+    position: absolute;
+  }
+
   ${(props) => props.variant === "tabs" && CommentNotificationTabs};
   ${(props) => props.variant === "nav" && CommentNotificationNavItem};
 `;
