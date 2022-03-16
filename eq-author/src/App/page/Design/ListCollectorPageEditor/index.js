36aae78b149096fc6f6e734a6861742dd769789c
jasehumphr <jason.humphries@ext.ons.gov.uk>
2022-03-16 15:05:09 +0000

    Added in some of the page content

--- eq-author/src/App/page/Design/ListCollectorPageEditor/index.js
+++ eq-author/src/App/page/Design/ListCollectorPageEditor/index.js
@@ -3,8 +3,10 @@ import PropTypes from "prop-types";
 import CustomPropTypes from "custom-prop-types";
 import gql from "graphql-tag";
 import { flowRight } from "lodash";
-
-import withChangeUpdate from "enhancers/withChangeUpdate";
+import Collapsible from "components/Collapsible";
+import styled from "styled-components";
+import { colors } from "constants/theme";
+import getIdForObject from "utils/getIdForObject";
 import withPropRenamed from "enhancers/withPropRenamed";
 import focusOnEntity from "utils/focusOnEntity";
 import TotalValidationRuleFragment from "graphql/fragments/total-validation-rule.graphql";
@@ -51,6 +53,37 @@ export const UnwrappedListCollectorEditor = (props) => {
     setEntity(updatedEntity);
   };
 
+  const StyledGrid = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1em;
+    /* border: 1px solid ${colors.lightGrey}; */
+    margin: 0.5em 1em;
+  `;
+
+  const List = styled.ol`
+    margin: 0 0 1.5em;
+    padding: 0;
+    counter-reset: item;
+  `;
+
+  const ListItem = styled.li`
+    margin: 0;
+    padding: 0 0 0.25em 2em;
+    text-indent: -1.5em;
+    list-style-type: none;
+    counter-increment: item;
+    &:before {
+      display: inline-block;
+      width: 1.5em;
+      padding-right: 0.5em;
+      font-weight: bold;
+      text-align: right;
+      content: counter(item) ".";
+    }
+  `;
+
+  const Text = styled.p``;
   const totalValidationErrors = page.validationErrorInfo.errors.filter(
     ({ field }) => field === "totalValidation"
   );
@@ -71,6 +104,75 @@ export const UnwrappedListCollectorEditor = (props) => {
         onChange={handleOnChange}
         alertText="All edits, properties and routing settings will also be removed."
       />
+      <StyledGrid>
+        {/* 
+
+Info text - The list collector is the way to recreate the List collector questionnaire design pattern. This is made up of two questions, a repeating list question and a collection question. These become available once you select a collection list to link them to.
+
+Collapsible label - How to create the List Collector questionnaire design pattern in Author?
+
+Collapsible text - To create the list collector pattern in Author we do this in the following steps:
+1. Go to the Collection Lists page on the left hand menu and create a Collection list.
+2. Add the answers you need to collect that will make up each item on the list, for example adding two text field answer types to collect the respondents first name and last name.
+3. In the questionnaire add a List collector page via the add/import menu.
+4. On the new List collector page there is Collection list area where you must select a list from the dropdown menu, this menu will show any lists that have been created in the Collection lists page.
+5. Once the list is selected two new containers appear on the page, these contain the two questions which will help compile the list. These are the Repeating list collector question and the Collection question.
+6. These two questions will repeat for the respondent until there is nothing else to add to the list and they select the negative radio option, for example (a) does anyone else live in the household? if yes then, (b) what is their name?
+7. Each time the respondent adds to the list this a summary of the list will be displayed on the Repeating list collector question.
+
+ */}
+        <Text>
+          <b>List collector</b>
+          <br />
+          The list collector is the way to recreate the List collector
+          questionnaire design pattern. This is made up of two questions, a
+          repeating list question and a collection question. These become
+          available once you select a collection list to link them to.
+        </Text>
+
+        <Collapsible title="How to create the List Collector questionnaire design pattern in Author?">
+          <Text>
+            To create the list collector pattern in Author we do this in the
+            following steps:
+          </Text>
+          <List>
+            <ListItem>
+              Go to the Collection Lists page on the left hand menu and create a
+              Collection list.
+            </ListItem>
+            <ListItem>
+              Add the answers you need to collect that will make up each item on
+              the list, for example adding two text field answer types to
+              collect the respondents first name and last name.
+            </ListItem>
+            <ListItem>
+              In the questionnaire add a List collector page via the add/import
+              menu.
+            </ListItem>
+            <ListItem>
+              On the new List collector page there is Collection list area where
+              you must select a list from the dropdown menu, this menu will show
+              any lists that have been created in the Collection lists page.
+            </ListItem>
+            <ListItem>
+              Once the list is selected two new containers appear on the page,
+              these contain the two questions which will help compile the list.
+              These are the Repeating list collector question and the Collection
+              question.
+            </ListItem>
+            <ListItem>
+              These two questions will repeat for the respondent until there is
+              nothing else to add to the list and they select the negative radio
+              option, for example (a) does anyone else live in the household? if
+              yes then, (b) what is their name?
+            </ListItem>
+            <ListItem>
+              Each time the respondent adds to the list this a summary of the
+              list will be displayed on the Repeating list collector question.
+            </ListItem>
+          </List>
+        </Collapsible>
+      </StyledGrid>
     </div>
   );
 };
@@ -120,7 +222,6 @@ UnwrappedListCollectorEditor.fragments = {
   `,
 };
 
-export default flowRight(
-  withUpdateListCollectorPage,
-  withChangeUpdate
-)(UnwrappedListCollectorEditor);
+export default flowRight(withUpdateListCollectorPage)(
+  UnwrappedListCollectorEditor
+);
