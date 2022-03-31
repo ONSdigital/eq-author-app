6bc6896cf45be280fd5b704f10c5f8308bbdcbf3
zimboflyman <nemazine@hotmail.com>
2022-03-31 11:33:58 +0100

    remove surplus code per Sams review

--- eq-author/src/App/importingContent/index.js
+++ eq-author/src/App/importingContent/index.js
@@ -156,24 +156,18 @@ const ImportingContent = ({ stopImporting, targetInsideFolder }) => {
           sectionId,
         };
 
-        const {
-          enabled: parentFolderIsEnabled,
-          id: folderId,
-          position,
-        } = getFolderByPageId(sourceQuestionnaire, currentEntityId);
+        const { id: folderId } = getFolderByPageId(
+          sourceQuestionnaire,
+          currentEntityId
+        );
 
         const { position: positionOfPreviousPage } = getPageById(
           sourceQuestionnaire,
           currentEntityId
         );
 
-        if (parentFolderIsEnabled) {
-          input.position.folderId = folderId;
-          input.position.index = positionOfPreviousPage + 1;
-        } else {
-          input.position.folderId = folderId;
-          input.position.index = 0;
-        }
+        input.position.folderId = folderId;
+        input.position.index = positionOfPreviousPage + 1;
 
         break;
       }
