569b21c3568ef331c8e1d967dc385c83c41474ae
farres1 <sam.farrell@ons.gov.uk>
2022-04-01 14:02:18 +0100

    Add file

---
+++ eq-author-api/migrations/updateCommentsReadByEditors.js
@@ -0,0 +1,39 @@
+const { getQuestionnaireMetaById } = require("../db/datastore");
+
+module.exports = (questionnaire) => {
+  const metadata = getQuestionnaireMetaById(questionnaire.id);
+  const { createdBy, editors } = metadata;
+
+  questionnaire.sections.forEach((section) => {
+    section.comments.forEach((comment) => {
+      if (!comment.readBy) {
+        comment.readBy = [createdBy, ...editors];
+      }
+    });
+    section.folders.forEach((folder) => {
+      folder.pages.forEach((page) => {
+        page.comments.forEach((comment) => {
+          if (!comment.readBy) {
+            comment.readBy = [createdBy, ...editors];
+          }
+        });
+      });
+    });
+  });
+  if (questionnaire?.introduction) {
+    questionnaire?.introduction?.comments?.forEach((comment) => {
+      if (!comment.readBy) {
+        comment.readBy = [createdBy, ...editors];
+      }
+    });
+  }
+  if (questionnaire?.submission) {
+    questionnaire?.submission?.comments?.forEach((comment) => {
+      if (!comment.readBy) {
+        comment.readBy = [createdBy, ...editors];
+      }
+    });
+  }
+
+  return questionnaire;
+};
