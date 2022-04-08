0226b5ac19e26c64e58a78a5d206ebffbc4a879d
jasehumphr <jason.humphries@ext.ons.gov.uk>
2022-04-08 09:29:54 +0100

    broken tests

---
+++ eq-author/src/App/page/Design/ListCollectorPageEditor/index.test.js
@@ -0,0 +1,169 @@
+import { UnwrappedListCollectorEditor } from "./";
+
+import React from "react";
+import { shallow } from "enzyme";
+
+jest.mock("components/NavigationCallbacks", () => ({
+  useSetNavigationCallbacksForPage: () => null,
+}));
+
+describe("List Collector Page Editor", () => {
+  let wrapper;
+
+  let mockHandlers;
+  let page;
+  let section;
+  let questionnaire;
+
+  const answer = {
+    id: "123",
+    __typename: "Answer",
+  };
+
+  const match = {
+    params: {
+      questionnaireId: "1",
+      sectionId: "2",
+      pageId: "3",
+    },
+  };
+
+  const render = ({ ...props }) => {
+    return shallow(
+      <UnwrappedListCollectorEditor
+        {...mockHandlers}
+        questionnaire={questionnaire}
+        page={page}
+        section={section}
+        showMovePageDialog={false}
+        showDeleteConfirmDialog={false}
+        match={match}
+        {...props}
+      />
+    );
+  };
+
+  beforeEach(() => {
+    mockHandlers = {
+      onUpdateAnswer: jest.fn(),
+      onUpdatePage: jest.fn(),
+      onDeletePageConfirm: jest.fn(),
+      onCloseDeleteConfirmDialog: jest.fn(),
+      onAddAnswer: jest.fn(() => Promise.resolve(answer)),
+      onAddOption: jest.fn(),
+      fetchAnswers: jest.fn(),
+      onDeleteOption: jest.fn(),
+      onDeleteAnswer: jest.fn(),
+      onAddExclusive: jest.fn(),
+      onUpdateOption: jest.fn(),
+      onMovePage: jest.fn(),
+      onCloseMovePageDialog: jest.fn(),
+      onAddOther: jest.fn(),
+      onDeleteOther: jest.fn(),
+      onChange: jest.fn(),
+      onUpdate: jest.fn(),
+    };
+
+    page = {
+      __typename: "Page",
+      id: "1",
+      displayName: "Question",
+      position: 1,
+      title: "<p>Hello world</p>",
+      alias: "Who am I?",
+      anotherNegativeDescription: "",
+      alias: "",
+      title: "List Names",
+      anotherPositiveDescription: "",
+      addItemTitle: "<p>What are the names of everyone who live at ?</p>",
+      anotherTitle:
+        "<p>Does anyone live at  as their permanent or family home?</p>",
+      pageType: "ListCollectorPage",
+      anotherPositive: "Yes",
+      id: "b6d9efb5-24e5-4e42-abbc-912ef6aebcdb",
+      listId: "list-1",
+      anotherNegative: "No",
+      validationErrorInfo: {
+        totalCount: 0,
+        errors: [],
+        id: "1",
+        __typename: "ValidationErrorInfo",
+      },
+      answers: [
+        {
+          __typename: "BasicAnswer",
+          id: "1",
+          title: "First name",
+          description: "",
+          type: "TextField",
+        },
+        {
+          __typename: "BasicAnswer",
+          id: "2",
+          title: "Last name",
+          description: "",
+          type: "TextField",
+        },
+      ],
+      section: { questionnaire: { metadata: [] } },
+    };
+
+    section = {
+      id: "3",
+      pages: [page],
+    };
+
+    questionnaire = {
+      id: "1",
+      __typename: "Questionnaire",
+      sections: [section],
+      collectionLists: {
+        lists: [
+          {
+            answers: [
+              {
+                secondaryLabel: null,
+
+                description: null,
+                properties: {
+                  required: false,
+                },
+                id: "list-answer-1",
+                label: "Pet's Name",
+                guidance: null,
+                type: "TextField",
+                qCode: null,
+              },
+            ],
+            id: "list-1",
+            listName: "Pets",
+            totalValidation: null,
+          },
+        ],
+        id: "collection-lists-1",
+      },
+    };
+
+    wrapper = render({});
+  });
+
+  it("should delete the correct answer", () => {
+    wrapper
+      .find("[data-test='answers-editor']")
+      .first()
+      .simulate("deleteAnswer", page.answers[0].id);
+
+    expect(mockHandlers.onDeleteAnswer).toHaveBeenCalledWith(
+      page.id,
+      page.answers[0].id
+    );
+  });
+
+  it("should add an answer with a type", () => {
+    wrapper.find("[data-test='add-answer']").simulate("select", "Textfield");
+    expect(mockHandlers.onAddAnswer).toHaveBeenCalledWith(
+      match.params.pageId,
+      "Textfield"
+    );
+  });
+});
---
+++ eq-author/src/App/page/Preview/ListCollectorPagePreview.test.js
@@ -0,0 +1,177 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { render, flushPromises, act } from "tests/utils/rtl";
+
+import commentsSubscription from "App/Comments/graphql/commentSubscription.graphql";
+import { MeContext } from "App/MeContext";
+import { byTestAttr } from "tests/utils/selectors";
+import { TEXTFIELD } from "constants/answer-types";
+
+import Error from "components/preview/Error";
+import { publishStatusSubscription } from "components/EditorLayout/Header";
+
+import QuestionPagePreview, {
+  DetailsContent,
+  DetailsTitle,
+} from "./QuestionPagePreview";
+
+describe("QuestionPagePreview", () => {
+  let page, me, mocks, questionnaireId;
+
+  beforeEach(() => {
+    me = {
+      id: "123",
+      displayName: "Raymond Holt",
+      email: "RaymondHolt@TheNineNine.com",
+      picture: "http://img.com/avatar.jpg",
+      admin: true,
+    };
+
+    page = {
+      __typename: "Page",
+      id: "1",
+      displayName: "Question",
+      position: 1,
+      title: "<p>Hello world</p>",
+      alias: "Who am I?",
+      anotherNegativeDescription: "",
+      alias: "",
+      title: "List Names",
+      anotherPositiveDescription: "",
+      addItemTitle: "<p>What are the names of everyone who live at ?</p>",
+      anotherTitle:
+        "<p>Does anyone live at  as their permanent or family home?</p>",
+      pageType: "ListCollectorPage",
+      anotherPositive: "Yes",
+      id: "b6d9efb5-24e5-4e42-abbc-912ef6aebcdb",
+      listId: "list-1",
+      anotherNegative: "No",
+      validationErrorInfo: { totalCount: 0, errors: [] },
+      folder: {
+        id: "folder-1",
+        position: 0,
+      },
+      totalValidation: null,
+    };
+    questionnaireId = "q123";
+    mocks = [
+      {
+        request: {
+          query: publishStatusSubscription,
+          variables: { id: questionnaireId },
+        },
+        result: () => ({
+          data: {
+            publishStatusUpdated: {
+              id: questionnaireId,
+              publishStatus: "Unpublished",
+              __typename: "Questionnaire",
+            },
+          },
+        }),
+      },
+      {
+        request: {
+          query: commentsSubscription,
+          variables: { id: "1" },
+        },
+        result: () => {
+          return {};
+        },
+      },
+    ];
+  });
+
+  it("should render", async () => {
+    const { getByText } = render(
+      <MeContext.Provider value={{ me }}>
+        <QuestionPagePreview page={page} />
+      </MeContext.Provider>,
+      {
+        route: `/q/${questionnaireId}/page/2`,
+        urlParamMatcher: "/q/:questionnaireId/page/:pageId",
+        mocks,
+      }
+    );
+    await act(async () => {
+      await flushPromises();
+    });
+    expect(getByText("Additional Info Content")).toBeTruthy();
+  });
+
+  it("should render warning when there are no answers", () => {
+    page.answers = [];
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper.find(byTestAttr("no-answers"))).toBeTruthy();
+  });
+
+  it("should not render description when disabled", () => {
+    page.descriptionEnabled = false;
+    const wrapper2 = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper2.exists(byTestAttr("description"))).toBeFalsy();
+  });
+
+  it("should render description missing message", () => {
+    page.description = "";
+    const wrapper2 = shallow(<QuestionPagePreview page={page} />);
+    expect(
+      wrapper2.find(byTestAttr("description")).find(Error)
+    ).toMatchSnapshot();
+  });
+
+  it("should not render guidance when disabled", () => {
+    page.guidanceEnabled = false;
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper.exists(byTestAttr("guidance"))).toBeFalsy();
+  });
+
+  it("should render guidance missing message", () => {
+    page.guidance = "";
+    const wrapper2 = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper2.find(byTestAttr("guidance")).find(Error)).toMatchSnapshot();
+  });
+
+  it("should not render definition when disabled", () => {
+    page.definitionEnabled = false;
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper.exists(byTestAttr("definition"))).toBeFalsy();
+  });
+
+  it("should render definition label missing message", () => {
+    page.definitionLabel = "";
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(
+      wrapper.find(byTestAttr("definition")).find(DetailsTitle)
+    ).toMatchSnapshot();
+  });
+
+  it("should render definition content missing message", () => {
+    page.definitionContent = "";
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(
+      wrapper.find(byTestAttr("definition")).find(DetailsContent)
+    ).toMatchSnapshot();
+  });
+
+  it("should not render additional information when disabled", () => {
+    page.additionalInfoEnabled = false;
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(wrapper.exists(byTestAttr("additional-info"))).toBeFalsy();
+  });
+
+  it("should render additional info label missing message", () => {
+    page.additionalInfoLabel = "";
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(
+      wrapper.find(byTestAttr("additional-info")).find(DetailsTitle)
+    ).toMatchSnapshot();
+  });
+
+  it("should render additional info content missing message", () => {
+    page.additionalInfoContent = "";
+    const wrapper = shallow(<QuestionPagePreview page={page} />);
+    expect(
+      wrapper.find(byTestAttr("additional-info")).find(DetailsContent)
+    ).toMatchSnapshot();
+  });
+});
