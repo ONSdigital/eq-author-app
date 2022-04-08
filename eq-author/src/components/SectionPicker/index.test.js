3cfd1b5cf10ab002fff01627f3fb9f796fc6c96e
farres1 <sam.farrell@ons.gov.uk>
2022-04-08 14:00:37 +0100

    Section picker interaction tests

--- eq-author/src/components/SectionPicker/index.test.js
+++ eq-author/src/components/SectionPicker/index.test.js
@@ -5,7 +5,7 @@ import { render, fireEvent } from "tests/utils/rtl";
 import SectionPicker from ".";
 import mockSections from "../../tests/mocks/mockSections.json";
 
-describe.only("SectionPicker", () => {
+describe("SectionPicker", () => {
   const defaultProps = {
     title: "Select the section(s) to import",
     isOpen: true,
@@ -14,7 +14,14 @@ describe.only("SectionPicker", () => {
     onClose: jest.fn(),
     onCancel: jest.fn(),
     startingSelectedSections: [],
-    sections: mockSections,
+    sections: [
+      ...mockSections,
+      {
+        id: "section3",
+        title: "",
+        displayName: "Pets 2",
+      },
+    ],
   };
 
   const renderSectionPicker = (args) =>
@@ -53,73 +60,71 @@ describe.only("SectionPicker", () => {
     });
   });
 
-  //   describe("Interaction testing", () => {
-  //     it("tests clicks on individual questions and registers the clicks", () => {
-  //       const { getByText } = renderSectionPicker({});
+  describe("Interaction testing", () => {
+    it("tests clicks on individual sections and registers the clicks", () => {
+      const { getByText } = renderSectionPicker({});
 
-  //       const firstQuestion = getByText("1a").closest("div");
-  //       fireEvent.click(firstQuestion);
+      const firstSection = getByText("Pets").closest("div");
+      fireEvent.click(firstSection);
 
-  //       expect(firstQuestion.getAttribute("aria-selected")).toBe("true");
+      expect(firstSection.getAttribute("aria-selected")).toBe("true");
 
-  //       fireEvent.click(firstQuestion);
-  //       expect(firstQuestion.getAttribute("aria-selected")).toBe("false");
-  //     });
-  //   });
+      fireEvent.click(firstSection);
+      expect(firstSection.getAttribute("aria-selected")).toBe("false");
+    });
+  });
 
-  //   describe("Testing the search UI", () => {
-  //     it("Types pet into the search bar and returns the question Why do you not have pets?", () => {
-  //       const { getByText, getByTestId, queryByText } = renderSectionPicker({});
+  describe("Testing the search UI", () => {
+    it("Types pet into the search bar and returns the Pets section", () => {
+      const { getByText, getByTestId, queryByText } = renderSectionPicker({});
 
-  //       const searchBar = getByTestId("search-bar");
+      const searchBar = getByTestId("search-bar");
 
-  //       fireEvent.change(searchBar, {
-  //         target: { value: "pet" },
-  //       });
+      fireEvent.change(searchBar, {
+        target: { value: "pet" },
+      });
 
-  //       expect(getByText("Why do you not have any pets?")).toBeInTheDocument();
-  //       expect(queryByText("Do you have a car?")).not.toBeInTheDocument();
-  //     });
+      expect(getByText("Pets")).toBeInTheDocument();
+      expect(queryByText("Cars")).not.toBeInTheDocument();
+    });
 
-  //     it("Types car into the search bar and returns the question Do you have a car && what colour car do you have?", () => {
-  //       const { getByText, getByTestId, queryByText } = renderSectionPicker({});
+    it("Types pet into the search bar and returns the sections Pets and Pets 2", () => {
+      const { getByText, getByTestId, queryByText } = renderSectionPicker({});
 
-  //       const searchBar = getByTestId("search-bar");
+      const searchBar = getByTestId("search-bar");
 
-  //       fireEvent.change(searchBar, {
-  //         target: { value: "car" },
-  //       });
+      fireEvent.change(searchBar, {
+        target: { value: "pet" },
+      });
 
-  //       expect(getByText("Do you have a car?")).toBeInTheDocument();
-  //       expect(getByText("What colour car do you have?")).toBeInTheDocument();
-  //       expect(
-  //         queryByText("Why do you not have any pets?")
-  //       ).not.toBeInTheDocument();
-  //     });
+      expect(queryByText("Cars")).not.toBeInTheDocument();
+      expect(getByText("Pets")).toBeInTheDocument();
+      expect(queryByText("Pets 2")).toBeInTheDocument();
+    });
 
-  //     it("Doesnt show the search bar if showSearch is false", () => {
-  //       const defaultPropsWithSearchFalse = {
-  //         title: "Select the question(s) to import",
-  //         isOpen: true,
-  //         showSearch: false,
-  //         warningPanel:
-  //           "You cannot import folders but you can import any questions they contain.",
-  //         onSubmit: jest.fn(),
-  //         onClose: jest.fn(),
-  //         onCancel: jest.fn(),
-  //         startingSelectedQuestions: [],
-  //         sections: mockSections,
-  //       };
+    // it("Doesnt show the search bar if showSearch is false", () => {
+    //   const defaultPropsWithSearchFalse = {
+    //     title: "Select the question(s) to import",
+    //     isOpen: true,
+    //     showSearch: false,
+    //     warningPanel:
+    //       "You cannot import folders but you can import any questions they contain.",
+    //     onSubmit: jest.fn(),
+    //     onClose: jest.fn(),
+    //     onCancel: jest.fn(),
+    //     startingSelectedQuestions: [],
+    //     sections: mockSections,
+    //   };
 
-  //       const renderSectionPicker = (args) =>
-  //         render(
-  //           <SectionPicker isOpen {...defaultPropsWithSearchFalse} {...args} />
-  //         );
+    //   const renderSectionPicker = (args) =>
+    //     render(
+    //       <SectionPicker isOpen {...defaultPropsWithSearchFalse} {...args} />
+    //     );
 
-  //       const { queryByTestId } = renderSectionPicker({});
-  //       const searchBar = queryByTestId("search-bar");
+    //   const { queryByTestId } = renderSectionPicker({});
+    //   const searchBar = queryByTestId("search-bar");
 
-  //       expect(searchBar).toBeNull();
-  //     });
-  //   });
+    //   expect(searchBar).toBeNull();
+    // });
+  });
 });
