08f0bc6315e07c6061c79862ca8e58f45894168f
jasehumphr <jason.humphries@ext.ons.gov.uk>
2022-04-28 11:02:11 +0100

    converted to lowercase

--- eq-author/src/App/SignInPage/PasswordStrength/index.js
+++ eq-author/src/App/SignInPage/PasswordStrength/index.js
@@ -2,6 +2,7 @@ const passwordStrength = (password) =>
   new Promise((resolve) => {
     fetch("/commonPasswordList.json").then((response) => {
       response.json().then((passwords) => {
+        password = password.toLowerCase();
         let found = passwords.includes(password);
         resolve(found);
       });
