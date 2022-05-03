c0ccb6f74d21b547802ade7614b8e009171b2d62
martyncolmer <martyc1974@hotmail.com>
2022-05-03 10:28:49 +0100

    fix for migrations

--- eq-author-api/middleware/runQuestionnaireMigrations.js
+++ eq-author-api/middleware/runQuestionnaireMigrations.js
@@ -4,35 +4,32 @@ const {
 } = require("../db/datastore");
 const { merge } = require("lodash");
 
-module.exports = (logger) => ({ currentVersion, migrations }) => async (
-  req,
-  res,
-  next
-) => {
-  if (!req.questionnaire || req.questionnaire.version === currentVersion) {
-    return next();
-  }
+module.exports =
+  (logger) =>
+  ({ currentVersion, migrations }) =>
+  async (req, res, next) => {
+    if (!req.questionnaire || req.questionnaire.version === currentVersion) {
+      return next();
+    }
 
-  try {
-    const questionnaireMeta = await getQuestionnaireMetaById(
-      req.questionnaire.id
-    );
-    merge(questionnaireMeta, { ...req.questionnaire });
-    req.questionnaire = questionnaireMeta;
-    req.questionnaire = await migrations
-      .slice(req.questionnaire.version)
-      .reduce((questionnaire, migration) => {
+    try {
+      const questionnaireMeta = await getQuestionnaireMetaById(
+        req.questionnaire.id
+      );
+      merge(questionnaireMeta, { ...req.questionnaire });
+      req.questionnaire = questionnaireMeta;
+      const migrationsToRun = migrations.slice(req.questionnaire.version);
+      migrationsToRun.forEach(async (migration) => {
         logger.info(
-          `Running migration for version ${migration.name} on ${questionnaire.id}`
+          `Running migration for version ${migration.name} on ${req.questionnaire.id}`
         );
-        return migration(questionnaire);
-      }, req.questionnaire);
-
-    req.questionnaire.version = currentVersion;
-    await saveQuestionnaire(req.questionnaire);
-    next();
-  } catch (e) {
-    logger.error(e);
-    next(e);
-  }
-};
+        req.questionnaire = await migration(req.questionnaire);
+      });
+      req.questionnaire.version = currentVersion;
+      await saveQuestionnaire(req.questionnaire);
+      next();
+    } catch (e) {
+      logger.error(e);
+      next(e);
+    }
+  };
