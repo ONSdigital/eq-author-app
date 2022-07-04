ca5f675ed68cde3ede5d5bd7466f723dd2edcdb5
farres1 <sam.farrell@ons.gov.uk>
2022-07-04 11:28:41 +0100

    Add migration to convert mutually exclusive options to new answer type

---
+++ eq-author-api/migrations/convertMutuallyExclusiveOptions.js
@@ -0,0 +1,36 @@
+const { getPageById } = require("../schema/resolvers/utils");
+const createAnswer = require("../src/businessLogic/createAnswer");
+const { enableOn } = require("../utils/featureFlag");
+
+module.exports = (questionnaire) => {
+  const ctx = { questionnaire };
+  if (enableOn(["mutuallyExclusiveAnswer"])) {
+    questionnaire.sections.forEach((section) => {
+      section.folders.forEach((folder) => {
+        folder.pages.forEach((page) => {
+          page.answers.forEach((answer) => {
+            if (answer.options !== undefined && answer.options !== null) {
+              answer.options.forEach((option) => {
+                if (option.mutuallyExclusive) {
+                  const mutuallyExclusiveAnswer = createAnswer(
+                    {
+                      type: "MutuallyExclusive",
+                      questionPageId: page.id,
+                    },
+                    getPageById(ctx, page.id)
+                  );
+
+                  mutuallyExclusiveAnswer.options[0].label = option.label;
+                  page.answers.push(mutuallyExclusiveAnswer);
+                  answer.options = undefined;
+                }
+              });
+            }
+          });
+        });
+      });
+    });
+  }
+
+  return questionnaire;
+};
