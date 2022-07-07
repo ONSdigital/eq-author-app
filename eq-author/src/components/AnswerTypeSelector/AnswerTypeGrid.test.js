2b9f99a44c83fe92d22958ca2b21f755fe29d5ec
farres1 <sam.farrell@ons.gov.uk>
2022-07-07 14:31:24 +0100

    Revert tests

--- eq-author/src/components/AnswerTypeSelector/AnswerTypeGrid.test.js
+++ eq-author/src/components/AnswerTypeSelector/AnswerTypeGrid.test.js
@@ -1,22 +1,10 @@
 import React from "react";
 import { render, fireEvent } from "tests/utils/rtl";
-import config from "config";
 
 import AnswerTypeGrid, { buttons } from "./AnswerTypeGrid";
 
 describe("components/AnswerTypeGrid", () => {
   let handleClose, handleSelect;
-
-  const renderAnswerTypeGrid = (props) => {
-    return render(
-      <AnswerTypeGrid
-        onClose={handleClose}
-        onSelect={handleSelect}
-        aria-labelledby="foo"
-        {...props}
-      />
-    );
-  };
 
   beforeEach(() => {
     handleClose = jest.fn();
@@ -24,7 +12,13 @@ describe("components/AnswerTypeGrid", () => {
   });
 
   it("should render", () => {
-    const { getByText } = renderAnswerTypeGrid();
+    const { getByText } = render(
+      <AnswerTypeGrid
+        onClose={handleClose}
+        onSelect={handleSelect}
+        aria-labelledby="foo"
+      />
+    );
     buttons.forEach(({ title }) => {
       getByText(title);
     });
@@ -33,40 +27,19 @@ describe("components/AnswerTypeGrid", () => {
   describe("when answer type button clicked", () => {
     it("should invoke `onSelect` callback", () => {
       const answerType = "Radio";
-      const { getByText } = renderAnswerTypeGrid({ radioEnabled: true });
-      const button = getByText(answerType);
-      fireEvent.click(button);
-
-      expect(handleClose).toHaveBeenCalled();
-      expect(handleSelect).toHaveBeenCalledWith(answerType);
-    });
-  });
-
-  describe("mutually exclusive", () => {
-    it("should not display mutually exclusive on AWS", () => {
-      const { queryByText } = render(
+      const { getByText } = render(
         <AnswerTypeGrid
           onClose={handleClose}
           onSelect={handleSelect}
           aria-labelledby="foo"
           radioEnabled
-          mutuallyExclusiveEnabled
         />
       );
+      const button = getByText(answerType);
+      fireEvent.click(button);
 
-      expect(queryByText(/OR answer/)).not.toBeInTheDocument();
-    });
-
-    it("should display mutually exclusive on GCP", () => {
-      process.env.FEATURE_FLAGS = "mutuallyExclusiveAnswer";
-      console.log(
-        "config.REACT_APP_FEATURE_FLAGS",
-        config.REACT_APP_FEATURE_FLAGS
-      );
-
-      const { queryByText } = renderAnswerTypeGrid();
-
-      expect(queryByText(/OR answer/)).toBeInTheDocument();
+      expect(handleClose).toHaveBeenCalled();
+      expect(handleSelect).toHaveBeenCalledWith(answerType);
     });
   });
 });
--- eq-author/src/utils/featureFlags/index.js
+++ eq-author/src/utils/featureFlags/index.js
@@ -2,7 +2,6 @@ import config from "config";
 
 export const enableOn = (flags) => {
   const enabledFlags = config.REACT_APP_FEATURE_FLAGS.split(",");
-  console.log("config.REACT_APP_FEATURE_FLAGS", config.REACT_APP_FEATURE_FLAGS);
 
   const displayFeature = flags.every((flag) => enabledFlags.includes(flag));
 
