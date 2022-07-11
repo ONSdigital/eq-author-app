048dcbd7f7c64213ab71003560ac2bbd161e848f
farres1 <sam.farrell@ons.gov.uk>
2022-07-11 12:27:23 +0100

    Remove unused test

--- eq-author-api/migrations/convertMutuallyExclusiveOptions.test.js
+++ eq-author-api/migrations/convertMutuallyExclusiveOptions.test.js
@@ -37,23 +37,7 @@ describe("convertMutuallyExclusiveOptions", () => {
     };
   });
 
-  it("should not convert mutually exclusive to mutually exclusive answer type when feature flag is not set", () => {
-    const updatedQuestionnaire = convertMutuallyExclusiveOptions(questionnaire);
-    const updatedQuestionnaireAnswers =
-      updatedQuestionnaire.sections[0].folders[0].pages[0].answers;
-
-    expect(updatedQuestionnaireAnswers.length).toBe(1);
-    expect(updatedQuestionnaireAnswers[0].options.length).toBe(1);
-    expect(updatedQuestionnaireAnswers[0].type).toBe("Number");
-    expect(updatedQuestionnaireAnswers[0].options[0]).toMatchObject({
-      id: "exclusive-1",
-      label: "No pets",
-      mutuallyExclusive: true,
-    });
-    expect(updatedQuestionnaireAnswers[1]).toBeUndefined();
-  });
-
-  it("should convert mutually exclusive to mutually exclusive answer type with correct data when feature flag is set", () => {
+  it("should convert mutually exclusive to mutually exclusive answer type with correct data", () => {
     const updatedQuestionnaire = convertMutuallyExclusiveOptions(questionnaire);
     const updatedQuestionnaireAnswers =
       updatedQuestionnaire.sections[0].folders[0].pages[0].answers;
