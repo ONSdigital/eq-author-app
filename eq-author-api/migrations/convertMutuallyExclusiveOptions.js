dcae641480c3ed2aeffe68d80f5bab491b635797
farres1 <sam.farrell@ons.gov.uk>
2022-07-19 11:24:02 +0100

    Add conditionals to migration

--- eq-author-api/migrations/convertMutuallyExclusiveOptions.js
+++ eq-author-api/migrations/convertMutuallyExclusiveOptions.js
@@ -3,10 +3,10 @@ const createAnswer = require("../src/businessLogic/createAnswer");
 
 module.exports = (questionnaire) => {
   const ctx = { questionnaire };
-  questionnaire.sections.forEach((section) => {
-    section.folders.forEach((folder) => {
-      folder.pages.forEach((page) => {
-        page.answers.forEach((answer) => {
+  questionnaire?.sections?.forEach((section) => {
+    section?.folders?.forEach((folder) => {
+      folder?.pages?.forEach((page) => {
+        page?.answers?.forEach((answer) => {
           if (answer.options !== undefined && answer.options !== null) {
             answer.options.forEach((option) => {
               if (option.mutuallyExclusive) {
