95bc00283d65aee26af2ab3efa153e7081cdae34
shelleytruong <shelleytruong@outlook.com>
2022-08-02 16:50:06 +0100

    fix bug

--- eq-author/src/App/page/Design/answers/MultipleChoiceAnswer/Option.js
+++ eq-author/src/App/page/Design/answers/MultipleChoiceAnswer/Option.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { useMutation } from "@apollo/react-hooks";
 import styled from "styled-components";
 import { colors, radius } from "constants/theme";
@@ -136,6 +136,13 @@ export const StatelessOption = ({
       });
     }
     return allCheckboxAnswers;
+  };
+
+  //sets option.dynamicAnswer to false if there are no previous checkbox answers with more than one option
+  const checkValidCheckboxAnswers = () => {
+    if (getCheckboxAnswers().length === 0) {
+      option.dynamicAnswer = false;
+    }
   };
 
   const handleDeleteClick = () => onDelete(option.id);
@@ -290,6 +297,10 @@ export const StatelessOption = ({
       (checkboxAnswer) => checkboxAnswer.id === option.dynamicAnswerID
     );
   };
+
+  useEffect(() => {
+    checkValidCheckboxAnswers();
+  });
 
   return (
     <StyledOption id={getIdForObject(option)} key={option.id}>
