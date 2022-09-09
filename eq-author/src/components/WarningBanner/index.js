3da13b6ca5208cddc82e632c79fd8acc1fcf46d4
sudeep <sudeep.kunhikannan@ons.gov.uk>
2022-09-09 09:03:14 +0100

    Set the warning icon position, displayed the warning in the sign in page
    Signed-off-by: sudeep <sudeep.kunhikannan@ons.gov.uk>

--- eq-author/src/components/WarningBanner/index.js
+++ eq-author/src/components/WarningBanner/index.js
@@ -7,7 +7,7 @@ import WarningIcon from "./icon-warning.svg?inline";
 
 const Banner = styled.div`
   background-color: ${colors.red};
-  height: 6em;
+  height: 7em;
   display: flex;
 `;
 
@@ -15,10 +15,10 @@ const WarningMessage = styled(IconText)`
   display: flex;
   color: ${colors.white};
   padding: 1em;
-  margin-bottom: 3em;
+  margin-bottom: 2.7em;
   font-weight: bold;
   width: 20em;
-  font-size: 1.1em;
+  font-size: 1.2em;
 `;
 
 const BannerMessageContainer = styled.div`
@@ -27,8 +27,7 @@ const BannerMessageContainer = styled.div`
   flex-direction: column;
   width: 100em;
   font-weight: bold;
-  font-size: 0.9em;
-  margin-top: 0.7em;
+  margin-top: 1em;
 `;
 
 const Content = styled.div`
