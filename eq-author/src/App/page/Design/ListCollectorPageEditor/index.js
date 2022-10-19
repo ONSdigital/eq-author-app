ffa229553f7c7d9b7272fc925b91585506b2068f
Joshua Chapman <joshua.chapman@ons.gov.uk>
2022-10-19 08:20:40 +0100

    EAR 1869 Additional Guidance for Driving Questions

--- eq-author/src/App/page/Design/ListCollectorPageEditor/index.js
+++ eq-author/src/App/page/Design/ListCollectorPageEditor/index.js
@@ -495,6 +495,7 @@ const UnwrappedListCollectorPageEditor = (props) => {
                 />
               </Field>
             </RadioContainer>
+            Something here
           </Collapsible>
 
           <Collapsible
