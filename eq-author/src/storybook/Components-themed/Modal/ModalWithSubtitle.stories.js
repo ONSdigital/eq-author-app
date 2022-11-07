0caeeb4663092e366eae9fddc8b0bb711cd44465
farres1 <sam.farrell@ons.gov.uk>
2022-11-07 16:31:45 +0000

    Add modal Storybook files

---
+++ eq-author/src/storybook/Components-themed/Modal/ModalWithSubtitle.stories.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ModalComponent from "components-themed/Modal";
+import {
+  Title,
+  Primary,
+  ArgsTable,
+  Stories,
+  PRIMARY_STORY,
+} from "@storybook/addon-docs/blocks";
+
+import useModal from "hooks/useModal";
+
+export const ModalWithSubtitle = (props) => {
+  const [trigger, Modal] = useModal({
+    ...props,
+    title: "Delete questionnaire",
+    subtitle: "Questionnaire name",
+    warningMessage: "All questionnaire contents will be deleted",
+    positiveButtonText: "Delete",
+    component: ModalComponent,
+  });
+
+  return (
+    <>
+      <Modal {...props} />
+      <button onClick={trigger}>Display modal</button>
+    </>
+  );
+};
+
+export default {
+  title: "Themed components/ONS/Modal",
+  component: ModalComponent,
+  argTypes: {
+    onConfirm: { action: "Confirmed" },
+    onCancel: { action: "Cancelled" },
+  },
+  parameters: {
+    docs: {
+      page: () => (
+        <>
+          <Title />
+          <p>Modal with warning</p>
+          <ArgsTable story={PRIMARY_STORY} />
+          <Primary />
+          <Stories />
+        </>
+      ),
+    },
+  },
+};
