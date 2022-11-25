e16fd31f5a56b2e6e932245a12420a778952df8a
farres1 <sam.farrell@ons.gov.uk>
2022-11-25 08:34:07 +0000

    Add delete introduction modal

--- eq-author/src/App/introduction/IntroductionHeader.js
+++ eq-author/src/App/introduction/IntroductionHeader.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useMutation } from "@apollo/react-hooks";
 import CustomPropTypes from "custom-prop-types";
 
 import { Toolbar, ToolbarButtonContainer } from "components/Toolbar";
 import IconButtonDelete from "components/buttons/IconButtonDelete";
+import Modal from "components-themed/Modal";
 
 import DELETE_INTRODUCTION from "graphql/deleteIntroductionPage.graphql";
+import {
+  DELETE_INTRODUCTION_PAGE_TITLE,
+  DELETE_BUTTON_TEXT,
+} from "constants/modal-content";
 
 const IntroductionToolbar = styled(Toolbar)`
   padding: 1.5em 2em 0;
@@ -14,6 +19,7 @@ const IntroductionToolbar = styled(Toolbar)`
 
 const IntroductionHeader = ({ history }) => {
   const [deleteIntroduction] = useMutation(DELETE_INTRODUCTION);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const onDeleteIntroduction = () => {
     deleteIntroduction();
@@ -22,13 +28,22 @@ const IntroductionHeader = ({ history }) => {
   };
 
   return (
-    <IntroductionToolbar>
-      <ToolbarButtonContainer>
-        <IconButtonDelete onClick={onDeleteIntroduction}>
-          Delete
-        </IconButtonDelete>
-      </ToolbarButtonContainer>
-    </IntroductionToolbar>
+    <>
+      <Modal
+        title={DELETE_INTRODUCTION_PAGE_TITLE}
+        positiveButtonText={DELETE_BUTTON_TEXT}
+        isOpen={showDeleteModal}
+        onConfirm={() => onDeleteIntroduction()}
+        onClose={() => setShowDeleteModal(false)}
+      />
+      <IntroductionToolbar>
+        <ToolbarButtonContainer>
+          <IconButtonDelete onClick={() => setShowDeleteModal(true)}>
+            Delete
+          </IconButtonDelete>
+        </ToolbarButtonContainer>
+      </IntroductionToolbar>
+    </>
   );
 };
 
