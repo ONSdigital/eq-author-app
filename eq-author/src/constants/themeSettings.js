ee62fde517e737bb2e75c1221b4d6f13e0c1529e
farres1 <sam.farrell@ons.gov.uk>
2022-12-01 12:35:13 +0000

    Add social theme comment

--- eq-author/src/constants/themeSettings.js
+++ eq-author/src/constants/themeSettings.js
@@ -1,8 +1,8 @@
+// Add `social: "Social theme"` if required in future
 export const THEME_TITLES = {
   default: "GB theme",
   northernireland: "NI theme",
   ukis: "UKIS theme",
   ukis_ni: "UKIS NI theme", //eslint-disable-line
   orr: "Office of Rail and Road theme",
-  social: "Social theme",
 };
