9ef988b241d77b65bb119386731c95e94edbb7be
martyncolmer <martyc1974@hotmail.com>
2023-01-06 11:10:28 +0000

    updated text

--- eq-author/src/constants/themeSettings.js
+++ eq-author/src/constants/themeSettings.js
@@ -3,5 +3,5 @@ export const THEME_TITLES = {
   default: "GB theme",
   northernireland: "NI theme",
   orr: "Office of Rail and Road theme",
-  beis: "Department for Business, Energy and Industrial Strategy",
+  beis: "Department for Business, Energy and Industrial Strategy theme",
 };
