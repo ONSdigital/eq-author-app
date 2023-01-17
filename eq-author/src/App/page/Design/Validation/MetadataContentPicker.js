dbdfddf8dc8b68e5f9ad57dc70649a1a3c4f3504
sudeep <sudeep.kunhikannan@ons.gov.uk>
2023-01-17 11:45:43 +0000

    Refactored metadata content picker v3
    Signed-off-by: sudeep <sudeep.kunhikannan@ons.gov.uk>

--- eq-author/src/App/page/Design/Validation/MetadataContentPicker.js
+++ eq-author/src/App/page/Design/Validation/MetadataContentPicker.js
@@ -1,20 +1,21 @@
 import React from "react";
-import ContentPickerSelect from "components/ContentPickerSelect";
-import { METADATA } from "components/ContentPickerSelect/content-types";
+import ContentPickerSelect from "components/ContentPickerSelectv3";
+import { METADATA } from "components/ContentPickerSelectv3/content-types";
 import { useQuestionnaire } from "components/QuestionnaireContext";
 
 import { DATE } from "constants/metadata-types";
 
 export const MetadataContentPicker = ({ ...otherProps }) => {
   const { questionnaire } = useQuestionnaire();
-
+  const data = {
+    [METADATA]:
+      questionnaire?.metadata?.filter(({ type }) => type === DATE.value) || [],
+  };
   return (
     <ContentPickerSelect
       name="metadata"
       contentTypes={[METADATA]}
-      metadataData={
-        questionnaire?.metadata?.filter(({ type }) => type === DATE.value) || []
-      }
+      data={data}
       {...otherProps}
     />
   );
