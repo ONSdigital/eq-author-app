5c4798541c5e3f5938c6994866a3a94251baa0de
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-02-01 14:31:25 +0000

    Changes

--- eq-author/src/App/importingContent/index.test.js
+++ eq-author/src/App/importingContent/index.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ImportingContent from "./";
-import { render, screen } from "tests/utils/rtl";
+import { render } from "tests/utils/rtl";
 import { useMutation, useQuery } from "@apollo/react-hooks";
 import QuestionnaireContext from "components/QuestionnaireContext";
 
@@ -12,40 +12,40 @@ jest.mock("@apollo/react-hooks", () => ({
 }));
 
 useMutation.mockImplementation(jest.fn(() => [jest.fn()]));
-useQuery.mockImplementation(jest.fn(() => [jest.fn()]));
+
+const destinationQuestionnaire = buildQuestionnaire({ answerCount: 1 });
+
+const sourceQuestionnaires = [
+  {
+    id: "source-questionnaire-id",
+    title: "Source questionnaire",
+  },
+];
+
+const setImportingContent = jest.fn();
+
+const renderImportingContent = (props) =>
+  render(
+    <QuestionnaireContext.Provider value={destinationQuestionnaire}>
+      <ImportingContent
+        questionnaires={sourceQuestionnaires}
+        stopImporting={() => setImportingContent(false)}
+        targetInsideFolder
+        {...props}
+      />
+    </QuestionnaireContext.Provider>
+  );
+
+useQuery.mockImplementation(() => ({
+  loading: false,
+  error: false,
+  data: { sourceQuestionnaires },
+}));
 
 describe("Importing content", () => {
   it("should render ImportingContent modal", () => {
-    const sourceQuestionnaires = [
-      {
-        id: "16b650e8-b7cd-4490-8f65-c7631e03bf95",
-        title: "MC",
-        __typename: "Questionnaire",
-      },
-    ];
-
-    const setImportingContent = jest.fn();
-    const destinationQuestionnaire = buildQuestionnaire({ answerCount: 1 });
-
-    const renderImportingContent = (props) =>
-      render(
-        <QuestionnaireContext.Provider
-          value={destinationQuestionnaire}
-          {...props}
-        >
-          <ImportingContent
-            questionnaires={sourceQuestionnaires}
-            stopImporting={() => setImportingContent(false)}
-            targetInsideFolder
-            {...props}
-          />
-        </QuestionnaireContext.Provider>
-      );
-
     const { getByTestId } = renderImportingContent();
-    screen.debug();
 
-    //expect(getByTestId("questionnaire-select-modal")).toBeInTheDocument();
-    expect(screen.queryByText(/Select the source questionnaire/)).toBeTruthy();
+    expect(getByTestId("questionnaire-select-modal")).toBeInTheDocument();
   });
 });
