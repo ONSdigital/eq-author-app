c1a54e6bcf63c68aa02bb913e264833ef454f1be
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-08 13:26:20 +0000

    Add dataset table

--- eq-author/src/App/dataSettings/ONSDataset/index.js
+++ eq-author/src/App/dataSettings/ONSDataset/index.js
@@ -8,15 +8,31 @@ import * as Common from "../common";
 import { Grid, Column } from "components/Grid";
 import Header from "components/EditorLayout/Header";
 import ScrollPane from "components/ScrollPane";
+import Button from "components-themed/buttons";
 
+import Theme from "contexts/themeContext";
 import { colors } from "constants/theme";
 import Icon from "assets/icon-select.svg";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableColumn,
+  TableHeadColumn,
+} from "components/datatable/Elements";
 
 const StyledTitle = styled.h2`
   font-size: 1.1em;
   font-weight: bold;
   color: ${colors.text};
   margin: 0 0 0.5em 0;
+`;
+const Title = styled.h2`
+  font-size: 1.2em;
+  font-weight: bold;
+  color: ${colors.text};
+  margin: 1.5em 0 1em 0;
 `;
 
 const Option = styled.option``;
@@ -38,6 +54,20 @@ const CustomSelect = styled.select`
   &:hover {
     outline: none;
   }
+`;
+
+const StyledTableBody = styled(TableBody)`
+  background-color: white;
+`;
+
+const SpacedTableColumn = styled(TableColumn)`
+  padding: 0.5em 0.5em 0.2em;
+  color: ${colors.text};
+  word-break: break-word;
+`;
+
+const StyledButton = styled(Button)`
+  flex: 1;
 `;
 
 const lists = [
@@ -62,58 +92,101 @@ const ONSDatasetPage = () => {
   };
 
   return (
-    <Common.Container>
-      <ScrollPane>
-        <Header title={Common.headerTitle} />
-        <Common.PageContainer tabIndex="-1" className="keyNav">
-          <Common.PageMainCanvas>
-            <Grid>
-              <VerticalTabs
-                title={Common.navHeading}
-                cols={2.5}
-                tabItems={Common.tabItems({
-                  params,
-                })}
-              />
-              <Column gutters={false} cols={9.5}>
-                <Common.SampleFileDataContainer>
-                  <Common.StyledPanel>
-                    <Common.TabTitle>
-                      Select an ONS dataset to link to
-                    </Common.TabTitle>
-
-                    <Common.TabContent>
-                      Linking to an ONS dataset will allow you to pipe data that
-                      respondents have provided in previous questionnaires into
-                      question titles or percentage answer type labels.
-                    </Common.TabContent>
-
-                    <Common.TabContent>
-                      Only one dataset can be linked per questionnaire.
-                    </Common.TabContent>
-                    <StyledTitle>Select a survey ID</StyledTitle>
-                    <CustomSelect
-                      name="listId"
-                      data-test="list-select"
-                      onChange={handleChange}
-                      value={surveyID}
-                    >
-                      <Option value="surveyID">Survey ID</Option>
-                      {lists.map((list) => (
-                        <Option key={list.id} value={list.id}>
-                          {list.displayName}
-                        </Option>
-                      ))}
-                    </CustomSelect>
-                    {surveyID && <p>{surveyID}</p>}
-                  </Common.StyledPanel>
-                </Common.SampleFileDataContainer>
-              </Column>
-            </Grid>
-          </Common.PageMainCanvas>
-        </Common.PageContainer>
-      </ScrollPane>
-    </Common.Container>
+    <Theme themeName="onsLegacyFont">
+      <Common.Container>
+        <ScrollPane>
+          <Header title={Common.headerTitle} />
+          <Common.PageContainer tabIndex="-1" className="keyNav">
+            <Common.PageMainCanvas>
+              <Grid>
+                <VerticalTabs
+                  title={Common.navHeading}
+                  cols={2.5}
+                  tabItems={Common.tabItems({
+                    params,
+                  })}
+                />
+                <Column gutters={false} cols={9.5}>
+                  <Common.SampleFileDataContainer>
+                    <Common.StyledPanel>
+                      <Common.TabTitle>
+                        Select an ONS dataset to link to
+                      </Common.TabTitle>
+                      <Common.TabContent>
+                        Linking to an ONS dataset will allow you to pipe data
+                        that respondents have provided in previous
+                        questionnaires into question titles or percentage answer
+                        type labels.
+                      </Common.TabContent>
+                      <Common.TabContent>
+                        Only one dataset can be linked per questionnaire.
+                      </Common.TabContent>
+                      <StyledTitle>Select a survey ID</StyledTitle>
+                      <CustomSelect
+                        name="listId"
+                        data-test="list-select"
+                        onChange={handleChange}
+                        value={surveyID}
+                      >
+                        <Option value="surveyID">Survey ID</Option>
+                        {lists.map((list) => (
+                          <Option key={list.id} value={list.id}>
+                            {list.displayName}
+                          </Option>
+                        ))}
+                      </CustomSelect>
+                      {surveyID && (
+                        <>
+                          <Title>Datasets for survey ID {surveyID}</Title>
+                          <Table data-test="qcodes-table">
+                            <TableHead>
+                              <TableRow>
+                                <TableHeadColumn width="40%">
+                                  Version
+                                </TableHeadColumn>
+                                <TableHeadColumn width="40%">
+                                  Date created
+                                </TableHeadColumn>
+                                <TableHeadColumn width="20%">
+                                  Link dataset
+                                </TableHeadColumn>
+                              </TableRow>
+                            </TableHead>
+                            <StyledTableBody>
+                              <TableRow data-test={`answer-row-test`}>
+                                {
+                                  <>
+                                    <SpacedTableColumn>
+                                      Test 1
+                                    </SpacedTableColumn>
+                                    <SpacedTableColumn>
+                                      Test 2
+                                    </SpacedTableColumn>
+                                    <SpacedTableColumn>
+                                      <StyledButton
+                                        onClick={() => {}}
+                                        type="button"
+                                        variant="secondary"
+                                      >
+                                        Link
+                                      </StyledButton>
+                                    </SpacedTableColumn>
+                                  </>
+                                }
+                              </TableRow>
+                            </StyledTableBody>
+                          </Table>
+                        </>
+                      )}
+                    </Common.StyledPanel>
+                  </Common.SampleFileDataContainer>
+                </Column>
+              </Grid>
+            </Common.PageMainCanvas>
+          </Common.PageContainer>
+        </ScrollPane>
+      </Common.Container>
+    </Theme>
   );
 };
 
