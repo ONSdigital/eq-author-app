833268eac6feb12aab5368ea375f2ce8669b081e
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-15 14:42:29 +0000

    Add resolvers

--- eq-author-api/schema/tests/prePopSchemaVersions.test.js
+++ eq-author-api/schema/tests/prePopSchemaVersions.test.js
@@ -13,46 +13,46 @@ query GetPrepopSchemaVersions($id: ID!) {
   }
 `;
 
-// let surveyId;
-// const mocks = [
-//   {
-//     request: {
-//       query: query,
-//       variables: {
-//         id: surveyId,
-//       },
-//     },
-//     result: () => {
-//       return {
-//         data: {
-//           surveyId: "624",
-//           versions: [
-//             {
-//               id: "624-111-789",
-//               version: "1",
-//               dateCreated: "2023-01-12T13:37:27+00:00",
-//             },
-//             {
-//               id: "624-222-789",
-//               version: "2",
-//               dateCreated: "2023-02-08T12:37:27+00:00",
-//             },
-//             {
-//               id: "624-333-789",
-//               version: "3",
-//               dateCreated: "2023-03-23T08:37:27+00:00",
-//             },
-//           ],
-//         },
-//       };
-//     },
-//   },
-// ];
+let surveyId;
+const mocks = [
+  {
+    request: {
+      query: query,
+      variables: {
+        id: surveyId,
+      },
+    },
+    result: () => {
+      return {
+        data: {
+          surveyId: "624",
+          versions: [
+            {
+              id: "624-111-789",
+              version: "1",
+              dateCreated: "2023-01-12T13:37:27+00:00",
+            },
+            {
+              id: "624-222-789",
+              version: "2",
+              dateCreated: "2023-02-08T12:37:27+00:00",
+            },
+            {
+              id: "624-333-789",
+              version: "3",
+              dateCreated: "2023-03-23T08:37:27+00:00",
+            },
+          ],
+        },
+      };
+    },
+  },
+];
 
 describe("PrepopSchemaVersions", () => {
   it("should throw error if the ID is undefined", async () => {
     try {
-      await executeQuery(query);
+      await executeQuery(mocks.query);
     } catch (error) {
       expect(error.message).toMatch(
         `Variable "$id" of required type "ID!" was not provided.`
