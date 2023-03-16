5a544c0a724ec4689289808c7fc51cdd8a139915
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-16 16:12:35 +0000

    Update prepop api test

--- eq-author-api/schema/tests/prePopSchemaVersions.test.js
+++ eq-author-api/schema/tests/prePopSchemaVersions.test.js
@@ -1,5 +1,8 @@
 const executeQuery = require("../../tests/utils/executeQuery");
 
+jest.mock("node-fetch");
+
+const fetch = require("node-fetch");
 const query = `
 query GetPrepopSchemaVersions($id: ID!) {
     prepopSchemaVersions(id: $id) {
@@ -13,46 +16,36 @@ query GetPrepopSchemaVersions($id: ID!) {
   }
 `;
 
-let surveyId;
-const mocks = [
-  {
-    request: {
-      query: query,
-      variables: {
-        id: surveyId,
-      },
-    },
-    result: () => {
-      return {
-        data: {
-          surveyId: "624",
-          versions: [
-            {
-              id: "624-111-789",
-              version: "1",
-              dateCreated: "2023-01-12T13:37:27+00:00",
-            },
-            {
-              id: "624-222-789",
-              version: "2",
-              dateCreated: "2023-02-08T12:37:27+00:00",
-            },
-            {
-              id: "624-333-789",
-              version: "3",
-              dateCreated: "2023-03-23T08:37:27+00:00",
-            },
-          ],
+fetch.mockImplementation(() =>
+  Promise.resolve({
+    status: 200,
+    json: () => ({
+      surveyId: "123",
+      versions: [
+        {
+          id: "123-111-789",
+          version: "1",
+          dateCreated: "2023-01-12T13:37:27+00:00",
         },
-      };
-    },
-  },
-];
+        {
+          id: "123-222-789",
+          version: "2",
+          dateCreated: "2023-02-08T12:37:27+00:00",
+        },
+        {
+          id: "123-333-789",
+          version: "3",
+          dateCreated: "2023-03-23T08:37:27+00:00",
+        },
+      ],
+    }),
+  })
+);
 
 describe("PrepopSchemaVersions", () => {
   it("should throw error if the ID is undefined", async () => {
     try {
-      await executeQuery(mocks.query);
+      await executeQuery(query);
     } catch (error) {
       expect(error.message).toMatch(
         `Variable "$id" of required type "ID!" was not provided.`
@@ -60,8 +53,34 @@ describe("PrepopSchemaVersions", () => {
     }
   });
 
-  //   it("should query prepop schema", async () => {
-  //     const response = await executeQuery(query, { id: "121" });
-  //     console.log("response", response);
-  //   });
+  it("should query prepop schema", async () => {
+    const surveyId = "123";
+    const expectedResponse = {
+      data: {
+        prepopSchemaVersions: {
+          surveyId: "123",
+          versions: [
+            {
+              id: "123-111-789",
+              version: "1",
+              dateCreated: "2023-01-12T13:37:27+00:00",
+            },
+            {
+              id: "123-222-789",
+              version: "2",
+              dateCreated: "2023-02-08T12:37:27+00:00",
+            },
+            {
+              id: "123-333-789",
+              version: "3",
+              dateCreated: "2023-03-23T08:37:27+00:00",
+            },
+          ],
+        },
+      },
+    };
+
+    const response = await executeQuery(query, { id: surveyId });
+    expect(response).toMatchObject(expectedResponse);
+  });
 });
