dd1aedeea5cf0d1e67937325e3c97a4a94c6cf2c
Jason Lavarinhas <jason.da.cunha.lavarinhas@ons.gov.uk>
2023-03-21 13:51:30 +0000

    Update eq-author/src/App/section/Design/SectionEditor/RepeatingSection/index.js
    Co-authored-by: farres1 <71004932+farres1@users.noreply.github.com>

--- eq-author/src/App/section/Design/SectionEditor/RepeatingSection/index.js
+++ eq-author/src/App/section/Design/SectionEditor/RepeatingSection/index.js
@@ -116,7 +116,7 @@ const RepeatingSection = ({ section, handleUpdate }) => {
         item from the list forms a section on the Hub. The repeating section is
         used to ask the same questions for each item selected.
       </Caption>
-      <InlineField disabled={Boolean(!section.allowRepeatingSection)}>
+      <InlineField disabled={!section.allowRepeatingSection}>
         <Label htmlFor="repeating-section">Repeating section</Label>
         <ToggleWrapper>
           <ToggleSwitch
