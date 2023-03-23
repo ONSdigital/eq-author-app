720ea4cfd70f0965a46b8a27e56c0e2f14b77da1
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-23 15:54:53 +0000

    Add toggle

--- eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
+++ eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
@@ -3,33 +3,45 @@ import styled from "styled-components";
 import InlineField from "components/AnswerContent/Format/InlineField";
 import ToggleSwitch from "components/buttons/ToggleSwitch";
 
-const Wrapper = styled.div`
-  margin-top: 0.7em;
+const ToggleWrapper = styled.div`
+  opacity: ${({ disabled }) => (disabled ? "0.6" : "1")};
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
 `;
 
-const StyledToggleSwitch = styled(ToggleSwitch)`
-  display: inline;
-`;
+const Paragraph = styled.p``;
 
 const RepeatLabelAndInput = () => {
+  const repeatLabelAndInput = null;
   return (
-    <Wrapper>
-      <InlineField
-        id="repeat-label-and-input"
-        htmlFor="repeat-label-and-input"
-        label="Repeat label and input"
-      />
-      Repeat this label and input for each answer added to the linked collection
-      list. Each answer will be piped into a separate label.
-      <StyledToggleSwitch
-        id="repeat-label-and-input-toggle"
-        name="repeat-label-and-input-toggle"
-        hideLabels={false}
-        onChange={() => {}}
-        data-test="repeat-label-and-input-toggle"
-        checked={false}
-      />
-    </Wrapper>
+    <>
+      <ToggleWrapper>
+        <InlineField
+          id="repeat-label-and-input"
+          htmlFor="repeat-label-and-input"
+          label="Repeat label and input"
+        >
+          <Paragraph>
+            <br />
+            Repeat this label and input for each answer added to the linked
+            collection list. Each answer will be piped into a separate label.
+          </Paragraph>
+        </InlineField>
+        <InlineField
+          id="repeat-label-and-input"
+          htmlFor="repeat-label-and-input"
+          label=""
+        >
+          <ToggleSwitch
+            id="repeat-label-and-input-toggle"
+            name="repeat-label-and-input-toggle"
+            hideLabels={false}
+            onChange={() => {}}
+            data-test="repeat-label-and-input-toggle"
+            checked={repeatLabelAndInput || false}
+          />
+        </InlineField>
+      </ToggleWrapper>
+    </>
   );
 };
 
