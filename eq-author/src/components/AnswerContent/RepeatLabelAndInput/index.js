7066806f2c04fc6e15108a3886468457e406b990
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-23 11:01:02 +0000

    Create component

---
+++ eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import InlineField from "components/AnswerContent/Format/InlineField";
+
+const RepeatLabelAndInput = () => {
+  return (
+    <>
+      <InlineField
+        id="repeat-label-and-input"
+        htmlFor="repeat-label-and-input"
+        label="Repeat label and input"
+      />
+    </>
+  );
+};
+
+export default RepeatLabelAndInput;
