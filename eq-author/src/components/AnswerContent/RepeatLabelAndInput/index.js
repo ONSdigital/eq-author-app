e44d20e15bb57149de35bef3f0a1cb597c0ed619
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-24 11:18:03 +0000

    Fix padding left

--- eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
+++ eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
@@ -3,45 +3,53 @@ import styled from "styled-components";
 import InlineField from "components/AnswerContent/Format/InlineField";
 import ToggleSwitch from "components/buttons/ToggleSwitch";
 
+import { Label } from "components/Forms";
+
 const ToggleWrapper = styled.div`
+  margin: 0.7em 0 0 0;
   opacity: ${({ disabled }) => (disabled ? "0.6" : "1")};
   pointer-events: ${({ disabled }) => (disabled ? "none" : "auto")};
 `;
 
-const Paragraph = styled.p``;
+const Paragraph = styled.p`
+  margin: 0;
+`;
+
+const StyledInlineField = styled(InlineField)`
+  padding: 0;
+  margin: 0;
+`;
+
+const StyledToggleSwitch = styled(ToggleSwitch)`
+  margin: 0;
+`;
 
 const RepeatLabelAndInput = () => {
   const repeatLabelAndInput = null;
   return (
-    <>
-      <ToggleWrapper>
-        <InlineField
-          id="repeat-label-and-input"
-          htmlFor="repeat-label-and-input"
-          label="Repeat label and input"
-        >
-          <Paragraph>
-            <br />
-            Repeat this label and input for each answer added to the linked
-            collection list. Each answer will be piped into a separate label.
-          </Paragraph>
-        </InlineField>
-        <InlineField
-          id="repeat-label-and-input"
-          htmlFor="repeat-label-and-input"
-          label=""
-        >
-          <ToggleSwitch
-            id="repeat-label-and-input-toggle"
-            name="repeat-label-and-input-toggle"
-            hideLabels={false}
-            onChange={() => {}}
-            data-test="repeat-label-and-input-toggle"
-            checked={repeatLabelAndInput || false}
-          />
-        </InlineField>
-      </ToggleWrapper>
-    </>
+    <ToggleWrapper>
+      <Label>Repeat label and input</Label>
+      <Paragraph>
+        Repeat this label and input for each answer added to the linked
+        collection list. Each answer will be piped into a separate label.
+      </Paragraph>
+
+      <StyledInlineField
+        id="repeat-label-and-input"
+        htmlFor="repeat-label-and-input"
+        label=""
+      >
+        <StyledToggleSwitch
+          id="repeat-label-and-input-toggle"
+          name="repeat-label-and-input-toggle"
+          hideLabels={false}
+          onChange={() => {}}
+          data-test="repeat-label-and-input-toggle"
+          checked={repeatLabelAndInput || false}
+          blockDisplay
+        />
+      </StyledInlineField>
+    </ToggleWrapper>
   );
 };
 
