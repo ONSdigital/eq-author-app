7ec5a275b122f6b1c1239f389afce8d1ec6c1f47
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-03-24 15:01:27 +0000

    Add title

--- eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
+++ eq-author/src/components/AnswerContent/RepeatLabelAndInput/index.js
@@ -32,29 +32,36 @@ const RepeatLabelAndInput = () => {
   };
 
   return (
-    <ToggleWrapper>
-      <Label>Repeat label and input</Label>
-      <Paragraph>
-        Repeat this label and input for each answer added to the linked
-        collection list. Each answer will be piped into a separate label.
-      </Paragraph>
+    <>
+      <ToggleWrapper>
+        <Label>Repeat label and input</Label>
+        <Paragraph>
+          Repeat this label and input for each answer added to the linked
+          collection list. Each answer will be piped into a separate label.
+        </Paragraph>
 
-      <StyledInlineField
-        id="repeat-label-and-input"
-        htmlFor="repeat-label-and-input"
-        label=""
-      >
-        <StyledToggleSwitch
-          id="repeat-label-and-input-toggle"
-          name="repeat-label-and-input-toggle"
-          hideLabels={false}
-          onChange={handleChange}
-          data-test="repeat-label-and-input-toggle"
-          checked={toggleStatus}
-          blockDisplay
-        />
-      </StyledInlineField>
-    </ToggleWrapper>
+        <StyledInlineField
+          id="repeat-label-and-input"
+          htmlFor="repeat-label-and-input"
+          label=""
+        >
+          <StyledToggleSwitch
+            id="repeat-label-and-input-toggle"
+            name="repeat-label-and-input-toggle"
+            hideLabels={false}
+            onChange={handleChange}
+            data-test="repeat-label-and-input-toggle"
+            checked={toggleStatus}
+            blockDisplay
+          />
+        </StyledInlineField>
+      </ToggleWrapper>
+      {toggleStatus && (
+        <>
+          <Label>Linked collection list</Label>
+        </>
+      )}
+    </>
   );
 };
 
