4a5d618fd59ff4fca75b7a16f19d07c931f7e9e7
Joshua Chapman <joshua.chapman@ons.gov.uk>
2023-03-30 11:45:16 +0100

    fixed test

--- eq-author-api/migrations/updateBEIStoDBTTheme.test.js
+++ eq-author-api/migrations/updateBEIStoDBTTheme.test.js
@@ -3,49 +3,19 @@ const updateBEIStoDBTTheme = require("./updateBEIStoDBTTheme");
 describe("updateBEISThemes", () => {
   it("should update BEIS theme", () => {
     const questionnaire = {
-      themeSettings: {
-        id: "1",
-        previewTheme: "beis",
-        themes: [
-          {
-            enabled: true,
-            shortName: "beis",
-            legalBasisCode: "VOLUNTARY",
-            eqId: "123",
-            formType: "1234",
-            id: "beis",
-          },
-        ],
-      },
+      theme: "beis",
     };
 
     const updatedTheme = updateBEIStoDBTTheme(questionnaire);
-    expect(updatedTheme.themeSettings.themes[0].shortName).toBe("dbt_dsit");
-    expect(updatedTheme.themeSettings.themes[0].id).toBe("dbt_dsit");
-    expect(updatedTheme.themeSettings.previewTheme).toBe("dbt_dsit");
+    expect(updatedTheme.theme).toBe("dbt_dsit");
   });
 
   it("should update BEIS NI theme", () => {
     const questionnaire = {
-      themeSettings: {
-        id: "1",
-        previewTheme: "beis_ni",
-        themes: [
-          {
-            enabled: true,
-            shortName: "beis_ni",
-            legalBasisCode: "VOLUNTARY",
-            eqId: "123",
-            formType: "1234",
-            id: "beis_ni",
-          },
-        ],
-      },
+      theme: "beis_ni",
     };
 
     const updatedTheme = updateBEIStoDBTTheme(questionnaire);
-    expect(updatedTheme.themeSettings.themes[0].shortName).toBe("dbt_dsit_ni");
-    expect(updatedTheme.themeSettings.themes[0].id).toBe("dbt_dsit_ni");
-    expect(updatedTheme.themeSettings.previewTheme).toBe("dbt_dsit_ni");
+    expect(updatedTheme.theme).toBe("dbt_dsit_ni");
   });
 });
