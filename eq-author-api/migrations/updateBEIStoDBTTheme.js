8e92f4cdc9d950c478e52072632adb7d44187ae7
Joshua Chapman <joshua.chapman@ons.gov.uk>
2023-03-30 11:23:35 +0100

    fix migration

--- eq-author-api/migrations/updateBEIStoDBTTheme.js
+++ eq-author-api/migrations/updateBEIStoDBTTheme.js
@@ -1,20 +1,9 @@
 module.exports = (questionnaire) => {
-  questionnaire.themeSettings.themes.forEach((theme) => {
-    if (theme.shortName === "beis") {
-      theme.shortName = "dbt_dsit";
-      theme.id = "dbt_dsit";
-    }
-    if (theme.shortName === "beis_ni") {
-      theme.shortName = "dbt_dsit_ni";
-      theme.id = "dbt_dsit_ni";
-    }
-  });
-
-  if (questionnaire.themeSettings.previewTheme === "beis") {
-    questionnaire.themeSettings.previewTheme = "dbt_dsit";
+  if (questionnaire.theme === "beis") {
+    questionnaire.theme = "dbt_dsit";
   }
-  if (questionnaire.themeSettings.previewTheme === "beis_ni") {
-    questionnaire.themeSettings.previewTheme = "dbt_dsit_ni";
+  if (questionnaire.theme === "beis_ni") {
+    questionnaire.theme = "dbt_dsit_ni";
   }
   return questionnaire;
 };
