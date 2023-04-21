356d608a0269e2441fc84361a1e421969bd1bc71
farres1 <sam.farrell@ons.gov.uk>
2023-04-21 08:21:38 +0100

    Add empty section summary page title when section is created

--- eq-author-api/src/businessLogic/createSection.js
+++ eq-author-api/src/businessLogic/createSection.js
@@ -11,6 +11,7 @@ const createSection = (input = {}) => ({
   requiredCompleted: false,
   showOnHub: true,
   sectionSummary: false,
+  sectionSummaryPageDescription: "",
   repeatingSection: false,
   repeatingSectionListId: "",
   ...input,
