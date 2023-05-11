718effd88d99eb9d8b03181fc83a0e8761a640da
Tlamelo Masike <tlamelo.masike@ons.gov.uk>
2023-05-11 16:48:03 +0100

    Update piping

--- eq-author-api/src/businessLogic/onAnswerUpdated.js
+++ eq-author-api/src/businessLogic/onAnswerUpdated.js
@@ -1,18 +1,46 @@
 const { logger } = require("../../utils/logger");
+const cheerio = require("cheerio");
+
+const updatePipingValue = (htmlText, answerId, newValue) => {
+  if (!htmlText) {
+    return htmlText;
+  }
+  const htmlDoc = cheerio.load(htmlText, null, false);
+  const dataSpan = htmlDoc(`span[data-id=${answerId}]`);
+  dataSpan.each((i, elem) => {
+    elem.children[0].data = `[${newValue}]`;
+  });
+  return htmlDoc.html();
+};
 
 const updatePipingInAnswers = (oldAnswerLabel, updatedAnswer, pages) => {
   pages.forEach((page) => {
     const { title, description } = page;
     if (title?.includes(updatedAnswer.id)) {
-      page.title = title.replace(oldAnswerLabel, updatedAnswer.label);
+      page.title = updatePipingValue(
+        page.title,
+        updatedAnswer.id,
+        updatedAnswer.label.replace(/(<([^>]+)>)/gi, "")
+      );
     }
 
     if (description?.includes(updatedAnswer.id)) {
-      page.description = description.replace(
-        oldAnswerLabel,
-        updatedAnswer.label
+      page.description = updatePipingValue(
+        page.description,
+        updatedAnswer.id,
+        updatedAnswer.label.replace(/(<([^>]+)>)/gi, "")
       );
     }
+
+    page.answers?.forEach((answer) => {
+      if (answer.label.includes(updatedAnswer.id)) {
+        answer.label = updatePipingValue(
+          answer.label,
+          updatedAnswer.id,
+          updatedAnswer.label.replace(/(<([^>]+)>)/gi, "")
+        );
+      }
+    });
   });
 
   logger.info(`Piping In Answers Updated with ID ${updatedAnswer.id}`);
