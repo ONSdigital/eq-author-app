a0365a801b1d37f63a528a8fa13db83e211e5291
farres1 <sam.farrell@ons.gov.uk>
2023-06-26 16:21:36 +0100

    Remove preview front end toggle

--- eq-author/src/App/introduction/Design/IntroductionEditor/index.js
+++ eq-author/src/App/introduction/Design/IntroductionEditor/index.js
@@ -102,7 +102,7 @@ const IntroductionEditor = ({ introduction, history }) => {
     contactDetailsIncludeRuRef,
     additionalGuidancePanel,
     additionalGuidancePanelSwitch,
-    previewQuestions,
+    // previewQuestions,
     secondaryTitle,
     secondaryDescription,
     tertiaryTitle,
@@ -348,12 +348,9 @@ const IntroductionEditor = ({ introduction, history }) => {
           </InformationPanel>
         </Padding>
       </Section>
-      <Section>
+      {/* <Section>
         <Padding>
-          <InlineField
-            open={contactDetailsIncludeRuRef}
-            style={{ marginBottom: "0" }}
-          >
+          <InlineField open={previewQuestions} style={{ marginBottom: "0" }}>
             <Label htmlFor="toggle-preview-questions">Preview questions</Label>
             <ToggleSwitch
               id="toggle-preview-questions"
@@ -377,7 +374,7 @@ const IntroductionEditor = ({ introduction, history }) => {
             format.
           </SectionDescription>
         </Padding>
-      </Section>
+      </Section> */}
       <Section>
         <Padding>
           <SectionTitle style={{ marginBottom: "0" }}>
