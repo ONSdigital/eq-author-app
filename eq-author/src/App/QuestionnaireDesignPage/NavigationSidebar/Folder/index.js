07889c68b9490091de3e9814147e904147a06529
farres1 <sam.farrell@ons.gov.uk>
2023-06-28 08:51:50 +0100

    Fix missing error when linked collection list is deleted

--- eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/Folder/index.js
+++ eq-author/src/App/QuestionnaireDesignPage/NavigationSidebar/Folder/index.js
@@ -66,7 +66,9 @@ const Folder = ({
           <FolderNavItem
             title={displayName}
             open
-            icon={listId != null ? IconListCollectorFolder : IconBasicFolder} // `!= null` checks null and undefined
+            icon={
+              listId === undefined ? IconBasicFolder : IconListCollectorFolder
+            }
             disabled={isCurrentFolder(folderId, entityId)}
             titleUrl={buildFolderPath({
               questionnaireId,
