05d06fad235fc2f0452cfed20c77ab300071f1f5
farres1 <sam.farrell@ons.gov.uk>
2023-07-18 14:55:59 +0100

    Initial list collector folder migration rewrite

--- eq-author-api/migrations/convertListCollectorPageToFolder.js
+++ eq-author-api/migrations/convertListCollectorPageToFolder.js
@@ -1,114 +1,132 @@
 const { v4: uuidv4 } = require("uuid");
 const createFolder = require("../src/businessLogic/createFolder");
+
 const {
   createListCollectorQualifierPage,
   createListCollectorAddItemPage,
   createListCollectorConfirmationPage,
 } = require("../src/businessLogic/createListCollectorPages");
 
-module.exports = (questionnaire) => {
-  let convertedListCollectorFolder;
-  let folderPositionIterator = 1;
+const hasListCollectorPage = (folder) =>
+  folder.pages.some((page) => page.pageType === "ListCollectorPage");
 
+const generateListCollectorFolder = (page) => {
+  const generatedListCollectorFolder = {
+    id: page.id,
+    alias: page.alias,
+    title: page.title,
+    listId: page.listId,
+    pages: [
+      createListCollectorQualifierPage({
+        title: page.drivingQuestion,
+        pageDescription: page.pageDescription,
+        additionalGuidanceEnabled: page.additionalGuidancePanelSwitch,
+        additionalGuidanceContent: page.additionalGuidancePanel,
+        position: 0,
+        answers: [
+          {
+            type: "Radio",
+            qCode: page.drivingQCode,
+            options: [
+              {
+                id: uuidv4(),
+                label: page.drivingPositive,
+                description: page.drivingPositiveDescription,
+              },
+              {
+                id: uuidv4(),
+                label: page.drivingNegative,
+                description: page.drivingNegativeDescription,
+              },
+            ],
+          },
+        ],
+      }),
+      createListCollectorAddItemPage({
+        title: page.addItemTitle,
+        pageDescription: page.addItemPageDescription,
+        position: 1,
+      }),
+      createListCollectorConfirmationPage({
+        title: page.anotherTitle,
+        pageDescription: page.anotherPageDescription,
+        position: 2,
+        answers: [
+          {
+            type: "Radio",
+            qCode: page.anotherQCode,
+            options: [
+              {
+                id: uuidv4(),
+                label: page.anotherPositive,
+                description: page.anotherPositiveDescription,
+              },
+              {
+                id: uuidv4(),
+                label: page.anotherNegative,
+                description: page.anotherNegativeDescription,
+              },
+            ],
+          },
+        ],
+      }),
+    ],
+  };
+
+  return generatedListCollectorFolder;
+};
+
+module.exports = (questionnaire) => {
   questionnaire.sections.forEach((section) => {
     section.folders.forEach((folder) => {
-      // Uses for instead of forEach to prevent list collector pages being skipped when there are multiple list collector pages in folder
-      for (let pageIndex = 0; pageIndex < folder.pages.length; pageIndex++) {
-        const page = folder.pages[pageIndex];
-        if (page.pageType === "ListCollectorPage") {
-          // Creates list collector folder from list collector page data
-          convertedListCollectorFolder = {
-            id: page.id,
-            alias: page.alias,
-            title: page.title,
-            listId: page.listId,
-            pages: [
-              createListCollectorQualifierPage({
-                title: page.drivingQuestion,
-                pageDescription: page.pageDescription,
-                additionalGuidanceEnabled: page.additionalGuidancePanelSwitch,
-                additionalGuidanceContent: page.additionalGuidancePanel,
-                position: 0,
-                answers: [
-                  {
-                    type: "Radio",
-                    qCode: page.drivingQCode,
-                    options: [
-                      {
-                        id: uuidv4(),
-                        label: page.drivingPositive,
-                        description: page.drivingPositiveDescription,
-                      },
-                      {
-                        id: uuidv4(),
-                        label: page.drivingNegative,
-                        description: page.drivingNegativeDescription,
-                      },
-                    ],
-                  },
-                ],
-              }),
-              createListCollectorAddItemPage({
-                title: page.addItemTitle,
-                pageDescription: page.addItemPageDescription,
-                position: 1,
-              }),
-              createListCollectorConfirmationPage({
-                title: page.anotherTitle,
-                pageDescription: page.anotherPageDescription,
-                position: 2,
-                answers: [
-                  {
-                    type: "Radio",
-                    qCode: page.anotherQCode,
-                    options: [
-                      {
-                        id: uuidv4(),
-                        label: page.anotherPositive,
-                        description: page.anotherPositiveDescription,
-                      },
-                      {
-                        id: uuidv4(),
-                        label: page.anotherNegative,
-                        description: page.anotherNegativeDescription,
-                      },
-                    ],
-                  },
-                ],
-              }),
-            ],
-          };
+      // Check folder has list collector page
+      const folderContainsListCollectorPage = hasListCollectorPage(folder);
+      const folderIndex = section.folders.indexOf(folder);
 
-          // Gets the index of the folder containing the list collector page
-          const folderIndex = section.folders.indexOf(folder);
+      // If folder has list collector question, duplicate folder with a slice of questions
+      if (folderContainsListCollectorPage) {
+        const listCollectorPage = folder.pages.find(
+          (page) => page.pageType === "ListCollectorPage"
+        );
 
-          // Incremements iterator to handle folders containing multiple list collector pages
-          folderPositionIterator++;
-          // Adds list collector folder after the folder the list collector page was contained in, or after the last list collector folder created in the migration
-          const newFolderPosition = folderIndex + folderPositionIterator;
-          section.folders.splice(
-            newFolderPosition,
-            0,
-            convertedListCollectorFolder
-          );
+        const listCollectorPageIndex = folder.pages.indexOf(listCollectorPage);
 
-          // Deletes page at position i then decreases the iterator - prevents pages being skipped in the loop
-          folder.pages.splice(pageIndex--, 1);
-        } else {
-          let folderIndex = section.folders.indexOf(folder);
+        const pagesBeforeListCollectorPage = folder.pages.slice(
+          0,
+          listCollectorPageIndex
+        );
+        const pagesAfterListCollectorPage = folder.pages.slice(
+          listCollectorPageIndex + 1
+        );
 
-          folderPositionIterator++;
+        // Create basic folder of pages before list collector page
+        const folderWithPagesBeforeListCollector = createFolder({
+          pages: pagesBeforeListCollectorPage,
+        });
 
-          const newFolderPosition = folderIndex + folderPositionIterator;
-          const newFolder = createFolder({ pages: [page] });
-          section.folders.splice(newFolderPosition, 0, newFolder);
+        // Create a new folder of pages after list collector page
+        const folderWithPagesAfterListCollector = createFolder({
+          pages: pagesAfterListCollectorPage,
+        });
 
-          folder.pages.splice(pageIndex--, 1);
+        // Create list collector folder containing list collector page's data
+        const newListCollectorFolder =
+          generateListCollectorFolder(listCollectorPage);
 
-          if (folder.pages.length === 0) {
-            section.folders.splice(folderIndex, 1);
-          }
-        }
+        // Adds each of the created folders to their expected positions in the section
+        section.folders.splice(
+          folderIndex + 1,
+          0,
+          folderWithPagesBeforeListCollector
+        );
+        section.folders.splice(folderIndex + 2, 0, newListCollectorFolder);
+        section.folders.splice(
+          folderIndex + 3,
+          0,
+          folderWithPagesAfterListCollector
+        );
+
+        section.folders.splice(folderIndex, 1);
       }
     });
   });
