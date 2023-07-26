a3baab98ad998b9a1882ea6f25766d69fc53cc7c
farres1 <sam.farrell@ons.gov.uk>
2023-07-26 11:54:13 +0100

    Add initial add item tests

---
+++ eq-author/src/App/page/Design/ListCollectorPageEditors/AddItemPageEditor/index.test.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { render, fireEvent } from "tests/utils/rtl";
+
+import { useMutation } from "@apollo/react-hooks";
+
+import AddItemPageEditor from ".";
+
+import suppressConsoleMessage from "tests/utils/supressConsol";
+
+/*
+ * @description Suppresses specific messages from being logged in the Console.
+ */
+suppressConsoleMessage("componentWillMount has been renamed", "warn");
+suppressConsoleMessage("componentWillReceiveProps has been renamed", "warn");
+
+jest.mock("@apollo/react-hooks", () => ({
+  //   ...jest.requireActual("@apollo/react-hooks"),
+  useMutation: jest.fn(() => [() => null]),
+  //   useQuery: jest.fn(),
+}));
+
+const renderAddItemPageEditor = (props) => {
+  return render(<AddItemPageEditor {...props} />);
+};
+
+describe("ListCollectorPageEditors/AddItemPageEditor", () => {
+  let props, mockUseMutation;
+
+  beforeEach(() => {
+    mockUseMutation = jest.fn();
+    useMutation.mockImplementation(jest.fn(() => [mockUseMutation]));
+
+    props = {
+      page: {
+        id: "page-1",
+        alias: "P1",
+        title: "Page 1",
+        pageDescription: "Page 1 description",
+        displayName: "P1",
+        position: 0,
+        folder: { id: "folder-1" },
+        section: { id: "section-1" },
+        validationErrorInfo: { errors: [] },
+      },
+    };
+  });
+
+  it("should render add item page editor", () => {
+    const { getByTestId } = renderAddItemPageEditor(props);
+
+    expect(
+      getByTestId("list-collector-add-item-page-editor")
+    ).toBeInTheDocument();
+  });
+
+  //   it("should update page alias", () => {
+  //     const { getByTestId } = renderAddItemPageEditor(props);
+
+  //     const aliasInput = getByTestId("alias");
+
+  //     expect(mockUseMutation).toHaveBeenCalledTimes(0);
+
+  //     fireEvent.update(aliasInput, { target: { value: "New alias" } });
+
+  //     expect(mockUseMutation).toHaveBeenCalledTimes(1);
+  //   });
+});
