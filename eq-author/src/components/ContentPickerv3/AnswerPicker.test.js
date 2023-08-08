3ccca3a5420423eee503a22d65af4c9d9293fcdd
farres1 <sam.farrell@ons.gov.uk>
2023-08-08 14:19:12 +0100

    Add answer picker test for filtered list collector pages

--- eq-author/src/components/ContentPickerv3/AnswerPicker.test.js
+++ eq-author/src/components/ContentPickerv3/AnswerPicker.test.js
@@ -57,6 +57,101 @@ describe("Content Picker Answer Picker", () => {
     ],
   };
 
+  const section3 = {
+    id: "sec3",
+    displayName: "section 3",
+    folders: [
+      {
+        pages: [
+          {
+            id: "Page 3-1",
+            displayName: "Page 3-1",
+            answers: [
+              {
+                id: "Percentage 3",
+                displayName: "Percentage 3",
+                type: "Percentage",
+              },
+              {
+                id: "Hello 2",
+                displayName: "Hello 2",
+                type: "Hello 2",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: "folder-2",
+        listId: "list-1",
+        pages: [
+          {
+            id: "qualifier-1",
+            displayName: "List collector qualifier page",
+            pageType: "ListCollectorQualifierPage",
+            answers: [
+              {
+                id: "qualifier-answer",
+                type: "Radio",
+                options: [
+                  {
+                    id: "qualifier-option-positive",
+                    label: "Yes",
+                  },
+                  {
+                    id: "qualifier-option-negative",
+                    label: "No",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: "add-item-1",
+            displayName: "List collector add item page",
+            pageType: "ListCollectorAddItemPage",
+          },
+          {
+            id: "confirmation-1",
+            displayName: "List collector confirmation page",
+            pageType: "ListCollectorConfirmationPage",
+            answers: [
+              {
+                id: "confirmation-answer",
+                type: "Radio",
+                options: [
+                  {
+                    id: "confirmation-option-positive",
+                    label: "Yes",
+                  },
+                  {
+                    id: "confirmation-option-negative",
+                    label: "No",
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        pages: [
+          {
+            id: "Page 4-1",
+            displayName: "Page 4-1",
+            answers: [
+              {
+                id: "Percentage 4",
+                displayName: "Percentage 4",
+                type: "Percentage",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+
   const expectFlatAnswerList = ({
     answerToggle,
     sectionToggle,
@@ -149,6 +244,7 @@ describe("Content Picker Answer Picker", () => {
       section1Button: section1Button2,
     });
   });
+
   it("Types Percentage into the search bar and returns the answer", () => {
     const { getByText, queryByPlaceholderText, queryByText } =
       renderAnswerPicker({});
@@ -161,5 +257,35 @@ describe("Content Picker Answer Picker", () => {
 
     expect(getByText("Percentage 1")).toBeInTheDocument();
     expect(queryByText("Hello 1")).not.toBeInTheDocument();
+  });
+
+  it("should remove list collector page types from content picker", () => {
+    props.data.push(section2);
+    props.data.push(section3);
+    const { queryByText, getByTestId, queryAllByText } = renderAnswerPicker();
+
+    fireEvent.click(getByTestId("option-button-answers"));
+
+    expect(queryByText("Page 1")).toBeInTheDocument();
+    expect(queryByText("Percentage 1")).toBeInTheDocument();
+
+    expect(queryAllByText("Page 2-1").length).toEqual(2);
+    expect(queryByText("Percentage 2")).toBeInTheDocument();
+    expect(queryByText("Hello 1")).toBeInTheDocument();
+
+    expect(queryAllByText("Page 3-1").length).toEqual(2);
+    expect(queryByText("Percentage 3")).toBeInTheDocument();
+    expect(queryByText("Hello 2")).toBeInTheDocument();
+
+    expect(
+      queryByText("List collector qualifier page")
+    ).not.toBeInTheDocument();
+    expect(queryByText("List collector add item page")).not.toBeInTheDocument();
+    expect(
+      queryByText("List collector confirmation page")
+    ).not.toBeInTheDocument();
+
+    expect(queryByText("Page 4-1")).toBeInTheDocument();
+    expect(queryByText("Percentage 4")).toBeInTheDocument();
   });
 });
