c9390688dad976ecf914d858c790220455ca7b78
martyncolmer <martyc1974@hotmail.com>
2023-08-15 14:44:29 +0100

    Fix api tests

--- eq-author-api/schema/tests/prePopSchema.test.js
+++ eq-author-api/schema/tests/prePopSchema.test.js
@@ -36,90 +36,86 @@ describe("questionnaire", () => {
     Promise.resolve({
       status: 200,
       json: () => ({
-        id: "121-222-789",
-        surveyId: "121",
-        schema: {
-          $schema: "https://json-schema.org/draft/2020-12/schema",
-          $id: "roofing_tiles_and_slate.json",
-          title: "SDS schema for the Roofing Tiles + Slate survey",
-          type: "object",
-          properties: {
-            identifier: {
-              type: "string",
-              description:
-                "The unique top-level identifier. This is the reporting unit reference without the check letter appended",
-              minLength: 11,
-              pattern: "^[a-zA-Z0-9]+$",
-              examples: ["34942807969"],
-            },
-            companyName: {
-              type: "string",
-              minLength: 1,
-              examples: ["Joe Bloggs PLC"],
-            },
-            companyType: {
-              type: "string",
-              minLength: 1,
-              examples: ["Public Limited Company"],
-            },
-            items: {
-              type: "object",
-              properties: {
-                localUnits: {
-                  type: "array",
-                  description: "The data about each item",
-                  minItems: 1,
-                  uniqueItems: true,
-                  items: {
-                    type: "object",
-                    properties: {
-                      identifier: {
-                        type: "string",
-                        minLength: 1,
-                        description:
-                          "The unique identifier for the items. This is the local unit reference.",
-                        examples: ["3340224"],
-                      },
-                      luName: {
-                        type: "string",
-                        minLength: 1,
-                        description: "Name of the local unit",
-                        examples: ["STUBBS BUILDING PRODUCTS LTD"],
-                      },
-                      luAddress: {
-                        type: "array",
-                        description:
-                          "The fields of the address for the local unit",
-                        items: {
-                          type: "string",
-                          minLength: 1,
-                        },
-                        minItems: 1,
-                        uniqueItems: true,
-                        examples: [
-                          [
-                            "WELLINGTON ROAD",
-                            "LOCHMABEN",
-                            "SWINDON",
-                            "BEDS",
-                            "GLOS",
-                            "DE41 2WA",
-                          ],
-                        ],
-                      },
+        $schema: "https://json-schema.org/draft/2020-12/schema",
+        $id: "roofing_tiles_and_slate.json",
+        title: "SDS schema for the Roofing Tiles + Slate survey",
+        type: "object",
+        properties: {
+          identifier: {
+            type: "string",
+            description:
+              "The unique top-level identifier. This is the reporting unit reference without the check letter appended",
+            minLength: 11,
+            pattern: "^[a-zA-Z0-9]+$",
+            examples: ["34942807969"],
+          },
+          companyName: {
+            type: "string",
+            minLength: 1,
+            examples: ["Joe Bloggs PLC"],
+          },
+          companyType: {
+            type: "string",
+            minLength: 1,
+            examples: ["Public Limited Company"],
+          },
+          items: {
+            type: "object",
+            properties: {
+              localUnits: {
+                type: "array",
+                description: "The data about each item",
+                minItems: 1,
+                uniqueItems: true,
+                items: {
+                  type: "object",
+                  properties: {
+                    identifier: {
+                      type: "string",
+                      minLength: 1,
+                      description:
+                        "The unique identifier for the items. This is the local unit reference.",
+                      examples: ["3340224"],
                     },
-                    additionalProperties: false,
-                    required: ["identifier", "lu_name", "lu_address"],
+                    luName: {
+                      type: "string",
+                      minLength: 1,
+                      description: "Name of the local unit",
+                      examples: ["STUBBS BUILDING PRODUCTS LTD"],
+                    },
+                    luAddress: {
+                      type: "array",
+                      description:
+                        "The fields of the address for the local unit",
+                      items: {
+                        type: "string",
+                        minLength: 1,
+                      },
+                      minItems: 1,
+                      uniqueItems: true,
+                      examples: [
+                        [
+                          "WELLINGTON ROAD",
+                          "LOCHMABEN",
+                          "SWINDON",
+                          "BEDS",
+                          "GLOS",
+                          "DE41 2WA",
+                        ],
+                      ],
+                    },
                   },
+                  additionalProperties: false,
+                  required: ["identifier", "lu_name", "lu_address"],
                 },
               },
-              additionalProperties: false,
-              required: ["local_units"],
             },
+            additionalProperties: false,
+            required: ["local_units"],
           },
-          additionalProperties: false,
-          required: ["schema_version", "identifier", "items"],
         },
+        additionalProperties: false,
+        required: ["schema_version", "identifier", "items"],
       }),
     })
   );
--- eq-author-api/schema/tests/prePopSchemaVersions.test.js
+++ eq-author-api/schema/tests/prePopSchemaVersions.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const executeQuery = require("../../tests/utils/executeQuery");
 
 jest.mock("node-fetch");
@@ -8,9 +9,9 @@ query GetPrepopSchemaVersions($id: ID!) {
     prepopSchemaVersions(id: $id) {
       surveyId
       versions {
-        id
-        version
-        dateCreated
+        guid
+        sds_schema_version
+        sds_published_at
       }
     }
   }
@@ -19,40 +20,27 @@ query GetPrepopSchemaVersions($id: ID!) {
 fetch.mockImplementation(() =>
   Promise.resolve({
     status: 200,
-    json: () => ({
-      surveyId: "123",
-      versions: [
-        {
-          id: "123-111-789",
-          version: "1",
-          dateCreated: "2023-01-12T13:37:27+00:00",
-        },
-        {
-          id: "123-222-789",
-          version: "2",
-          dateCreated: "2023-02-08T12:37:27+00:00",
-        },
-        {
-          id: "123-333-789",
-          version: "3",
-          dateCreated: "2023-03-23T08:37:27+00:00",
-        },
-      ],
-    }),
+    json: () => [
+      {
+        guid: "123-111-789",
+        sds_schema_version: "1",
+        sds_published_at: "2023-01-12T13:37:27+00:00",
+      },
+      {
+        guid: "123-222-789",
+        sds_schema_version: "2",
+        sds_published_at: "2023-02-08T12:37:27+00:00",
+      },
+      {
+        guid: "123-333-789",
+        sds_schema_version: "3",
+        sds_published_at: "2023-03-23T08:37:27+00:00",
+      },
+    ],
   })
 );
 
 describe("PrepopSchemaVersions", () => {
-  it("should throw error if the ID is undefined", async () => {
-    try {
-      await executeQuery(query);
-    } catch (error) {
-      expect(error.message).toMatch(
-        `Variable "$id" of required type "ID!" was not provided.`
-      );
-    }
-  });
-
   it("should query prepop schema", async () => {
     const surveyId = "123";
     const expectedResponse = {
@@ -61,19 +49,19 @@ describe("PrepopSchemaVersions", () => {
           surveyId: "123",
           versions: [
             {
-              id: "123-111-789",
-              version: "1",
-              dateCreated: "2023-01-12T13:37:27+00:00",
+              guid: "123-111-789",
+              sds_schema_version: "1",
+              sds_published_at: "2023-01-12T13:37:27+00:00",
             },
             {
-              id: "123-222-789",
-              version: "2",
-              dateCreated: "2023-02-08T12:37:27+00:00",
+              guid: "123-222-789",
+              sds_schema_version: "2",
+              sds_published_at: "2023-02-08T12:37:27+00:00",
             },
             {
-              id: "123-333-789",
-              version: "3",
-              dateCreated: "2023-03-23T08:37:27+00:00",
+              guid: "123-333-789",
+              sds_schema_version: "3",
+              sds_published_at: "2023-03-23T08:37:27+00:00",
             },
           ],
         },
