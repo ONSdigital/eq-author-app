c56029135611b607ca6f748f7cfc96a557e2e33b
martyncolmer <martyc1974@hotmail.com>
2023-08-21 14:00:25 +0100

    updated coverage

---
+++ eq-author-api/src/businessLogic/onUnlinkSupplementaryData.test.js
@@ -0,0 +1,44 @@
+const onUnlinkSupplementaryData = require("./onUnlinkSupplementaryData");
+
+describe("onUnlinkSupplementaryData", () => {
+  let questionnaire, supplementaryData;
+
+  beforeEach(() => {
+    supplementaryData = [
+      {
+        id: "025a58d6",
+        listName: "test",
+        schemaFields: [
+          {
+            id: "65ed8557",
+            type: "string",
+            identifier: "note",
+            selector: "title",
+          },
+        ],
+      },
+    ];
+    questionnaire = {
+      id: "456",
+      sections: [
+        {
+          id: "123",
+          introductionTitle: `<p><span data-piped="supplementary" data-id="65ed8557">[note - title]</span></p>`,
+          repeatingSectionListId: "025a58d6",
+        },
+      ],
+    };
+  });
+
+  it("Deletes any piping from supplementary data", () => {
+    onUnlinkSupplementaryData(questionnaire, supplementaryData);
+    expect(questionnaire.sections[0].introductionTitle).toBe(
+      `<p><span data-piped="supplementary" data-id="65ed8557">[Deleted answer]</span></p>`
+    );
+  });
+
+  it("sets repeating section  list id to emply string", () => {
+    onUnlinkSupplementaryData(questionnaire, supplementaryData);
+    expect(questionnaire.sections[0].repeatingSectionListId).toBe("");
+  });
+});
