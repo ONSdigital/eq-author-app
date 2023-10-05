7b63c016c44ca1f7162b7a62910b2ddde0cc03b9
Paul-Joel <paul.joel@ons.gov.uk>
2023-10-05 19:13:50 +0100

    Extra bits

--- eq-author/src/App/PasteModalContext.js
+++ eq-author/src/App/PasteModalContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
+import PropType from "prop-types";
 
 const PasteModalContext = createContext();
 PasteModalContext.displayName = "PasteModalContext";
@@ -14,3 +15,21 @@ const PasteModalContextProvider = ({ children }) => {
     </PasteModalContext.Provider>
   );
 };
+
+function usePasteModalContext() {
+  const context = useContext(PasteModalContext);
+  // if context is undefined this means it was used outside of its provider
+  // you can throw an error telling that to your fellow developers
+  if (!context) {
+    throw new Error(
+      "usePasteModalContext must be used under <PasteModalContextProvider/>"
+    );
+  }
+  return context;
+}
+
+PasteModalContextProvider.propTypes = {
+  children: PropType.node,
+};
+
+export { PasteModalContextProvider, usePasteModalContext };
