560072ed75a067d23ecefbca6aab25d7236d41ea
Farhanam76 <farhanna700@gmail.com>
2023-10-09 15:32:56 +0100

    testing

--- eq-author/src/utils/answersHaveAnswerType.js
+++ eq-author/src/utils/answersHaveAnswerType.js
@@ -1,5 +1,6 @@
 // Returns true if answers includes an answer of the specified answerType
 const answersHaveAnswerType = (answers, answerType) => {
+  console.log("test");
   if (answers.some((answer) => answer.type === answerType)) {
     return true;
   } else {
