6bdde0b5eeedfc3f0b965cdee3f4bb763a10bf0a
Farhanam76 <farhanna700@gmail.com>
2023-10-09 16:21:07 +0100

    testing4

--- eq-author/src/utils/answersHaveAnswerType.js
+++ eq-author/src/utils/answersHaveAnswerType.js
@@ -3,6 +3,7 @@ const answersHaveAnswerType = (answers, answerType) => {
   console.log("test");
   console.log("test2");
   console.log("test3");
+  console.log("test4");
 
   if (answers.some((answer) => answer.type === answerType)) {
     return true;
