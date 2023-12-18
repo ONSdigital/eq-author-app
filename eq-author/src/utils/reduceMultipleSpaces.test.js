c4fd963e86d1290f672da33814e672ce813dbc34
Paul Joel <paul.joel@ons.gov.uk>
2023-12-18 10:18:18 +0000

    Update eq-author/src/utils/reduceMultipleSpaces.test.js
    Co-authored-by: farres1 <71004932+farres1@users.noreply.github.com>

--- eq-author/src/utils/reduceMultipleSpaces.test.js
+++ eq-author/src/utils/reduceMultipleSpaces.test.js
@@ -1,7 +1,7 @@
 import { reduceMultipleSpaces } from "./reduceMultipleSpaces";
 
 describe("reduceMultipleSpaces function", () => {
-  test("replaces multiple spaces with a single space", () => {
+  it("should replace multiple spaces with a single space", () => {
     const inputText = "   Hello    World    ";
     const expectedOutput = " Hello World ";
     const result = reduceMultipleSpaces(inputText);
