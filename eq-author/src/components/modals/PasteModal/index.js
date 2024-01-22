d407aa614f4d851b508946e15c591c00f7910e99
farres1 <sam.farrell@ons.gov.uk>
2024-01-22 14:31:44 +0000

    Update paste modal design to match import modal

--- eq-author/src/components/modals/PasteModal/index.js
+++ eq-author/src/components/modals/PasteModal/index.js
@@ -1,34 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import { colors } from "constants/theme";
-import Modal from "components/modals/Modal";
-import Button from "components-themed/buttons";
+import Modal from "components-themed/Modal";
 import PropTypes from "prop-types";
-import Theme from "contexts/themeContext";
-
-const StyledModal = styled(Modal)`
-  .Modal {
-    max-width: 500px;
-    border-radius: 0.4rem;
-    padding: 0 20px 20px 20px;
-  }
-`;
-
-const Title = styled.h2`
-  color: ${colors.darkGrey};
-`;
 
 const Message = styled.div``;
-
-const StyledButton = styled(Button)`
-  margin-right: ${(props) => props.margin && `0.5em`};
-`;
-
-const ButtonContainer = styled.div`
-  margin-top: 1em;
-  float: right;
-  padding-bottom: 0.3em;
-`;
 
 export const preserveRichFormatting = (text) => {
   // Replace multiple spaces and tabs with a single space
@@ -48,24 +23,16 @@ const PasteModal = ({
   onConfirm: handleConfirm,
   onCancel: handleCancel,
 }) => {
-  useEffect(() => {
-    const close = (event) => {
-      if (event.key === "Escape") {
-        handleCancel();
-      }
-    };
-
-    window.addEventListener("keydown", close);
-    return () => window.removeEventListener("keydown", close);
-  });
-
   return (
-    <StyledModal
+    <Modal
+      title="Confirm the removal of extra spaces from copied content"
+      warningMessage="By cancelling the content will not be pasted"
       isOpen={isOpen}
+      withButtonMargin={false}
+      onConfirm={handleConfirm}
       onClose={handleCancel}
       data={{ test: "paste-modal" }}
     >
-      <Title>Confirm the removal of extra spaces from copied content</Title>
       <Message>
         <p>
           The copied content contains extra spaces at the start of lines of
@@ -73,30 +40,9 @@ const PasteModal = ({
         </p>
         <p>
           Extra spaces need to be removed before this content can be pasted.
-          Confirming will remove them automatically, while cancelling will
-          prevent pasting.
         </p>
       </Message>
-      <Theme themeName={"onsLegacyFont"}>
-        <ButtonContainer>
-          <StyledButton
-            variant="secondary"
-            margin
-            onClick={handleCancel}
-            data-test="paste-modal-cancel"
-          >
-            Cancel
-          </StyledButton>
-          <StyledButton
-            variant="primary"
-            onClick={handleConfirm}
-            data-test="paste-modal-confirm"
-          >
-            Confirm
-          </StyledButton>
-        </ButtonContainer>
-      </Theme>
-    </StyledModal>
+    </Modal>
   );
 };
 
