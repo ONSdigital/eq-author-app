0df8c892271a39d09d94701266650836adeb5355
farres1 <sam.farrell@ons.gov.uk>
2024-02-27 08:37:22 +0000

    Add migration tests

---
+++ eq-author-api/migrations/addAllowableDataVersions.test.js
@@ -0,0 +1,77 @@
+const addAllowableDataVersions = require("./addAllowableDataVersions");
+
+describe("addAllowableDataVersions", () => {
+  it("should add allowableDataVersions with data version 3 when questionnaire has collection list", () => {
+    const questionnaire = {
+      id: "questionnaire-1",
+      title: "Questionnaire 1",
+      collectionLists: {
+        lists: [{ id: "list-1" }],
+      },
+    };
+
+    const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
+
+    expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
+  });
+
+  it("should add allowableDataVersions with data version 3 when questionnaire has supplementary data", () => {
+    const questionnaire = {
+      id: "questionnaire-1",
+      title: "Questionnaire 1",
+      supplementaryData: { id: "supplementary-data-1" },
+    };
+
+    const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
+
+    expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
+  });
+
+  it("should add allowableDataVersions with data version 3 when questionnaire has dynamic answer", () => {
+    const questionnaire = {
+      id: "questionnaire-1",
+      title: "Questionnaire 1",
+      sections: [
+        {
+          id: "section-1",
+          folders: [
+            {
+              id: "folder-1",
+              pages: [
+                {
+                  id: "page-1",
+                  answers: [
+                    {
+                      id: "answer-1",
+                      options: [
+                        {
+                          id: "option-1",
+                          dynamicAnswer: true,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
+    const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
+
+    expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
+  });
+
+  it("should add allowableDataVersions with data versions 1 and 3 when questionnaire has no collection lists, supplementary data or dynamic answers", () => {
+    const questionnaire = {
+      id: "questionnaire-1",
+      title: "Questionnaire 1",
+    };
+
+    const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
+
+    expect(updatedQuestionnaire.allowableDataVersions).toEqual(["1", "3"]);
+  });
+});
