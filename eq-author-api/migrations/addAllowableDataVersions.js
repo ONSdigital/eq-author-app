e08ea46ba7ed893b7366a0cb99852bca43a2ad17
farres1 <sam.farrell@ons.gov.uk>
2024-02-28 09:38:30 +0000

    Update migration to set data version

--- eq-author-api/migrations/addAllowableDataVersions.js
+++ eq-author-api/migrations/addAllowableDataVersions.js
@@ -3,11 +3,12 @@ const { getOptions } = require("../schema/resolvers/utils");
 module.exports = (questionnaire) => {
   const allQuestionnaireOptions = getOptions({ questionnaire });
 
-  if (questionnaire.collectionLists?.lists?.length > 0) {
-    questionnaire.allowableDataVersions = ["3"];
-  } else if (questionnaire.supplementaryData) {
-    questionnaire.allowableDataVersions = ["3"];
-  } else if (allQuestionnaireOptions?.some((option) => option.dynamicAnswer)) {
+  if (
+    questionnaire.collectionLists?.lists?.length > 0 ||
+    questionnaire.supplementaryData ||
+    allQuestionnaireOptions?.some((option) => option.dynamicAnswer)
+  ) {
+    questionnaire.dataVersion = "3";
     questionnaire.allowableDataVersions = ["3"];
   } else {
     questionnaire.allowableDataVersions = ["1", "3"];
--- eq-author-api/migrations/addAllowableDataVersions.test.js
+++ eq-author-api/migrations/addAllowableDataVersions.test.js
@@ -1,7 +1,7 @@
 const addAllowableDataVersions = require("./addAllowableDataVersions");
 
 describe("addAllowableDataVersions", () => {
-  it("should add allowableDataVersions with data version 3 when questionnaire has collection list", () => {
+  it("should set dataVersion to 3 and add allowableDataVersions with data version 3 when questionnaire has collection list", () => {
     const questionnaire = {
       id: "questionnaire-1",
       title: "Questionnaire 1",
@@ -12,10 +12,11 @@ describe("addAllowableDataVersions", () => {
 
     const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
 
+    expect(updatedQuestionnaire.dataVersion).toEqual("3");
     expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
   });
 
-  it("should add allowableDataVersions with data version 3 when questionnaire has supplementary data", () => {
+  it("should set dataVersion to 3 and add allowableDataVersions with data version 3 when questionnaire has supplementary data", () => {
     const questionnaire = {
       id: "questionnaire-1",
       title: "Questionnaire 1",
@@ -24,10 +25,11 @@ describe("addAllowableDataVersions", () => {
 
     const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
 
+    expect(updatedQuestionnaire.dataVersion).toEqual("3");
     expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
   });
 
-  it("should add allowableDataVersions with data version 3 when questionnaire has dynamic answer", () => {
+  it("should set dataVersion to 3 and add allowableDataVersions with data version 3 when questionnaire has dynamic answer", () => {
     const questionnaire = {
       id: "questionnaire-1",
       title: "Questionnaire 1",
@@ -61,17 +63,20 @@ describe("addAllowableDataVersions", () => {
 
     const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
 
+    expect(updatedQuestionnaire.dataVersion).toEqual("3");
     expect(updatedQuestionnaire.allowableDataVersions).toEqual(["3"]);
   });
 
-  it("should add allowableDataVersions with data versions 1 and 3 when questionnaire has no collection lists, supplementary data or dynamic answers", () => {
+  it("should add allowableDataVersions with data versions 1 and 3 without updating dataVersion when questionnaire has no collection lists, supplementary data or dynamic answers", () => {
     const questionnaire = {
       id: "questionnaire-1",
       title: "Questionnaire 1",
+      dataVersion: "1",
     };
 
     const updatedQuestionnaire = addAllowableDataVersions(questionnaire);
 
+    expect(updatedQuestionnaire.dataVersion).toEqual("1");
     expect(updatedQuestionnaire.allowableDataVersions).toEqual(["1", "3"]);
   });
 });
