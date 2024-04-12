0f9b33f040bdeb9afe6758c5d80f5a93f10e993f
farres1 <sam.farrell@ons.gov.uk>
2024-04-12 13:10:18 +0100

    Fix migration to prevent wrapping text in two strong tags when original text was wrapped in strong and em tags

--- eq-author-api/migrations/convertEmTagsToStrongTags.js
+++ eq-author-api/migrations/convertEmTagsToStrongTags.js
@@ -1,7 +1,13 @@
 const convertEmTagsToStrongTags = (inputData) => {
   if (inputData !== null && inputData !== undefined) {
     if (typeof inputData === "string") {
+      /* 
+        Removes em tags wrapped in strong tags and em tags wrapped around strong tags before converting em tags 
+        Prevents wrapping text in two strong tags when it was previously wrapped in both strong and em tags
+      */
       inputData = inputData
+        .replace(/<strong><em>|<em><strong>/g, "<strong>")
+        .replace(/<\/em><\/strong>|<\/strong><\/em>/g, "</strong>")
         .replace(/<em>/g, "<strong>")
         .replace(/<\/em>/g, "</strong>");
     } else if (Array.isArray(inputData)) {
