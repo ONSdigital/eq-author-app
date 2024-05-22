a2a30d5bc3f42f77fcbe1b2d83efa40cfa7e7dcb
farres1 <sam.farrell@ons.gov.uk>
2024-05-22 12:17:42 +0100

    Add getContentBeforeEntity tests

--- eq-author/src/utils/getContentBeforeEntity.test.js
+++ eq-author/src/utils/getContentBeforeEntity.test.js
@@ -4,6 +4,8 @@ import {
 } from "tests/utils/createMockQuestionnaire";
 import getPreviousContent from "./getContentBeforeEntity";
 
+import { MUTUALLY_EXCLUSIVE } from "constants/answer-types";
+
 let questionnaire = buildQuestionnaire({
   sectionCount: 2,
   folderCount: 2,
@@ -15,8 +17,146 @@ questionnaire.introduction = {
 };
 
 describe("utils/getPreviousAnswers", () => {
+  beforeEach(() => {
+    // Adds page ID to each answer
+    questionnaire.sections.forEach((section) => {
+      section.folders.forEach((folder) => {
+        folder.pages.forEach((page) => {
+          page.answers.forEach((answer) => {
+            answer.page = {
+              id: page.id,
+            };
+          });
+        });
+      });
+    });
+  });
+
   it("should return empty array when questionnaire or ID not provided", () => {
     expect(getPreviousContent()).toHaveLength(0);
+  });
+
+  it("should return mutually exclusive answers when expression's left side answer is on a different page", () => {
+    questionnaire.sections[1].folders[0].pages[1].answers[1].type =
+      MUTUALLY_EXCLUSIVE;
+
+    const previousContent = getPreviousContent({
+      questionnaire,
+      id: questionnaire.sections[1].folders[0].pages[1].id,
+      includeTargetPage: true,
+      expressionGroup: {
+        operator: "And",
+        expressions: [
+          {
+            left: {
+              id: questionnaire.sections[1].folders[0].pages[0].answers[0].id,
+              page: {
+                id: questionnaire.sections[1].folders[0].pages[0].id,
+              },
+            },
+          },
+        ],
+      },
+    });
+
+    // Tests previousContent[0] matches the questionnaire's first section
+    expect(previousContent).toHaveLength(2);
+    expect(previousContent[0]).toMatchObject(questionnaire.sections[0]);
+
+    // Tests previousContent[1]'s first folder's first page matches the questionnaire's second section's first folder's first page
+    expect(previousContent[1].folders[0].pages).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[0].answers).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[0]).toMatchObject(
+      questionnaire.sections[1].folders[0].pages[0]
+    );
+
+    // Tests previousContent[1]'s first folder's second page matches the questionnaire's second section's first folder's second page
+    expect(previousContent[1].folders[0].pages[1].answers).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[1]).toMatchObject(
+      questionnaire.sections[1].folders[0].pages[1]
+    );
+  });
+
+  it("should not return mutually exclusive answers on the same page as the expression's left side answer", () => {
+    questionnaire.sections[1].folders[0].pages[1].answers[1].type =
+      MUTUALLY_EXCLUSIVE;
+
+    const previousContent = getPreviousContent({
+      questionnaire,
+      id: questionnaire.sections[1].folders[0].pages[1].id,
+      includeTargetPage: true,
+      expressionGroup: {
+        operator: "And",
+        expressions: [
+          {
+            left: {
+              id: questionnaire.sections[1].folders[0].pages[1].answers[0].id,
+              page: {
+                id: questionnaire.sections[1].folders[0].pages[1].id,
+              },
+            },
+          },
+        ],
+      },
+    });
+
+    // Tests previousContent[0] matches the questionnaire's first section
+    expect(previousContent).toHaveLength(2);
+    expect(previousContent[0]).toMatchObject(questionnaire.sections[0]);
+
+    // Tests previousContent[1]'s first folder's first page matches the questionnaire's second section's first folder's first page
+    expect(previousContent[1].folders[0].pages).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[0].answers).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[0]).toMatchObject(
+      questionnaire.sections[1].folders[0].pages[0]
+    );
+
+    // Tests mutually exclusive answer is removed from previousContent[1]'s first folder's second page
+    expect(previousContent[1].folders[0].pages[1].answers).toHaveLength(1);
+    expect(previousContent[1].folders[0].pages[1].answers[0]).toMatchObject(
+      questionnaire.sections[1].folders[0].pages[1].answers[0]
+    );
+    expect(
+      previousContent[1].folders[0].pages[1].answers.some(
+        (answer) => answer.type === MUTUALLY_EXCLUSIVE
+      )
+    ).toBe(false);
+  });
+
+  it("should not return answers on the same page as expression's left side answer when left side answer is mutually exclusive", () => {
+    questionnaire.sections[1].folders[0].pages[1].answers[1].type =
+      MUTUALLY_EXCLUSIVE;
+
+    const previousContent = getPreviousContent({
+      questionnaire,
+      id: questionnaire.sections[1].folders[0].pages[1].id,
+      includeTargetPage: true,
+      expressionGroup: {
+        operator: "And",
+        expressions: [
+          {
+            left: {
+              id: questionnaire.sections[1].folders[0].pages[1].answers[1].id,
+              type: MUTUALLY_EXCLUSIVE,
+              page: {
+                id: questionnaire.sections[1].folders[0].pages[1].id,
+              },
+            },
+          },
+        ],
+      },
+    });
+
+    // Tests previousContent[0] matches the questionnaire's first section
+    expect(previousContent).toHaveLength(2);
+    expect(previousContent[0]).toMatchObject(questionnaire.sections[0]);
+
+    // Tests previousContent[1]'s first folder's first page matches the questionnaire's second section's first folder's first page, and that the second page has been removed
+    expect(previousContent[1].folders[0].pages).toHaveLength(1);
+    expect(previousContent[1].folders[0].pages[0].answers).toHaveLength(2);
+    expect(previousContent[1].folders[0].pages[0]).toMatchObject(
+      questionnaire.sections[1].folders[0].pages[0]
+    );
   });
 
   it("should return empty array on questionnaire introduction page", () => {
