38bc8b979f506a433e2286efe002855bf3c8f647
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-09-09 13:49:56 +0100

    Code cleaned up

--- eq-author-api/src/validation/customKeywords/validateSurveyID.js
+++ eq-author-api/src/validation/customKeywords/validateSurveyID.js
@@ -14,24 +14,13 @@ module.exports = (ajv) =>
       {
         instancePath, // gives the path /surveyId
         rootData: questionnaire, // gives the whole object of questionnaire
-        parentData, // gives the whole object of questionnaire
+        // parentData, // gives the whole object of questionnaire
         parentDataProperty: fieldName, // gives the field name surveyId
       }
     ) {
-      // console.log("instancePath", instancePath);
-      // console.log("schema", schema);
-      // console.log("parentSchema", parentSchema);
       console.log("data", data);
-      // console.log("questionnaire", questionnaire.supplementaryData.surveyId);
-      // Get the supplementary data from rootData
-      const supplementaryData = questionnaire.supplementaryData; // gives the supplementary data field
-      console.log("supplementaryData", supplementaryData.surveyId);
-
-      // const questionnaireSurveyId = questionnaire.surveyId; // gives the data entered into the survey ID field
-      // console.log("questionnaireSurveyId", questionnaireSurveyId);
-
-      // console.log("data", data); // gives the data entered into the survey ID field
-      // console.log("parentdata", parentData); // the whole questionnaire object
+      // Get the supplementary data from the questionnaire object
+      const supplementaryData = questionnaire.supplementaryData;
 
       // If supplementaryData is not available or doesn't contain surveyId, or if surveyId doesn't match, throw an error
       if (
@@ -39,7 +28,6 @@ module.exports = (ajv) =>
         supplementaryData.surveyId &&
         data !== supplementaryData.surveyId
       ) {
-        // console.log("meow", ERR_SURVEY_ID_MISMATCH);
         isValid.errors = [
           createValidationError(
             instancePath,
