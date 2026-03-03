5e27b38496bed7644f91c904bf35c09b24e1062c
Martyn <46242834+martyncolmer@users.noreply.github.com>
2026-03-03 09:31:35 +0000

    commented out GUID check
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>

--- eq-author-api/schema/resolvers/utils/publishSchema.js
+++ eq-author-api/schema/resolvers/utils/publishSchema.js
@@ -155,12 +155,12 @@ const publishSchema = async (ctx) => {
         }
         logger.info(publishResult, `publish questionnaire with version id ${ctx.questionnaire.questionnaireVersionId}  - posted to second CIR gateway`);
 
-        //check CIR id matches the versionId to ensure the correct CI has been published
-        if(publishResult.cirId !== ctx.questionnaire.questionnaireVersionId) {
+        //check CIR id matches the versionId to ensure the correct CI has been published, commented out until CIR complete thier work to return the GUID
+        /* if(publishResult.cirId !== ctx.questionnaire.questionnaireVersionId) {
             publishResult.success = false;
             publishResult.errorMessage = `CIR gateway did not return expected guid - expected ${ctx.questionnaire.questionnaireVersionId} but got ${publishResult.cirId}`;
             publishResult.displayErrorMessage = "Contact eQ services team";
-        }
+        } */
     }
     finally {
         if (!publishResult.success) {
